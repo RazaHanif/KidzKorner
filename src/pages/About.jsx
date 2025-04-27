01e4a7857267c7fbf2c3b1ba1ddf290119e5d56a
Raza Hanif <raza_hanif@ymail.com>
2025-04-27 09:53:48 -0400

    Working on Frontend @ 9:53:48 AM

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 <iframe
-  src="https://www.google.com/maps/embed/v1/place?key=YOUR_API_KEY&q=KidzKorner+Milton"
+  src="https://www.google.com/maps/embed/v1/place?key=${}&q=KidzKorner+Milton"
   width="600"
   height="450"
   style="border:0;"
