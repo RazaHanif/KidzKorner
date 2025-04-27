0a1c5ead6b09d93444c58540db7aa194b9e83994
Raza Hanif <raza_hanif@ymail.com>
2025-04-27 19:03:10 -0400

    Working on Frontend @ 7:03:10 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ const Home = () => {
     return (
         <>
             <h4>Alittle about us.</h4>
-            <p>Kidz Korner is a family-owned business located in the heart of Miltion, Ontario.</p>
+            <p>Kidz Korner is a family-owned business located in the heart of Miltion, Ontario. Our commitment to providing loving, nurturing care has earned us the trust of many families and community resources, creating a multi-generational legacy.</p>
         </>
     );
 }
