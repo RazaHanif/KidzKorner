629372d7d5fec14c8851c498a8ee77d2b19d03e5
Raza Hanif <raza_hanif@ymail.com>
2025-04-27 19:38:43 -0400

    Working on Frontend @ 7:38:43 PM

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,38 +1,36 @@
-import { NavLink } from 'react-router-dom'
-import './navigation.css'
+import { NavLink } from 'react-router-dom';
+import './navigation.css';
 
 function Navigation() {
   return (
-    <nav>
-      <ul className="nav">
-        <li className="nav-item">
-          <NavLink to="/childcare" end className="nav-link text-light">
-            Child Care
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/parties" end className="nav-link text-light">
-            Parties
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/playground" end className="nav-link text-light">
-            Playground
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/workshop" end className="nav-link text-light">
-            Workshops
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/about" end className="nav-link text-light">
-            About Us
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <ul className="navbar-nav ms-auto">
+      <li className="nav-item">
+        <NavLink to="/childcare" end className="nav-link">
+          Child Care
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink to="/parties" end className="nav-link">
+          Parties
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink to="/playground" end className="nav-link">
+          Playground
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink to="/workshop" end className="nav-link">
+          Workshops
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink to="/about" end className="nav-link">
+          About Us
+        </NavLink>
+      </li>
+    </ul>
   );
 }
 
-export default Navigation
+export default Navigation;
