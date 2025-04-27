8a7c015b882c41867f7e0451e18e2294d08a1b50
Raza Hanif <raza_hanif@ymail.com>
2025-04-27 09:57:54 -0400

    Working on Frontend @ 9:57:54 AM

--- api/maps-url.js
+++ api/maps-url.js
@@ -1,3 +1,9 @@
 export default function hider(req, res) {
     const API_KEY = process.env.GOOGLE_MAPS_API
+    const address = "Kidz Korner Milton"
+    const url = `https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=${encodeURIComponent(address)}`
+
+    console.log(url)
 }
+
+hider()
