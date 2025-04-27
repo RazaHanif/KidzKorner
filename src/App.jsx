f98f3fe4249a673ca226efa1a6259158832c9535
Raza Hanif <raza_hanif@ymail.com>
2025-04-27 10:55:28 -0400

    Working on Frontend @ 10:55:28 AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Footer from './components/Footer'
 import Header from './components/Header'
-import About from './pages/About'
-import Childcare from './pages/Childcare'
-import Home from './pages/Home'
-import Parties from './pages/Parties'
-import Playground from './pages/Playground'
-import Workshops from './pages/Workshops'
+import About from './pages/About.jsx'
+import Childcare from './pages/Childcare.jsx'
+import Home from './pages/Home.jsx'
+import Parties from './pages/Parties.jsx'
+import Playground from './pages/Playground.jsx'
+import Workshops from './pages/Workshops.jsx'
 
 
 function App(){
