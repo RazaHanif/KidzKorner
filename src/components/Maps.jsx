79d790479c518491d482e2150db1a07075b1385b
Raza Hanif <raza_hanif@ymail.com>
2025-04-27 10:22:04 -0400

    Working on Frontend @ 10:22:04 AM

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -5,7 +5,20 @@ function Map() {
 
     useEffect( () => {
         fetch('api/maps-url')
-    })
+        .then(res => res.json())
+        .then(data => setMapUrl(data.url)) 
+    }, [])
+
+    if (!mapUrl) return <div>Loading map...</div>
+
+    return (
+        <iframe
+        src={mapUrl}
+        allowFullScreen
+        loading="lazy"
+        style={{ width: "100%", height: "400px", border: 0 }}
+      />
+    )
 }
 
 export default Map
