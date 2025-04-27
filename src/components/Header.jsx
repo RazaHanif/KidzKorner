cb7698f449387081e1f086949aada49a6c8fff7b
Raza Hanif <raza_hanif@ymail.com>
2025-04-27 18:34:32 -0400

    Working on Frontend @ 6:34:32 PM

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,16 +6,13 @@ const Header = () => {
     return (
         <header className='header'>
             <div className='header-container'>
-                <div className='header-nav'>
-                    <div className='header-logo'>
-                        <NavLink to='/' end>
-                            LOGO
-                        </NavLink>
-                    </div>
-                    <div className='header-nav-links'>
-                        <Navigation />
-                    </div>
-
+                <div className='header-logo'>
+                    <NavLink to='/' end>
+                        LOGO
+                    </NavLink>
+                </div>
+                <div className='header-nav-links'>
+                    <Navigation />
                 </div>
             </div>
         </header>
