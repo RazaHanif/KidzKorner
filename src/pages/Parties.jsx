ef4769ea308bed7aa30aa7321fec5344db8ff679
Raza Hanif <raza_hanif@ymail.com>
2025-04-27 20:54:15 -0400

    Working on Frontend @ 8:54:14 PM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -6,6 +6,24 @@ const Parties = () => {
                 Looking for a fun and safe indoor space for your child to play?<br/>
                 Our indoor playground is open for drop-in play from 9 am to 3 pm Monday to Friday
             </p>
+            <h5>
+                What We Offer:<br/>
+            </h5>
+            <li>
+                A varity of skill-building play areas for your child to explore and enjoy.
+            </li>
+            <li>
+                A perfect escape from the heat, cold, rain or snow.
+            </li>
+            <li>
+                Convenient facilities with two washrooms, diaper changing stations, and step stools.
+            </li>
+
+            <br/>
+            <p>
+                Tea and Coffee are complimentary!
+            </p>
+
             
         </>
     );
