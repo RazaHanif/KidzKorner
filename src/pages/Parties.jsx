b56abb83550947dafe53577111987cb82a4c1ad7
Raza Hanif <raza_hanif@ymail.com>
2025-04-27 20:50:59 -0400

    Working on Frontend @ 8:50:59 PM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -3,8 +3,10 @@ const Parties = () => {
         <>
             <h4>Drop-In Fun!</h4>
             <p>
-                Looking for a fun and safe indoor space for your child to play?
+                Looking for a fun and safe indoor space for your child to play?<br/>
+                Our indoor playground is open for drop-in play from 9 am to 3 pm Monday to Friday
             </p>
+            
         </>
     );
 }
