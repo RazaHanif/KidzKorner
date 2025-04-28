9bc1d66b255ad6816671166c25998ba669521c6d
Raza Hanif <raza_hanif@ymail.com>
2025-04-28 17:52:47 -0400

    last commit hopefully

--- api/send-email.js
+++ api/send-email.js
@@ -29,69 +29,54 @@ export default async function handler(req, res) {
     let emailContent = ''
     let subject = ''
 
+
     if (data.type === 'about') {
         subject = `General Inquiry - ${data.name}`
         emailContent = `
-            <html>
-                <body style="font-family: Arial, sans-serif; color: #333;">
-                    <h1>General Inquiry</h1>
-                    <p><strong>Name:</strong> ${data.name}</p>
-                    <p><strong>Email:</strong> ${data.email}</p>
-                    <p><strong>Phone:</strong> ${data.phone}</p>
-                    <p><strong>Message:</strong> ${data.message}</p>
-                </body>
-            </html>
+            General Inquiry\n
+            Name: ${data.name}\n
+            Email: ${data.email}\n
+            Phone: ${data.phone}\n
+            Message: ${data.message}
         `
     }
     else if (data.type === 'childcare') {
         subject = `Childcare Inquiry - ${data.name}`
         emailContent = `
-            <html>
-                <body style="font-family: Arial, sans-serif; color: #333;">
-                    <h1>Childcare Inquiry</h1>
-                    <p><strong>Name:</strong> ${data.name}</p>
-                    <p><strong>Email:</strong> ${data.email}</p>
-                    <p><strong>Phone:</strong> ${data.phone}</p>
-                    <p><strong>Date of Birth:</strong> ${data.dob}</p>
-                    <p><strong>Enrollment Date:</strong> ${data.startDate}</p>
-                    <p><strong>Program Type:</strong> ${data.programType}</p>
-                    <p><strong>Availability for Tour:</strong> ${data.interviewTiming}</p>
-                    <p><strong>Message:</strong> ${data.message}</p>
-                </body>
-            </html>
+            Childcare Inquiry\n
+            Name: ${data.name}\n
+            Email: ${data.email}\n
+            Phone: ${data.phone}\n
+            Date of Birth: ${data.dob}\n
+            Enrollment Date: ${data.startDate}\n
+            Program Type: ${data.programType}\n
+            Availability for Tour: ${data.interviewTiming}\n
+            Message: ${data.message}
         `
     } 
     else if (data.type === 'parties') {
         subject = `Party Inquiry - ${data.name}`
         emailContent = `
-            <html>
-                <body style="font-family: Arial, sans-serif; color: #333;">
-                    <h1>Party Inquiry</h1>
-                    <p><strong>Name:</strong> ${data.name}</p>
-                    <p><strong>Email:</strong> ${data.email}</p>
-                    <p><strong>Phone:</strong> ${data.phone}</p>
-                    <p><strong>Party Date:</strong> ${data.partyDate}</p>
-                    <p><strong>Number of Guests:</strong> ${data.numOfGuests}</p>
-                    <p><strong>Message:</strong> ${data.message}</p>
-                </body>
-            </html>
+            Party Inquiry\n
+            Name: ${data.name}\n
+            Email: ${data.email}\n
+            Phone: ${data.phone}\n
+            Party Date: ${data.partyDate}\n
+            Number of Guests: ${data.numOfGuests}\n
+            Message: ${data.message}
         `
     }
     else if (data.type === 'workshop') {
         subject = `Workshop Inquiry - ${data.name}`
         emailContent = `
-            <html>
-                <body style="font-family: Arial, sans-serif; color: #333;">
-                    <h1>General Inquiry</h1>
-                    <p><strong>Name:</strong> ${data.name}</p>
-                    <p><strong>Email:</strong> ${data.email}</p>
-                    <p><strong>Phone:</strong> ${data.phone}</p>
-                    <p><strong>Workshop:</strong> ${data.workshop}</p>
-                    <p><strong>Social Media Consent:</strong> ${data.socialMedia}</p>
-                    <p><strong>Contact Again Consent:</strong> ${data.contactAgain}</p>
-                    <p><strong>Allergies:</strong> ${data.allergies}</p>
-                </body>
-            </html>
+            General Inquiry\n
+            Name: ${data.name}\n
+            Email: ${data.email}\n
+            Phone: ${data.phone}\n
+            Workshop: ${data.workshop}\n
+            Social Media Consent: ${data.socialMedia}\n
+            Contact Again Consent: ${data.contactAgain}\n
+            Allergies: ${data.allergies}
         `
     }
     else {
@@ -103,7 +88,7 @@ export default async function handler(req, res) {
         from: process.env.EMAIL_DUMMY,
         to: process.env.EMAIL_INFO,
         subject: subject,
-        html: emailContent,
+        text: emailContent,
         replyTo: data.email
     })
 
