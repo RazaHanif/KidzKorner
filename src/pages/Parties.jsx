8df5fcc1542b9f9426044b76d245eacf478baae7
Raza Hanif <raza_hanif@ymail.com>
2025-04-27 21:19:59 -0400

    Working on Frontend @ 9:19:59 PM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -1,99 +1,29 @@
 const Parties = () => {
     return (
         <>
-            <h4>Drop-In Fun!</h4>
-            <p>
-                Looking for a fun and safe indoor space for your child to play?<br/>
-                Our indoor playground is open for drop-in play from 9 am to 3 pm Monday to Friday
-            </p>
-            <h5>
-                What We Offer:<br/>
-            </h5>
-            <li>
-                A varity of skill-building play areas for your child to explore and enjoy.
-            </li>
-            <li>
-                A perfect escape from the heat, cold, rain or snow.
-            </li>
-            <li>
-                Convenient facilities with two washrooms, diaper changing stations, and step stools.
-            </li>
+            <h4>Host Your Child's Party at Kidz Korner</h4>
+
+            <h6>
+                Make your child’s special day unforgettable with a Kidz Korner Party! Enjoy exclusive access to our indoor playground—just for you and your invited family and friends.
+            </h6>
+
+            <h5>Party Package:</h5>
+            <li>3 hours of private playground access</li>
+            <li>Complimentary tea and coffee for guests</li>
+            <li>Bring your own food at no extra charge (peanut/nut-free)</li>
+            <li>Bring your own decorations to personalize the space. We do offer in house decoration arrangements. </li>
+            <li>Max capacity: 50 guests (seating available for up to 30 adults)</li>
+            <li>Extra 15 minutes before & after for setup and wrap-up—free of charge</li>
 
             <br/>
-            <p>
-                Tea and Coffee are complimentary!
-            </p>
 
-            <div className="accordion accordion-flush" id="accordionFlushExample">
-                <div className="accordion-item">
-                    <h2 className="accordion-header">
-                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                            Drop In
-                        </button>
-                    </h2>
-                    <div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div className="accordion-body">
-                            <h4>$13 | 1 Child & 1 Adult</h4>
-                        </div>
-                    </div>
-                </div>
-            
-                <div className="accordion-item">
-                    <h2 className="accordion-header">
-                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                            Siblings #1
-                        </button>
-                    </h2>
-                    <div id="flush-collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div className="accordion-body">
-                            <h4>$22 | 2 Children & 1 Adult</h4>
-                        </div>
-                    </div>
-                </div>
+            <h5>Pricing & Booking Details:</h5>
+            <li>Price: $395 + tax</li>
+            <li>Deposit: A $200 non-refundable deposit is required at the time of booking</li>
+            <li>Additional Time: Extra party time available at $50 per hour.</li>
+            <p className="vsmall">*Prices are subject to change without notice*</p>
 
-                <div className="accordion-item">
-                    <h2 className="accordion-header">
-                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                            Siblings #2
-                        </button>
-                    </h2>
-                    <div id="flush-collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div className="accordion-body">
-                            <h4>$35 | 3-4 Children & 1 Adult</h4>
-                        </div>
-                    </div>
-                </div>
-                
-                <div className="accordion-item">
-                    <h2 className="accordion-header">
-                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
-                            Play Date #1
-                        </button>
-                    </h2>
-                    <div id="flush-collapseFour" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div className="accordion-body">
-                            <h4>$25 | 2 Children & 2 Adults</h4>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="accordion-item">
-                    <h2 className="accordion-header">
-                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
-                            Play Date #2
-                        </button>
-                    </h2>
-                    <div id="flush-collapseFive" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div className="accordion-body">
-                            <h4>$45 | 3-5 Children & 3 Adults</h4>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
-
-
+            <br/>
         </>
     );
 }
--- src/pages/Playground.jsx
+++ src/pages/Playground.jsx
@@ -1,7 +1,99 @@
 const Playground = () => {
     return (
         <>
-            Playground
+            <h4>Drop-In Fun!</h4>
+            <p>
+                Looking for a fun and safe indoor space for your child to play?<br/>
+                Our indoor playground is open for drop-in play from 9 am to 3 pm Monday to Friday
+            </p>
+            <h5>
+                What We Offer:<br/>
+            </h5>
+            <li>
+                A varity of skill-building play areas for your child to explore and enjoy.
+            </li>
+            <li>
+                A perfect escape from the heat, cold, rain or snow.
+            </li>
+            <li>
+                Convenient facilities with two washrooms, diaper changing stations, and step stools.
+            </li>
+
+            <br/>
+            <p>
+                Tea and Coffee are complimentary!
+            </p>
+
+            <div className="accordion accordion-flush" id="accordionFlushExample">
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                            Drop In
+                        </button>
+                    </h2>
+                    <div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                        <div className="accordion-body">
+                            <h4>$13 | 1 Child & 1 Adult</h4>
+                        </div>
+                    </div>
+                </div>
+            
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                            Siblings #1
+                        </button>
+                    </h2>
+                    <div id="flush-collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                        <div className="accordion-body">
+                            <h4>$22 | 2 Children & 1 Adult</h4>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                            Siblings #2
+                        </button>
+                    </h2>
+                    <div id="flush-collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                        <div className="accordion-body">
+                            <h4>$35 | 3-4 Children & 1 Adult</h4>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+                            Play Date #1
+                        </button>
+                    </h2>
+                    <div id="flush-collapseFour" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                        <div className="accordion-body">
+                            <h4>$25 | 2 Children & 2 Adults</h4>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
+                            Play Date #2
+                        </button>
+                    </h2>
+                    <div id="flush-collapseFive" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                        <div className="accordion-body">
+                            <h4>$45 | 3-5 Children & 3 Adults</h4>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+
         </>
     );
 }
