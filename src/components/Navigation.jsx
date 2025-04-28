6744c8d631663a422587a34aefdd91e35b0de5d4
Raza Hanif <raza_hanif@ymail.com>
2025-04-27 20:00:10 -0400

    Working on Frontend @ 8:00:10 PM

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,25 @@
+import { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import './navigation.css';
 
 function Navigation() {
+    useEffect(() => {
+        const handleResize = () => {
+            const navbarCollapse = document.getElementById('navbarNav');
+            const navbarToggler = document.querySelector('.navbar-toggler');
+
+            if (navbarCollapse.classList.contains('show')) {
+            navbarToggler.click();
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+        }, []);
+
   return (
     <ul className="navbar-nav ms-auto">
       <li className="nav-item">
