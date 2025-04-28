2c3ff2c42ef82cd08bc0312c79a53288f5573ee6
Raza Hanif <raza_hanif@ymail.com>
2025-04-27 20:04:44 -0400

    Working on Frontend @ 8:04:44 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,15 @@ const Home = () => {
     return (
         <>
             <h4>Alittle about us.</h4>
-            <p>Kidz Korner is a family-owned business located in the heart of Miltion, Ontario. Our commitment to providing loving, nurturing care has earned us the trust of many families and community resources, creating a multi-generational legacy.</p>
+            <p>
+                Kidz Korner is a family-owned business located in the heart of Milton, Ontario. Our commitment to providing loving, nurturing care has earned us the trust of many families and community resources, creating a multi-generational legacy..
+            </p>
+            <p>
+                At Kidz Korner, we believe in child-led learning. By observing, engaging with, and reflecting on each child’s experiences, we create a learning environment that is both inclusive and adaptable to each child’s needs. Our dedicated educators work closely with families to foster a strong sense of connection and support.
+            </p>
+            <p>
+                With decades of experience in early childhood education, we are proud to help families navigate the important developmental milestones of the early years. We are here to support your child’s growth, development, and well-being every step of the way.
+            </p>
         </>
     );
 }
