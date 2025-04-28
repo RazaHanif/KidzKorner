4d745dfc04b2e51d3b0cbc32bf7d83b10ef24f08
Raza Hanif <raza_hanif@ymail.com>
2025-04-28 18:23:02 -0400

    burv im "this" close to giving up

--- api/send-email.js
+++ api/send-email.js
@@ -88,9 +88,7 @@ export default async function handler(req, res) {
         from: process.env.EMAIL_DUMMY,
         to: process.env.EMAIL_INFO,
         subject: subject,
-        text: emailContent,
-        html: emailContent.replace(/\n/g, '<br>'),
-        replyTo: data.email
+        text: emailContent
     })
 
     res.status(200).json({ success: true })
