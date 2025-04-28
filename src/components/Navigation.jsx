e88e2433d65cca70025622597ee97485b24bc307
Raza Hanif <raza_hanif@ymail.com>
2025-04-27 20:02:17 -0400

    Working on Frontend @ 8:02:17 PM

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -20,30 +20,40 @@ function Navigation() {
         };
         }, []);
 
+        const closeNavbar = () => {
+            // Get the navbar toggler button
+            const navbarToggler = document.querySelector('.navbar-toggler');
+            
+            // If the navbar is expanded, simulate a click to close it
+            if (navbarToggler && navbarToggler.classList.contains('collapsed') === false) {
+              navbarToggler.click();
+            }
+          };
+
   return (
     <ul className="navbar-nav ms-auto">
       <li className="nav-item">
-        <NavLink to="/childcare" end className="nav-link">
+        <NavLink to="/childcare" end className="nav-link" onClick={closeNavbar}>
           Child Care
         </NavLink>
       </li>
       <li className="nav-item">
-        <NavLink to="/parties" end className="nav-link">
+        <NavLink to="/parties" end className="nav-link" onClick={closeNavbar}>
           Parties
         </NavLink>
       </li>
       <li className="nav-item">
-        <NavLink to="/playground" end className="nav-link">
+        <NavLink to="/playground" end className="nav-link" onClick={closeNavbar}>
           Playground
         </NavLink>
       </li>
       <li className="nav-item">
-        <NavLink to="/workshop" end className="nav-link">
+        <NavLink to="/workshop" end className="nav-link" onClick={closeNavbar}>
           Workshops
         </NavLink>
       </li>
       <li className="nav-item">
-        <NavLink to="/about" end className="nav-link">
+        <NavLink to="/about" end className="nav-link" onClick={closeNavbar}>
           About Us
         </NavLink>
       </li>
