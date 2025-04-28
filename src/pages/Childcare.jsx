013007bc231be68af065e4c39b3e4aace45a80de
Raza Hanif <raza_hanif@ymail.com>
2025-04-27 20:13:33 -0400

    Working on Frontend @ 8:13:32 PM

--- src/pages/Childcare.jsx
+++ src/pages/Childcare.jsx
@@ -1,7 +1,57 @@
 const Childcare = () => {
     return (
         <>
-            Childcare
+            <h4>Child Care - Preschool Program</h4>
+            <h5>24 Months - 6 Years Old</h5>
+
+            <p>
+                Our preschool program provides a nurturing, engaging, and stimulating enviroment designed to support your child's growth and development
+            </p>
+            <p>
+                Our curriculum is guided by the Ministry of Education's "How Does Learning Happen?" (HDLH) framework, focusing on the well-being, engagment expression and belonging of each individual child.
+            </p>
+
+            <br/>
+
+            <h5>Program Options - Full & Half Days</h5>
+            <li>Full Day 9:00 am - 3:00 pm</li>
+            <li>Half Day - Morning 9:00 am - 11:30 am</li>
+            <li>Half Day - Afternoon  12:30 pm - 3:00 pm</li>
+            
+            <br/>
+
+            <h5>What We Offer</h5>
+            <h6>Engaging Learning Experiences</h6>
+            <p>
+                Our program is designed to foster curiosity, exploration, and imagination. Educators create meaningful invitations based on children's interests, promoting problem-solving, self-regulation, and social-emotional development.
+            </p>
+            <h6>Interactive & Play-Based Learning</h6>
+            <p>
+                Children engage in group time, open play, indoor playground exploration, and hands-on activities that support learning through play.
+            </p>
+            <h6>Parent Communication & Learning Documentation</h6>
+            <p>
+                We believe in strong parent-educator partnerships. Children’s learning is documented and shared within the center and through a secure mobile app. Parents will receive daily updates about their child’s day. 
+            </p>
+            <h6>Nutritious Meals & Snacks</h6>
+            <p>
+                Delicious and balanced hot lunches and snacks are prepared on-site using a family-style dining approach. We accommodate dietary restrictions and maintain a peanut/nut-free environment. 
+
+            </p>
+            <h6>Mixed-Age Classrooms</h6>
+            <p>
+                Our mixed-age groupings create a family-like environment, allowing older children to develop leadership skills while younger children benefit from peer mentorship.
+            </p>
+            <h6>Our Educators</h6>
+            <p>
+                Our team of qualified Early Childhood Educators (ECEs) provides a safe, engaging, and responsive learning environment. They encourage choice, independence, and active play, fostering a love for learning from an early age.
+            </p>
+            <br/>
+
+            <p>
+                📍 Ready to join us? Contact us today to learn more about our preschool program or schedule a visit!
+            </p>
+
         </>
     );
 }
