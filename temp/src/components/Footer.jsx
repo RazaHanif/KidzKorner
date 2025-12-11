cf93356a51d16068b4580bec49e21c1fa49f0413
Raza Hanif <raza_hanif@ymail.com>
2025-12-11 12:29:21 -0500

    tailwind/shadcn refactor @ 12:29:20 PM

--- temp/src/components/Footer.jsx
+++ temp/src/components/Footer.jsx
@@ -1,33 +1,39 @@
-import { MapPin } from "lucide-react";
+import { Instagram, Mail, MapPin, Phone } from "lucide-react";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-center w-full bg-primary">
-      <div className="flex-1 flex flex-col justify-center p-2 gap-2">
-        <div className="text-xl flex justify-start items-start">
+    <div className="flex flex-col md:flex-row justify-center w-full bg-primary border-t border-chart-4">
+      <div className="flex-1 flex flex-col justify-center items-center gap-2">
+        <div className="text-xl flex justify-center items-center w-full">
           Contact Us
         </div>
         <div className="flex-1 flex flex-col justify-start items-start gap-2">
           <div>
             <a 
               href="tel:+19058784697"
-              className="hover:underline"
+              className="hover:underline flex flex-row gap-2"
             >
-              (905) 878-4697
+              <Phone />
+              <div>
+                (905) 878-4697
+              </div>
             </a>
           </div>
           <div>
             <a 
               href="mailto:info@kidzkornermilton.com"
-              className="hover:underline"
+              className="hover:underline flex flex-row gap-2"
             >
-              info@kidzkornermilton.com
+              <Mail />
+              <div>
+                info@kidzkornermilton.com
+              </div>
             </a>
           </div>
           <div>
             <a 
               href="https://maps.app.goo.gl/TcUQhJ58bgf6tME18"
-              className="hover:underline flex flex-row"
+              className="hover:underline flex flex-row gap-2"
             >
               <MapPin />
               <div>
@@ -38,16 +44,23 @@ const Footer = () => {
           <div>
             <a 
               href="https://www.instagram.com/kidz.kornermilton/"
-              className="hover:underline"
+              className="hover:underline flex flex-row gap-2"
             >
-              Follow us on Instagram
+              <Instagram />
+              <div>
+                Follow us on Instagram
+              </div>
             </a>
           </div>
         </div>
       </div>
 
-      <div className="flex-1 flex flex-col justify-center p-2 gap-2">
-        <div className="text-xl flex justify-start items-start">
+      <div className="border-r border-chart-4 md:flex hidden"></div>
+      <div className="border-t border-chart-4 md:hidden flex"></div>
+
+
+      <div className="flex-1 flex flex-col justify-center items-center gap-2">
+        <div className="text-xl flex justify-center items-center w-full">
           Child Care Hours
         </div>
         <div className="flex-1 flex flex-col justify-start items-start gap-2">
@@ -57,8 +70,11 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="flex-1 flex flex-col justify-center p-2 gap-2">
-        <div className="text-xl flex justify-start items-start">
+      <div className="border-r border-chart-4 md:flex hidden"></div>
+      <div className="border-t border-chart-4 md:hidden flex"></div>      
+
+      <div className="flex-1 flex flex-col justify-center items-center gap-2">
+        <div className="text-xl flex justify-center items-center w-full">
           After School Hours
         </div>
         <div className="flex-1 flex flex-col justify-start items-start gap-2">
@@ -68,9 +84,11 @@ const Footer = () => {
         </div>
       </div>
 
+      <div className="border-r border-chart-4 md:flex hidden"></div>
+      <div className="border-t border-chart-4 md:hidden flex"></div>
 
-      <div className="flex-1 flex flex-col justify-center p-2 gap-2">
-        <div className="text-xl flex justify-start items-start">
+      <div className="flex-1 flex flex-col justify-center items-center gap-2">
+        <div className="text-xl flex justify-center items-center w-full">
           Indoor Playground Hours
         </div>
         <div className="flex-1 flex flex-col justify-start items-start gap-2">
