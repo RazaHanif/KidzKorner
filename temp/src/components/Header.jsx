62c50d12a8f5d506f302bbfdca9a35742efd1c11
Raza Hanif <raza_hanif@ymail.com>
2025-12-11 10:27:22 -0500

    tailwind/shadcn refactor @ 10:27:22 AM

--- temp/src/components/Header.jsx
+++ temp/src/components/Header.jsx
@@ -15,19 +15,6 @@ const Header = () => {
             />
           </Link>
 
-          {/* Hamburger button
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button> */}
-
           {/* Collapsible links */}
           <div>
             <Navigation />
