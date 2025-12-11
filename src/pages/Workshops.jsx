7f952be30784dece7f01fffa0f0dd96352137c15
Raza Hanif <raza_hanif@ymail.com>
2025-12-11 14:37:17 -0500

    tailwind/shadcn refactor @ 2:37:17 PM

--- src/pages/Workshops.jsx
+++ src/pages/Workshops.jsx
@@ -3,28 +3,28 @@ import WorkshopForm from "../components/WorkshopFrom"
 
 const Workshops = () => {
     return (
-        <div className="flex flex-col justify-center items-center w-full">
+        <div className="flex flex-col justify-center items-center w-full gap-2">
             <div className="flex justify-center items-center w-3/4">
                 <WorkshopAccordion />
             </div>
-            <div>
+            <div className="flex md:flex-row flex-col justify-between items-center w-3/4">
                 <img 
-                    src='/K_K_39.jpg'
+                    src='/K_K_3.jpg'
                     alt='Kidz Korner Store Front'
                     className="w-3xs rounded-lg border-2 shadow-lg"
                 />
                 <img 
-                    src='/K_K_39.jpg'
+                    src='/K_K_11.jpg'
                     alt='Kidz Korner Store Front'
                     className="w-3xs rounded-lg border-2 shadow-lg"
                 />
                 <img 
-                    src='/K_K_39.jpg'
+                    src='/K_K_17.jpg'
                     alt='Kidz Korner Store Front'
                     className="w-3xs rounded-lg border-2 shadow-lg"
                 />
                 <img 
-                    src='/K_K_39.jpg'
+                    src='/K_K_32.jpg'
                     alt='Kidz Korner Store Front'
                     className="w-3xs rounded-lg border-2 shadow-lg"
                 />
