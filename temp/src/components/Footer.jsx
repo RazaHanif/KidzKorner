7d69547aade5f25b59d5ca31666e8dd9b0522de0
Raza Hanif <raza_hanif@ymail.com>
2025-12-11 12:41:34 -0500

    tailwind/shadcn refactor @ 12:41:33 PM

--- temp/src/components/Footer.jsx
+++ temp/src/components/Footer.jsx
@@ -5,6 +5,83 @@ const Footer = () => {
     <div className="flex flex-col md:flex-row justify-center w-full bg-primary border-t border-chart-4 p-4">
       <div className="flex-1 flex flex-col justify-center items-center gap-2">
         <div className="text-xl flex justify-center items-center w-full">
+          Child Care Hours
+        </div>
+        <div className="flex-1 flex flex-col justify-start items-start gap-2">
+          <div className="flex md:flex-col lg:flex-row flex-row md:gap-0 lg:gap-1 gap-1 justify-center items-center">
+            <div>
+              Monday to Friday
+            </div>
+            <div className="md:hidden lg:flex flex">
+              -
+            </div>
+            <div>
+              9:00am to 3:00pm
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="border-r border-chart-4 md:flex hidden m-2"></div>
+      <div className="border-t border-chart-4 md:hidden flex m-2"></div>      
+
+      <div className="flex-1 flex flex-col justify-center items-center gap-2">
+        <div className="text-xl flex justify-center items-center w-full">
+          After School Hours
+        </div>
+        <div className="flex-1 flex flex-col justify-start items-start gap-2">
+          <div className="flex md:flex-col lg:flex-row flex-row md:gap-0 lg:gap-1 gap-1 justify-center items-center">
+            <div>
+              Monday to Friday
+            </div>
+            <div className="md:hidden lg:flex flex">
+              -
+            </div>
+            <div>
+              3:00pm to 6:00pm
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="border-r border-chart-4 md:flex hidden m-2"></div>
+      <div className="border-t border-chart-4 md:hidden flex m-2"></div>
+
+      <div className="flex-1 flex flex-col justify-center items-center gap-2">
+        <div className="text-xl flex justify-center items-center w-full">
+          Indoor Playground Hours
+        </div>
+        <div className="flex-1 flex flex-col justify-start items-start gap-2">
+          <div className="flex md:flex-col lg:flex-row flex-row md:gap-0 lg:gap-1 gap-1 justify-center items-center">
+            <div>
+              Monday to Friday
+            </div>
+            <div className="md:hidden lg:flex flex">
+              -
+            </div>
+            <div>
+              3:00pm to 6:00pm
+            </div>
+          </div>
+          <div className="flex md:flex-col lg:flex-row flex-row md:gap-0 lg:gap-1 gap-1 justify-center items-center">
+            <div>
+              Saturday to Sunday
+            </div>
+            <div className="md:hidden lg:flex flex">
+              -
+            </div>
+            <div>
+              Parties Only
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="border-r border-chart-4 md:flex hidden m-2"></div>
+      <div className="border-t border-chart-4 md:hidden flex m-2"></div>
+
+      <div className="flex-1 flex flex-col justify-center items-center gap-2">
+        <div className="text-xl flex justify-center items-center w-full">
           Contact Us
         </div>
         <div className="flex-1 flex flex-col justify-start items-start gap-2">
@@ -55,83 +132,6 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="border-r border-chart-4 md:flex hidden"></div>
-      <div className="border-t border-chart-4 md:hidden flex"></div>
-
-
-      <div className="flex-1 flex flex-col justify-center items-center gap-2">
-        <div className="text-xl flex justify-center items-center w-full">
-          Child Care Hours
-        </div>
-        <div className="flex-1 flex flex-col justify-start items-start gap-2">
-          <div className="flex md:flex-col lg:flex-row flex-row md:gap-0 lg:gap-1 gap-1 justify-center items-center">
-            <div>
-              Monday to Friday
-            </div>
-            <div className="md:hidden lg:flex flex">
-              -
-            </div>
-            <div>
-              9:00am to 3:00pm
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="border-r border-chart-4 md:flex hidden"></div>
-      <div className="border-t border-chart-4 md:hidden flex"></div>      
-
-      <div className="flex-1 flex flex-col justify-center items-center gap-2">
-        <div className="text-xl flex justify-center items-center w-full">
-          After School Hours
-        </div>
-        <div className="flex-1 flex flex-col justify-start items-start gap-2">
-          <div className="flex md:flex-col lg:flex-row flex-row md:gap-0 lg:gap-1 gap-1 justify-center items-center">
-            <div>
-              Monday to Friday
-            </div>
-            <div className="md:hidden lg:flex flex">
-              -
-            </div>
-            <div>
-              3:00pm to 6:00pm
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="border-r border-chart-4 md:flex hidden"></div>
-      <div className="border-t border-chart-4 md:hidden flex"></div>
-
-      <div className="flex-1 flex flex-col justify-center items-center gap-2">
-        <div className="text-xl flex justify-center items-center w-full">
-          Indoor Playground Hours
-        </div>
-        <div className="flex-1 flex flex-col justify-start items-start gap-2">
-          <div className="flex md:flex-col lg:flex-row flex-row md:gap-0 lg:gap-1 gap-1 justify-center items-center">
-            <div>
-              Monday to Friday
-            </div>
-            <div className="md:hidden lg:flex flex">
-              -
-            </div>
-            <div>
-              3:00pm to 6:00pm
-            </div>
-          </div>
-          <div className="flex md:flex-col lg:flex-row flex-row md:gap-0 lg:gap-1 gap-1 justify-center items-center">
-            <div>
-              Saturday to Sunday
-            </div>
-            <div className="md:hidden lg:flex flex">
-              -
-            </div>
-            <div>
-              Parties Only
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
