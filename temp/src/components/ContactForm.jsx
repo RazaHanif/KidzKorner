6c9949490ba6ebd5eba5cc26f3829a0b0a683e5c
Raza Hanif <raza_hanif@ymail.com>
2025-12-11 12:56:54 -0500

    tailwind/shadcn refactor @ 12:56:53 PM

--- temp/src/components/ContactForm.jsx
+++ temp/src/components/ContactForm.jsx
@@ -57,7 +57,7 @@ const ContactForm = () => {
 
       {/* Show thank you message when form is submitted */}
       { submitted ? (
-        <div className="flex flex-col justify-center items-center w-lg text-black bg-card border-2 border-gray-200 rounded-lg">
+        <div className="flex flex-col justify-center items-center w-lg text-black bg-card border-2 rounded-lg">
           Form submitted, thank you!
         </div>
       ) : (
@@ -67,7 +67,7 @@ const ContactForm = () => {
           </div>
           <form 
             onSubmit={handleSubmit}
-            className='w-3/4 flex flex-col justify-center items-center gap-2 border-2 border-gray-200 bg-card rounded-lg pt-4'
+            className='w-3/4 flex flex-col justify-center items-center gap-2 border-2 bg-card rounded-lg pt-4'
           >
             <div 
               className="grid items-center gap-2 w-4/5 p-2"
@@ -134,6 +134,7 @@ const ContactForm = () => {
 
             <Button
               type='submit'
+              variant='outline'
               disabled={isSubmitting}
               className="mb-4"
             >
