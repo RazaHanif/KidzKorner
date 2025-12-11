9dbb8175fd362031c642b0644b2f86109747f961
Raza Hanif <raza_hanif@ymail.com>
2025-12-11 15:59:07 -0500

    done for the day hopefully its all good

--- temp/api/maps-url.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-    const API_KEY = process.env.GOOGLE_MAPS_KEY
-    const address = "Kidz Korner Milton"
-    const url = `https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=${encodeURIComponent(address)}`
-
-
-    res.status(200).sjon({ url })
-}
-
