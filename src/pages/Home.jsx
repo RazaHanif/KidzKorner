4b6cd5ddc1d5ca0a073f55a7bd7b27c15cf50715
Raza Hanif <raza_hanif@ymail.com>
2025-12-11 13:55:14 -0500

    tailwind/shadcn refactor @ 1:55:14 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,41 +5,27 @@ const Home = () => {
             </div>
 
             <div className="flex flex-col justify-center items-center gap-2 w-3/4">
-                <div className="text-2xl">
-                    Alittle about us.
-                </div>
                 <div className="flex flex-row items-center gap-4">
                     <img 
                     src='/K_K_39.jpg'
-                    alt='Home'
-                    className="w-xs rounded-lg"
+                    alt='Kidz Korner Store Front'
+                    className="w-3xs rounded-lg border-2 shadow-lg"
                     />
                     <div>
                         Kidz Korner is a family-owned business located in the heart of Milton, Ontario. 
-                        Our commitment to providing loving, nurturing care has earned us the trust of many families and community resources, creating a multi-generational legacy.
+                        Our commitment to providing loving, nurturing care has earned us the trust of many families and community resources, creating a multi-generational legacy. We believe in child-led learning. By observing, engaging with, and reflecting on each child's experiences, we create a learning environment that is both inclusive and adaptable to each child's needs.
                     </div>
                 </div>
                 <div className="flex flex-row items-center gap-4">
                     <div>
-                        At Kidz Korner, we believe in child-led learning.
-                        By observing, engaging with, and reflecting on each child's experiences, we create a learning environment that is both inclusive and adaptable to each child's needs.
-                        Our dedicated educators work closely with families to foster a strong sense of connection and support.
+                        Our dedicated educators work closely with families to foster a strong sense of connection and support. With decades of experience in early childhood education, we are proud to help families navigate the important developmental milestones of the early years. We are here to support your child's growth, development, and well-being every step of the way.
                     </div>
                     <img 
                     src='/K_K_39.jpg'
-                    alt='Home'
-                    className="w-xs rounded-lg"
+                    alt='Kidz Korner Store Front'
+                    className="w-3xs rounded-lg border-2 shadow-lg"
                     />
-                </div>
-                <div className="flex flex-row items-center gap-4">
-                    <img 
-                    src='/K_K_39.jpg'
-                    alt='Home'
-                    className="w-xs rounded-lg"
-                    />
-                    <div>
-                        With decades of experience in early childhood education, we are proud to help families navigate the important developmental milestones of the early years. We are here to support your child's growth, development, and well-being every step of the way.q
-                    </div>
+                    <div></div>
                 </div>
             </div>
         </div>
