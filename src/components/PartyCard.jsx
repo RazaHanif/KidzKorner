30502a3c1512ff518d85afd1dfb2fba3160faa4a
Raza Hanif <raza_hanif@ymail.com>
2026-02-02 11:03:28 -0500

    2026 Q1 updates @ 11:03:28 AM

--- src/components/PartyCard.jsx
+++ src/components/PartyCard.jsx
@@ -11,7 +11,7 @@ import {
 const PartyCard = ({ title, price, desc }) => {
     return (
         <Card className="w-3/4">
-            <CardHeader className="bg-purple-500">
+            <CardHeader>
                 <CardTitle className="p-2 bg-accent flex justify-center items-center rounded-md">
                     { title }
                 </CardTitle>
