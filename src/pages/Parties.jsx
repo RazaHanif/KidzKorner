7aad9fe72b21073464a6195ba363f6b9c4438bd7
Raza Hanif <raza_hanif@ymail.com>
2026-02-02 12:05:00 -0500

    2026 Q1 updates @ 12:04:59 PM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -5,7 +5,7 @@ const Parties = () => {
     return (
         <div className="flex flex-col justify-center items-center w-full gap-2">
             <div className="flex flex-col justify-center items-center w-full">
-                <div className="text-3xl font-bold text-center">
+                <div className="text-3xl font-semibold text-center">
                     Host Your Child's Party at Kidz Korner
                 </div>
             </div>
@@ -23,14 +23,14 @@ const Parties = () => {
                     desc={"Enjoy exclusive venue access and full use of our indoor playspace for your guests!"}
                     bullet={[
                         <li key={1}>
-                            <span className="font-bold">3.5</span> Hours of Private Access
+                            <span className="font-semibold">3.5</span> Hours of Private Access
                         </li>,
                         <li key={2}>
                             Complimentary Tea & Coffee Station for Adults
                         </li>,
                         <li key={3}>
                             Bring your own food at no additional and use out kithcen space
-                            <div className="font-bold text-xs">Strict Nut-Free Poilcy for childrens safety</div>
+                            <div className="font-semibold text-xs">Strict Nut-Free Poilcy for childrens safety</div>
                         </li>,
                         <li key={4}>
                             Bring your own decorations to transform the space
@@ -46,11 +46,11 @@ const Parties = () => {
                     price={"550"} 
                     desc={"Elevate your experience with extended time and food inclusions!"}
                     bullet={[
-                        <div key={1} className="font-bold text-xs">
+                        <div key={1} className="font-semibold text-xs">
                             Includes everything in the Tiny Tornado Package plus
                         </div>,
                         <li key={2}>
-                            <span className="font-bold">4.5</span> Hours of Private Access
+                            <span className="font-semibold">4.5</span> Hours of Private Access
                         </li>,
                         <li key={3}>
                             2 Jumbo Cheese Pizza's
@@ -63,11 +63,11 @@ const Parties = () => {
                     price={"750"} 
                     desc={"The ultimate private party package with extended time, decor upgrades and interactive party features!"}
                     bullet={[
-                        <div key={1} className="font-bold text-xs">
+                        <div key={1} className="font-semibold text-xs">
                             Includes everything in the Tiny Tornado Package & the Platnium Play Package plus
                         </div>,
                         <li key={2}>
-                            <span className="font-bold">5.5</span> Hours of Private Access
+                            <span className="font-semibold">5.5</span> Hours of Private Access
                         </li>,
                         <li key={3}>
                             DIY Ballon Arch & Table Cloths provided for self-setup
