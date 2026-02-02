8ad7bf241d0d1c83d62e4c2d92e727ec77518300
Raza Hanif <raza_hanif@ymail.com>
2026-02-02 11:59:24 -0500

    2026 Q1 updates @ 11:59:24 AM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -10,68 +10,67 @@ const Parties = () => {
                 </div>
             </div>
 
-            <PartyCard 
-                title={"Tiny Tornado Package"} 
-                price={"395"} 
-                desc={"Enjoy exclusive venue access and full use of our indoor playspace for your guests!"}
-                bullet={[
-                    <li key={1}>
-                        <span className="font-bold">3.5</span> Hours of Private Access
-                    </li>,
-                    <li key={2}>
-                        Complimentary Tea & Coffee Station for Adults
-                    </li>,
-                    <li key={3}>
-                        Bring your own food at no additional and use out kithcen space
-                        <div className="font-bold text-xs">Strict Nut-Free Poilcy for childrens safety</div>
-                    </li>,
-                    <li key={4}>
-                        Bring your own decorations to transform the space
-                    </li>,
-                    <li key={5}>
-                        Bring up to 50 guests
-                    </li>
-                ]}
-            />
+            <div className="flex md:flex-row">
+                <PartyCard 
+                    title={"Tiny Tornado Package"} 
+                    price={"395"} 
+                    desc={"Enjoy exclusive venue access and full use of our indoor playspace for your guests!"}
+                    bullet={[
+                        <li key={1}>
+                            <span className="font-bold">3.5</span> Hours of Private Access
+                        </li>,
+                        <li key={2}>
+                            Complimentary Tea & Coffee Station for Adults
+                        </li>,
+                        <li key={3}>
+                            Bring your own food at no additional and use out kithcen space
+                            <div className="font-bold text-xs">Strict Nut-Free Poilcy for childrens safety</div>
+                        </li>,
+                        <li key={4}>
+                            Bring your own decorations to transform the space
+                        </li>,
+                        <li key={5}>
+                            Bring up to 50 guests
+                        </li>
+                    ]}
+                />
 
-            <PartyCard 
-                title={"Platnium Play Package"} 
-                price={"550"} 
-                desc={"Elevate your experience with extended time and food inclusions!"}
-                bullet={[
-                    <div key={1} className="font-bold text-xs">
-                        Includes everything in the Tiny Tornado Package plus
-                    </div>,
-                    <li key={2}>
-                        <span className="font-bold">4.5</span> Hours of Private Access
-                    </li>,
-                    <li key={3}>
-                        2 Jumbo Cheese Pizza's
-                    </li>,
-                ]}
-            />
-            
-            <PartyCard 
-                title={"VIP Package"} 
-                price={"750"} 
-                desc={"The ultimate private party package with extended time, decor upgrades and interactive party features!"}
-                bullet={[
-                    <div key={1} className="font-bold text-xs">
-                        Includes everything in the Tiny Tornado Package & the Platnium Play Package plus
-                    </div>,
-                    <li key={2}>
-                        <span className="font-bold">5.5</span> Hours of Private Access
-                    </li>,
-                    <li key={3}>
-                        DIY Ballon Arch & Table Cloths provided for self-setup
-                    </li>,
-                    <li key={4}>
-                        Kidz Activity Table
-                    </li>,
-                ]}
-            />
-            <div>
+                <PartyCard 
+                    title={"Platnium Play Package"} 
+                    price={"550"} 
+                    desc={"Elevate your experience with extended time and food inclusions!"}
+                    bullet={[
+                        <div key={1} className="font-bold text-xs">
+                            Includes everything in the Tiny Tornado Package plus
+                        </div>,
+                        <li key={2}>
+                            <span className="font-bold">4.5</span> Hours of Private Access
+                        </li>,
+                        <li key={3}>
+                            2 Jumbo Cheese Pizza's
+                        </li>,
+                    ]}
+                />
                 
+                <PartyCard 
+                    title={"VIP Package"} 
+                    price={"750"} 
+                    desc={"The ultimate private party package with extended time, decor upgrades and interactive party features!"}
+                    bullet={[
+                        <div key={1} className="font-bold text-xs">
+                            Includes everything in the Tiny Tornado Package & the Platnium Play Package plus
+                        </div>,
+                        <li key={2}>
+                            <span className="font-bold">5.5</span> Hours of Private Access
+                        </li>,
+                        <li key={3}>
+                            DIY Ballon Arch & Table Cloths provided for self-setup
+                        </li>,
+                        <li key={4}>
+                            Kidz Activity Table
+                        </li>,
+                    ]}
+                />
             </div>
 
             <div className="flex flex-col justify-center items-center w-3/4">
