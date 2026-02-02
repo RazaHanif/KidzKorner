9d0d1b4d2e37aa9e5ec85c431439bf6fabb465e3
Raza Hanif <raza_hanif@ymail.com>
2026-02-02 11:12:50 -0500

    2026 Q1 updates @ 11:12:50 AM

--- src/components/PartyCard.jsx
+++ src/components/PartyCard.jsx
@@ -20,8 +20,11 @@ const PartyCard = ({ title, price, desc,  }) => {
                 </CardDescription>
             </CardHeader>
             <CardContent>
+                <div>
+                    
+                </div>
                 { desc }
-                
+
             </CardContent>
         </Card>
     )
