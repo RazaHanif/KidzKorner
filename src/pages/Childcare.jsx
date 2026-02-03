2a50ee60872ff847922043226429b67802b2996a
Raza Hanif <raza_hanif@ymail.com>
2026-02-02 22:40:40 -0500

    2026 Q1 updates @ 10:40:39 PM

--- src/pages/Childcare.jsx
+++ src/pages/Childcare.jsx
@@ -23,7 +23,7 @@ const Childcare = () => {
                     Our curriculum is guided by the Ministry of Education's <span className="italic">"How Does Learning Happen?" (HDLH)</span> framework, focusing on the well-being, engagment expression and belonging of each individual child.
                 </div>
                 <div className="flex flex-col justify-center md:w-1/2 w-full m-4">
-                    Our experienced and caring childcare educators strive to build strong relationships with children and families, ensuring individualized attention and consistent communication. With high qua
+                    Our experienced and caring childcare educators strive to build strong relationships with children and families, ensuring individualized attention and consistent communication. With high quality programs, qualified educators and a vibrant learning space 
                 </div>
             </div>
 
