eebe417a39bef909132d6e44726d3b727e13accb
Raza Hanif <raza_hanif@ymail.com>
2026-02-02 22:40:24 -0500

    2026 Q1 updates @ 10:40:24 PM

--- src/pages/Childcare.jsx
+++ src/pages/Childcare.jsx
@@ -22,8 +22,8 @@ const Childcare = () => {
                     Kidz Korner is a licensed child care centre dedicated to providing high quality early learning and care. Our toddler, preschool, & before/after school programs provides a nurturing, engaging, and stimulating enviroment designed to support your child's growth and development
                     Our curriculum is guided by the Ministry of Education's <span className="italic">"How Does Learning Happen?" (HDLH)</span> framework, focusing on the well-being, engagment expression and belonging of each individual child.
                 </div>
-                <div>
-                    Our experienced and caring childcare educators strive to build strong relationships with children and families, ensuring individualized attention and consistent communication
+                <div className="flex flex-col justify-center md:w-1/2 w-full m-4">
+                    Our experienced and caring childcare educators strive to build strong relationships with children and families, ensuring individualized attention and consistent communication. With high qua
                 </div>
             </div>
 
