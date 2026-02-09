a3d83a05898cd729b6e1e4b23f670c0970d4a9af
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 13:19:59 -0500

    2026 Q1 updates @ 1:19:59 PM

--- src/components/WorkshopCarousel.jsx
+++ src/components/WorkshopCarousel.jsx
@@ -15,16 +15,43 @@ import {
 
 
 const WorkshopCarousel = () => {
+
+    const workshopCard = (title, price, content) => {
+        return (
+            <Card>
+                <CardHeader>
+                    <CardTitle className="p-2 bg-primary text-primary-foreground flex justify-center items-center rounded-md text-lg">
+                        { title }
+                    </CardTitle>
+                    <CardDescription className="p-2 bg-accent flex justify-center items-center rounded-md text-bold text-lg">
+                    <span className="text-xs text-center">
+                        {price}
+                    </span>
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    { content }
+                </CardContent>
+            </Card>
+        )
+    }
     return (
         <Carousel>
             <CarouselContent>
                 <CarouselItem>
+                    { workshopCard(
+                        
+                    )}
                 </CarouselItem>
                 <CarouselItem>
-                    ...
+                    { workshopCard(
+                        
+                    )}
                 </CarouselItem>
                 <CarouselItem>
-                    ...
+                    { workshopCard(
+                        
+                    )}
                 </CarouselItem>
             </CarouselContent>
             <CarouselPrevious />
