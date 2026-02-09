11486444a3ffe86376d6f1e89f73d82991405da3
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 11:00:04 -0500

    2026 Q1 updates @ 11:00:04 AM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
                     <img 
                         src='/K_K_39.jpg'
                         alt='Kidz Korner Store Front'
-                        className="w-3xs rounded-lg border-2 shadow-lg"
+                        className="w-xs rounded-lg border-2 shadow-lg"
                         loading="lazy"
                     />
                     <div className="md:w-1/2 w-full flex justify-center items-center">
