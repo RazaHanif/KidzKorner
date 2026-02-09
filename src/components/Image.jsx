112c045cb734816404cf8f22d1595570027cd0f9
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 15:58:50 -0500

    2026 Q1 updates @ 3:58:50 PM

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,1 +1,12 @@
-const Image = ({})
+const Image = ({src, alt}) => {
+    return (
+        <img 
+            src={src}
+            alt={alt}
+            className="w-3xs rounded-lg border-2 shadow-lg"
+            loading="lazy"
+        />
+    )
+}
+
+export default Image
