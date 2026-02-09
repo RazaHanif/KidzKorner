fd41f0ac3031152eeec00750e350ef531341b3e5
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 14:58:41 -0500

    2026 Q1 updates @ 2:58:41 PM

--- src/components/FloatingButton.jsx
+++ src/components/FloatingButton.jsx
@@ -6,7 +6,7 @@ const FloatingButton = ({ action = () => console.log('btn'), ...props }) => {
     return (
         <Button
             variant="btn"
-            className="fixed bottom-22 right-5 h-10 w-10 rounded-lg shadow-lg bg-primary text-primary-foreground flex items-center justify-center"
+            className="absolute bottom-22 right-5 h-10 w-10 rounded-lg shadow-lg bg-primary text-primary-foreground flex items-center justify-center"
             onClick={ action }
             {...props}
         >
