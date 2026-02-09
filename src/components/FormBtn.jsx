54974afe9dff465a5e66f26cdc9211f4f3ec4f41
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 14:55:04 -0500

    2026 Q1 updates @ 2:55:03 PM

--- src/components/FormBtn.jsx
+++ src/components/FormBtn.jsx
@@ -10,7 +10,7 @@ import FloatingButton from "@/components/FloatingButton";
 import { useState } from "react";
 
 // Should take in a list of people to message and display, clicking on them takes you to that chat or creates a new chat
-const FormBtn = (title, form) => {
+const FormBtn = ({title, form}) => {
 
     const [ open, setOpen ] = useState(false)
 
