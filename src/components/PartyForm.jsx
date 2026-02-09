524fd1ba840d775033b3a2e305a6b7c09c1e5347
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 16:47:29 -0500

    2026 Q1 updates @ 4:47:29 PM

--- src/components/PartyForm.jsx
+++ src/components/PartyForm.jsx
@@ -61,24 +61,13 @@ const PartyForm = () => {
 
   };
 
-  return (
-    <div className="flex flex-col justify-center items-center w-full p-2">
-
-      {/* Show thank you message when form is submitted */}
-      { submitted ? (
-        <div className="flex flex-col justify-center items-center w-lg text-black bg-card border-2 rounded-lg">
-          Form submitted, thank you!
-        </div>
-      ) : (
-        <div className='flex flex-col justify-center items-center w-lg'>
-          <div className='flex flex-col justify-center items-center w-full text-2xl font-bold text-black'>
-              Party Inquiry Form
-          </div>
-          <form 
-            onSubmit={handleSubmit}
-            className='w-3/4 flex flex-col justify-center items-center gap-2 border-2 bg-card rounded-lg pt-4'
-          >
-            <div 
+  <FormLayout 
+    title={"Party Inquiry Form"}
+    handleSubmit={handleSubmit}
+    submitted={submitted}
+    form={
+      <>
+      <div 
               className="grid items-center gap-2 w-4/5 p-2"
             >
               <Label 
@@ -220,11 +209,173 @@ const PartyForm = () => {
             >
               Submit
             </Button>
-          </form>
-        </div>
-      )}
-    </div>
-  );
+      </>
+    }
+  />
+  // return (
+  //   <div className="flex flex-col justify-center items-center w-full p-2">
+
+  //     {/* Show thank you message when form is submitted */}
+  //     { submitted ? (
+  //       <div className="flex flex-col justify-center items-center w-lg text-black bg-card border-2 rounded-lg">
+  //         Form submitted, thank you!
+  //       </div>
+  //     ) : (
+  //       <div className='flex flex-col justify-center items-center w-lg'>
+  //         <div className='flex flex-col justify-center items-center w-full text-2xl font-bold text-black'>
+  //             Party Inquiry Form
+  //         </div>
+  //         <form 
+  //           onSubmit={handleSubmit}
+  //           className='w-3/4 flex flex-col justify-center items-center gap-2 border-2 bg-card rounded-lg pt-4'
+  //         >
+  //           <div 
+  //             className="grid items-center gap-2 w-4/5 p-2"
+  //           >
+  //             <Label 
+  //               htmlFor="name"
+  //             >
+  //               Name
+  //             </Label>
+  //             <Input
+  //               type="text"
+  //               placeholder="Spongebob Squarepants"
+  //               id="name"
+  //               name="name"
+  //               value={formData.name}
+  //               onChange={handleChange}
+  //               required
+  //             />
+  //           </div>
+
+  //           <div 
+  //             className="grid items-center gap-2 w-4/5 p-2"
+  //           >
+  //             <Label 
+  //               htmlFor="email"
+  //             >
+  //               Email
+  //             </Label>
+  //             <Input
+  //               type="email"
+  //               placeholder="squarepants@krustykrab.com"
+  //               id="email"
+  //               name="email"
+  //               value={formData.email}
+  //               onChange={handleChange}
+  //               required
+  //             />
+  //           </div>
+
+  //           <div 
+  //             className="grid items-center gap-2 w-4/5 p-2"
+  //           >
+  //             <Label 
+  //               htmlFor="phone"
+  //             >
+  //               Phone Number
+  //             </Label>
+  //             <Input
+  //               type="tel"
+  //               placeholder="905-878-4697"
+  //               id="phone"
+  //               name="phone"
+  //               value={formData.phone}
+  //               onChange={handleChange}
+  //               required
+  //             />
+  //           </div>
+
+  //           <div className="grid items-center gap-2 w-4/5 p-2">
+  //             <Label 
+  //               htmlFor="partyDate" 
+  //             >
+  //               Party Date
+  //             </Label>
+  //             <Popover open={openPartyDate} onOpenChange={setOpenPartyDate}>
+  //               <PopoverTrigger asChild>
+  //                 <Button
+  //                   variant="outline"
+  //                   id="partyDate"
+  //                   name="partyDate"
+  //                   value={formData.partyDate}
+  //                   onChange={handleChange}
+  //                   className="w-full justify-between font-normal"
+  //                 >
+  //                   {formData.partyDate ? formData.partyDate : "Select date"}
+  //                   <ChevronDownIcon />
+  //                 </Button>
+  //               </PopoverTrigger>
+  //               <PopoverContent className="w-full p-0" side="bottom" align="start" avoidCollisions={false} forceMount>
+  //                 <Calendar
+  //                   mode="single"
+  //                   name="partyDate"
+  //                   selected={formData.partyDate ? new Date(formData.partyDate) : undefined}
+  //                   onSelect={(date) => {
+  //                     if (date instanceof Date && !isNaN(date.getTime())) {
+  //                       setFormData((prev) => ({
+  //                         ...prev,
+  //                         partyDate: date.toISOString().split('T')[0],
+  //                       }));
+  //                       setOpenPartyDate(false);
+  //                     }
+  //                   }}
+  //                   captionLayout="dropdown"
+  //                 />
+  //               </PopoverContent>
+  //             </Popover>
+  //           </div>
+
+  //           <div 
+  //             className="grid items-center gap-2 w-4/5 p-2"
+  //           >
+  //             <Label 
+  //               htmlFor="numOfGuests"
+  //             >
+  //               Number of Guests
+  //             </Label>
+  //             <Input
+  //               type="number"
+  //               placeholder="25"
+  //               id="numOfGuests"
+  //               name="numOfGuests"
+  //               value={formData.numOfGuests}
+  //               onChange={handleChange}
+  //               required
+  //             />
+  //           </div>
+
+  //           <div 
+  //             className="grid items-center gap-2 w-4/5 p-2"
+  //           >
+  //             <Label 
+  //               htmlFor="message"
+  //             >
+  //               Tell us more...
+  //             </Label>
+  //             <Input
+  //               type='text'
+  //               id="message"
+  //               name="message"
+  //               value={formData.message}
+  //               onChange={handleChange}
+  //               required
+  //             />
+  //           </div>
+
+  //           <Button
+  //             type='submit'
+  //             variant='outline'
+  //             disabled={isSubmitting}
+  //             className="mb-4"
+  //           >
+  //             Submit
+  //           </Button>
+  //         </form>
+  //       </div>
+  //     )}
+  //   </div>
+  // );
 };
 
 export default PartyForm;
