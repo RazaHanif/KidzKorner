21214c05939fe84fe30fc372a1ef988ff7eab698
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 15:32:00 -0500

    2026 Q1 updates @ 3:32:00 PM

--- src/pages/Childcare.jsx
+++ src/pages/Childcare.jsx
@@ -106,6 +106,34 @@ const Childcare = () => {
                     </div>
                 </div>
             </div>
+
+                        <div className="flex md:flex-row flex-col justify-evenly items-center w-3/4">
+                <Card>
+                    <CardHeader className="hidden">
+                        <CardTitle className="p-2 bg-primary text-primary-foreground flex justify-center items-center rounded-md text-lg">
+                            Childcare
+                        </CardTitle>
+                        <CardDescription className="p-2 bg-accent hidden rounded-md text-bold text-lg">
+                            Childcare
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="flex flex-col justify-center items-center w-full text-center">
+                            <span>
+                                Kidz Korner is a licensed child care centre dedicated to providing high quality early learning and care. We offer toddler, preschool, & before/after school programs. Our curriculum is guided by the
+                            </span>
+                            <span className="italic self-center">
+                                Ministry of Education's "How Does Learning Happen?" (HDLH) 
+                            </span>
+                            <span>
+                                framework. Focusing on the well-being, engagement, expression and belonging of each individual child. 
+                                Our educators provide a high quality programs and a vibrant learning space, including our most unique feature, the indoor playground.
+                                We provide a variety of care options to meet each families unique needs.
+                            </span>
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
             
             <div className="flex md:flex-row flex-col justify-around items-center w-3/4 gap-4">
                     <img 
