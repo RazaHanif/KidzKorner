f2ae7e6ddd3d363dcf89b3699bc4cb705d0fd7f0
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 18:16:39 -0500

    2026 Q1 updates @ 6:16:39 PM

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -10,6 +10,7 @@ const NotFound = () => {
       </div>
 
       <div className="w-full flex flex-col justify-center items-center gap-4 text-muted-foreground">
+        <div className="text-6xl font-[ui]">🎠</div>
         <div>
             <img src='/slide.svg' alt='slide' className="w-20"/>
         </div>
