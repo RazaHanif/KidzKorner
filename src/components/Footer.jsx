639a7f617a80c7930f7c41d186b61af71782cbe2
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 18:37:37 -0500

    2026 Q1 updates @ 6:37:37 PM

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -59,7 +59,7 @@ const Footer = () => {
         <div className="text-xl flex justify-center items-center w-full border-2 border-red-500">
           Indoor Playground Hours
         </div>
-        <div className="w-3/4 md:w-full border-2 border-green-500">
+        <div className="w-3/4 md:w-full border-2 border-green-500 flex-1">
           <Table className="w-full border-none">
             <TableBody>
               <TableRow className="whitespace-normal wrap-break-word border-none">
