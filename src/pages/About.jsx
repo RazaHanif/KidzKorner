9a288429f22e170c3ba70086285704878b5d3cf9
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 17:28:10 -0500

    2026 Q1 updates @ 5:28:10 PM

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,17 +7,17 @@ const About = () => {
 
     return (
         <div className="flex flex-col justify-center items-center gap-2 w-full text-justify">
-            <div className="flex flex-col justify-center items-center w-3/4 m-4 gap-4 border">
+            <div className="flex md:flex-row flex-col justify-center items-center w-3/4 m-4 gap-4 border">
                 <div className="border">
                     <ContactUsTable />
                 </div>
 
-                {/* <div>
+                <div>
                     <Image 
                         src='/K_K_2.jpg'
                         alt='Kid making silly face'
                     />
-                </div> */}
+                </div>
 
                 <div className="border">
                     <HoursTimeTable />
