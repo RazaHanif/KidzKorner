3953708d9586f430d31a3598f45be1da6609883f
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 14:52:40 -0500

    2026 Q1 updates @ 2:52:39 PM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -5,7 +5,9 @@ import WorkshopFormBtn from "../components/FormBtn";
 const Test = () => {
     return (
         <div className="flex flex-col justify-center items-center w-full gap-2">
-            <WorkshopFormBtn />
+            <WorkshopFormBtn 
+                title={"Workshop Form"}
+            />
         </div>
     );
 }
