c1e7be8827c73840c968673571d7ecdae109891b
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 14:38:44 -0500

    2026 Q1 updates @ 2:38:44 PM

--- src/pages/Workshops.jsx
+++ src/pages/Workshops.jsx
@@ -1,14 +1,17 @@
 import WorkshopAccordion from "@/components/WorkshopAccordion";
 import WorkshopForm from "@/components/WorkshopFrom"
+import WorkshopCarousel from "../components/WorkshopCarousel";
 
 const Workshops = () => {
     return (
         <div className="flex flex-col justify-center items-center w-full gap-2">
-            <div className="flex justify-center items-center w-3/4 mb-4">
-                <WorkshopAccordion />
-
+            <div>
+                
             </div>
-            
+            <div className="flex justify-center items-center w-3/4 mb-4">
+                <WorkshopCarousel />
+            </div>
+
             <div className="flex md:flex-row flex-col justify-between items-center w-3/4 gap-2">
                 <img 
                     src='/K_K_11.jpg'
