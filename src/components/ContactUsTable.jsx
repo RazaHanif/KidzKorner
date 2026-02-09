08642c353a997b197a7bc502ad868d7a5498542f
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 16:56:57 -0500

    2026 Q1 updates @ 4:56:57 PM

---
+++ src/components/ContactUsTable.jsx
@@ -0,0 +1,50 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+} from "@/components/ui/table"
+
+const TimeTable = () => {
+    const timetable = [
+        {
+            session: "Before School",
+            time: "7:30am - 9:00am"
+        },
+        {
+            session: "Full Day",
+            time: "9:00am - 3:00pm"
+        },
+        {
+            session: "Half Day - Morning",
+            time: "9:00am - 11:30am"
+        },
+        {
+            session: "Half Day - Afternoon",
+            time: "12:30am - 3:00pm"
+        },
+        {
+            session: "After School",
+            time: "3:00pm - 6:00pm"
+        },
+    ]
+
+    return (
+        <Table>
+            <TableBody>
+                {timetable.map((item) => (
+                    <TableRow key={item}>
+                        <TableCell className='font-semibold'>
+                            {item.session}
+                        </TableCell>
+                        <TableCell>
+                            {item.time}
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    )
+}
+
+export default TimeTable;
