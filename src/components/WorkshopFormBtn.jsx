f56cf992444ef9704a32886d746bde4ede262757
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 14:42:24 -0500

    2026 Q1 updates @ 2:42:24 PM

---
+++ src/components/WorkshopFormBtn.jsx
@@ -0,0 +1,37 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import FloatingButton from "@/components/FloatingButton";
+import { useState } from "react";
+
+// Should take in a list of people to message and display, clicking on them takes you to that chat or creates a new chat
+const WorkshopFormBtn = () => {
+
+    const [ open, setOpen ] = useState(false)
+
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
+                <FloatingButton />
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        Create a new chat!
+                    </DialogTitle>
+                </DialogHeader>
+                    [ List of people to message ]
+                <DialogFooter>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog> 
+    )
+
+}
+
+export default WorkshopFormBtn 
