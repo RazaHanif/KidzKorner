f2e841e77017108bada4e40a04417658c4a49d0f
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 18:05:05 -0500

    2026 Q1 updates @ 6:05:05 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
                         loading="lazy"
                     />
                     <div className="flex flex-col justify-center items-center w-full p-4">
-                        <div className="hidden md:block text-3xl font-bold self-center">
+                        <div className="hidden md:block text-3xl font-bold self-center font-serif">
                             Welcome to Kidz Korner
                         </div>
                         
