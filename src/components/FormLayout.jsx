dfca180de5ab638a851f1f54859a0a404afe731f
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 16:35:28 -0500

    2026 Q1 updates @ 4:35:28 PM

--- src/components/FormLayout.jsx
+++ src/components/FormLayout.jsx
@@ -1,7 +1,6 @@
 const FormLayout = ({ title, form, handleSubmit, submitted }) => {
     return (
         <div className="flex flex-col justify-center items-center w-full">
-            {/* Show thank you message when form is submitted */}
             { submitted ? (
                 <div 
                     className="flex flex-col justify-center items-center w-lg text-black bg-card border-2 rounded-lg"
