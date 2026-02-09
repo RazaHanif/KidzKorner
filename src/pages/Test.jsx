07ac1e40bfd132b81fcc4a8ddb00fb125a353b35
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 13:14:54 -0500

    2026 Q1 updates @ 1:14:54 PM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,9 +1,10 @@
+import WorkshopCarousel from "../components/WorkshopCarousel";
 
 
 const Test = () => {
     return (
         <div className="flex flex-col justify-center items-center w-full gap-2">
-            Test
+            <WorkshopCarousel />
         </div>
     );
 }
