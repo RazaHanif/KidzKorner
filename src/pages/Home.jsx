645817794bc4403d81ff88247e2b799e05882fcc
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 11:04:29 -0500

    2026 Q1 updates @ 11:04:29 AM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
                         className="w-3xs md:w-xs rounded-lg border-2 shadow-lg"
                         loading="lazy"
                     />
-                    <div className="md:w-1/2 w-full flex justify-center items-center">
+                    <div className="md:w-1/2 w-full flex justify-center items-center p-4 border flex-1">
                         Kidz Korner is a family-owned business located in the heart of Milton, Ontario.
                         We offer license, high quality child care for toddlers and preschoolers along with before and after school programs and licensed school age camps.
                         Camps are offered during PA / PD days, March / Summer / Winter breaks.
