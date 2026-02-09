98c36b73f749d0ded4839755e8f0f95740dc5184
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 17:02:32 -0500

    2026 Q1 updates @ 5:02:32 PM

--- src/components/ContactUsTable.jsx
+++ src/components/ContactUsTable.jsx
@@ -11,23 +11,18 @@ import {
 const ContactUsTable = () => {
     const timetable = [
         {
-            session: "Before School",
             time: "7:30am - 9:00am"
         },
         {
-            session: "Full Day",
             time: "9:00am - 3:00pm"
         },
         {
-            session: "Half Day - Morning",
             time: "9:00am - 11:30am"
         },
         {
-            session: "Half Day - Afternoon",
             time: "12:30am - 3:00pm"
         },
         {
-            session: "After School",
             time: "3:00pm - 6:00pm"
         },
     ]
