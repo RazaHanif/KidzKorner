81f4e5f04d5ae9dd218a42895b41863fc00b0d87
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 13:18:03 -0500

    2026 Q1 updates @ 1:18:03 PM

--- src/components/WorkshopCarousel.jsx
+++ src/components/WorkshopCarousel.jsx
@@ -19,21 +19,6 @@ const WorkshopCarousel = () => {
         <Carousel>
             <CarouselContent>
                 <CarouselItem>
-                    <Card>
-                        <CardHeader>
-                            <CardTitle className="p-2 bg-primary text-primary-foreground flex justify-center items-center rounded-md text-lg">
-                                Title
-                            </CardTitle>
-                            <CardDescription className="p-2 bg-accent flex justify-center items-center rounded-md text-bold text-lg">
-                                <span className="text-xs text-center">
-                                    $Price
-                                </span>
-                            </CardDescription>
-                        </CardHeader>
-                        <CardContent>
-                            Images Go Here
-                        </CardContent>
-                    </Card>
                 </CarouselItem>
                 <CarouselItem>
                     ...
