cf7effdfaaace8513407cc3932ec62e55ff4a433
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 11:23:31 -0500

    2026 Q1 updates @ 11:23:31 AM

--- src/pages/Camp.jsx
+++ src/pages/Camp.jsx
@@ -50,7 +50,6 @@ const Camp = () => {
                                 All materials included
                             </li>
                         </ul>
-        
                     </CardContent>
                 </Card>
             </div>
