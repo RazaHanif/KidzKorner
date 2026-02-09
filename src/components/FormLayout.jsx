be8f3d7238ef7acd7f927df01d78453176e98e83
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 16:35:17 -0500

    2026 Q1 updates @ 4:35:17 PM

--- src/components/FormLayout.jsx
+++ src/components/FormLayout.jsx
@@ -3,21 +3,27 @@ const FormLayout = ({ title, form, handleSubmit, submitted }) => {
         <div className="flex flex-col justify-center items-center w-full">
             {/* Show thank you message when form is submitted */}
             { submitted ? (
-            <div className="flex flex-col justify-center items-center w-lg text-black bg-card border-2 rounded-lg">
-                Form submitted, thank you!
-            </div>
-            ) : (
-            <div className='flex flex-col justify-center items-center w-full'>
-                <div className='flex flex-col justify-center items-center w-full text-2xl font-bold text-black'>
-                    {title}
-                </div>
-                <form
-                onSubmit={handleSubmit}
-                className='w-full flex flex-col justify-center items-center gap-2 border-2 bg-card rounded-lg pt-4'
+                <div 
+                    className="flex flex-col justify-center items-center w-lg text-black bg-card border-2 rounded-lg"
                 >
-                { form }
-                </form>
-            </div>
+                    Form submitted, thank you!
+                </div>
+            ) : (
+                <div 
+                    className='flex flex-col justify-center items-center w-full'
+                >
+                    <div 
+                        className='flex flex-col justify-center items-center w-full text-2xl font-bold text-black'
+                    >
+                        {title}
+                    </div>
+                    <form
+                        onSubmit={handleSubmit}
+                        className='w-full flex flex-col justify-center items-center gap-2 border-2 bg-card rounded-lg pt-4'
+                    >
+                        { form }
+                    </form>
+                </div>
             )}
         </div>
     );
