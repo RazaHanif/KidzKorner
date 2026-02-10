59d90ca43de0913447d88fc939af764059481b33
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 21:34:15 -0500

    2026 Q1 updates @ 9:34:15 PM

--- src/pages/Childcare.jsx
+++ src/pages/Childcare.jsx
@@ -23,7 +23,7 @@ const Childcare = () => {
             </div>
             
             <div className="flex md:flex-row flex-col justify-between items-center w-3/4 gap-4">
-                <Image src='' alt='1' />
+                <Image src='/' alt='1' />
                 <Image src='' alt='2' />
                 <Image src='' alt='3' />
             </div>
