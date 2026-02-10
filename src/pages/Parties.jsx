fd4d70f5876eb9fe467aa7b94c8d6cbcf457f216
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 21:45:49 -0500

    2026 Q1 updates @ 9:45:49 PM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -102,7 +102,7 @@ const Parties = () => {
                 <div className="flex md:flex-row flex-col justify-between items-center gap-2 w-full">
                     <Image src='/' alt='1' />
                     <Image src='/IMG-20250302-WA0001.jpg' alt='2' />
-                    <Image src='/' alt='3' />
+                    <Image src='/IMG-20250519-WA0002.jpg' alt='3' />
                 </div>
             </div>
 
