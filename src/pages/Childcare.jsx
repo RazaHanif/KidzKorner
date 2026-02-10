ca959aed9ebce5d4b0dff468fcc24858cfbef2bb
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 22:55:59 -0500

    2026 Q1 updates @ 10:55:59 PM

--- src/pages/Childcare.jsx
+++ src/pages/Childcare.jsx
@@ -25,7 +25,7 @@ const Childcare = () => {
             <div className="flex md:flex-row flex-col justify-between items-center w-3/4 gap-4">
                 <Image src='/20251010_122316.jpg' alt='Child Playing With Magnetic Toy' />
                 <Image src='/IMG-20260108-WA0007.jpg' alt='Child Tracing Alphabets' />
-                <Image src='/IMG-20260209-WA0006.jpg' alt='Child Playing With Playdog' />
+                <Image src='/IMG-20260209-WA0006.jpg' alt='Child Playing With Play-Doh' />
             </div>
 
             <div className="flex md:flex-row flex-col justify-evenly items-center w-3/4">
