4a427342a04a05a4ae5be6d6bbfc2bd00150c230
Raza Hanif <raza_hanif@ymail.com>
2026-02-09 22:58:21 -0500

    2026 Q1 updates @ 10:58:21 PM

--- src/pages/Camp.jsx
+++ src/pages/Camp.jsx
@@ -97,7 +97,7 @@ const Camp = () => {
                         </ul>
                     </CardContent>
                 </Card>
-                <Image src='/20250711_111518.jpg' alt='0' w="w-sm"/>
+                <Image src='/20250711_111518.jpg' alt='Child Playing With Dinosaurs' w="w-sm"/>
             </div>
 
             <div className="flex flex-col justify-center w-3/4 md:w-1/3 gap-4">
