75352fe538828aa6a06c06fe801bb1b1f9df1282
Raza Hanif <raza_hanif@ymail.com>
2026-05-08 13:24:18 -0400

    2026 Q2 updates @ 1:24:17 PM

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
           <Route path='/workshop' element={<Workshops />} /> 
           <Route path='/camp' element={<Camp />} />
           <Route path='/about' element={<About />} />
-          <Route path='/test' element={<Test />} />
+          {/* <Route path='/test' element={<Test />} /> */}
           <Route path='*' element={<NotFound />} />
         </Routes>
       </div>
