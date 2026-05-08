418c57b7325c8c6c37268befd79b0714ae59fdc1
Raza Hanif <raza_hanif@ymail.com>
2026-05-08 14:20:35 -0400

    final og update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,7 +39,7 @@ const Home = () => {
             
             <meta 
                 property="og:title" 
-                content="Licensed Childcare & Daycare in Milton, Ontario | Kidz Korner" 
+                content="Kidz Korner Milton" 
             />
             <meta 
                 property="og:description" 
