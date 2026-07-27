4378f16101af21d72d58bfaed7e3db6e0ca9b96e
Raza Hanif <raza_hanif@ymail.com>
2026-07-27 17:37:23 -0400

    2026 Q3 Improvements @ 5:37:23 PM

--- src/schema/structDataSchema.js
+++ src/schema/structDataSchema.js
@@ -185,6 +185,10 @@ export const breadcrumbSchema = ({ breadcrumbs }) => {
         "@context": "https://schema.org",
         "@type": "BreadcrumbList",
 
-        "itemListElement": breadcrumbs.map()
+        "itemListElement": breadcrumbs.map((crumb, index) => ({
+            "@type": "ListItem",
+            "position": index + 1,
+            
+        }))
     }
 };
