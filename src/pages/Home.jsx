5d04e1f83f4c55227f40c8e94791f6cce2c3bc31
Raza Hanif <raza_hanif@ymail.com>
2026-07-27 15:54:41 -0400

    2026 Q3 Improvements @ 3:54:41 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,6 +27,8 @@ const Home = () => {
 
                     </div>
 
+                    
+
                     <div className="flex flex-col justify-center items-center w-full p-4">
                         <h2 className="w-full flex justify-center items-center m-4 font-bold flex-1">
                             Licensed Daycare Conveniently Located in Milton
