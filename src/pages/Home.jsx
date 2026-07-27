2b9087102da8a2178d54e5daed54c0ad5305702d
Raza Hanif <raza_hanif@ymail.com>
2026-07-27 16:06:38 -0400

    2026 Q3 Improvements @ 4:06:38 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,7 +41,8 @@ const Home = () => {
                             Dempsey, Timberlea, and nearby communities. Our
                             licensed childcare centre is conveniently located to
                             support busy parents looking for trusted daycare
-                            close to home. serving Milton families located near Highway 401 close to downtown Milton serving families throughout Halton Region
+                            close to home. 
+                            Serving Milton families located near Highway 401 close to downtown Milton serving families throughout Halton Region
                         </p>
 
                         <p>
