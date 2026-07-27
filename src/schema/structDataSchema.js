1a615579d34f8e05dff5ff6b9f988cdab0d4baf9
Raza Hanif <raza_hanif@ymail.com>
2026-07-27 17:19:07 -0400

    2026 Q3 Improvements @ 5:19:07 PM

--- src/schema/structDataSchema.js
+++ src/schema/structDataSchema.js
@@ -6,7 +6,7 @@ export const localBusinessSchema = {
     "@type": "LocalBusiness",
 
     "name": "Kidz Korner",
-    
+    "id": BUSINESS_ID,
     "url": "https://www.kidzkornermilton.com/",
     "description": "Kidz Korner Milton is a licensed daycare and childcare centre providing toddler daycare, preschool programs, before and after school care, and children's camps for families in Milton, Ontario.",
     "logo": "https://www.kidzkornermilton.com/logo.png",
