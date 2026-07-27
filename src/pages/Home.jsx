be6ee936746e13454095248e487fbc0f2ddeeb31
Raza Hanif <raza_hanif@ymail.com>
2026-07-27 15:08:42 -0400

    2026 Q3 Improvements @ 3:08:42 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
         <div className="flex flex-col justify-center items-center gap-2 w-full">
             <div className="lg:hidden flex flex-col justify-center items-center w-full mb-4">
                 <h1 className="lg:hidden text-3xl font-bold font-serif text-center">
-                    Welcome to Kidz Korner
+                    Licensed Daycare & Childcare in Milton
                 </h1>
             </div>
 
@@ -14,14 +14,33 @@ const Home = () => {
                     <Image src='/K_K_39.jpg' alt='Kidz Korner Milton, Ontario Store Front' w="w-md"/>
                     <div className="flex flex-col justify-center items-center w-full p-4">
                         <h1 className="hidden lg:block text-3xl font-bold self-center font-serif">
-                            Welcome to Kidz Korner
+                            Licensed Daycare & Childcare in Milton
                         </h1>
                         
-                        <p className="w-full flex justify-center items-center m-4 flex-1">
+                        {/* 
+                            Licensed daycare in Milton
+                            Childcare in Milton
+                            Preschool in Milton
+                            Toddler daycare
+                            Before and after school care
+                        */}
+                        
+                        {/* <p className="w-full flex justify-center items-center m-4 flex-1">
                                 Kidz Korner is a family-owned business located in the heart of Milton, Ontario.
                                 We offer licensed high quality child care for toddlers, preschoolers, before / after school programs, and licensed school age camps.
                                 Camps are offered during PA / PD days, March breaks, Summer breaks, and Winter breaks.
                                 Our commitment to providing loving, nurturing care has earned us the trust of many families and community members, creating a multi-generational legacy.
+                        </p> */}
+
+                        <h2 className="w-full flex justify-center items-center m-4 font-bold flex-1">
+                            Trusted Licne
+                        </h2>
+
+                        <p className="w-full flex justify-center items-center m-4 flex-1"> 
+                            We offer daycare for toddlers and preschoolers in Milton, before and after school care, and licensed school-age camps designed to support children at every stage of development. 
+                            Our camps are available during PA/PD days, March Break, Summer Break, and Winter Break. 
+                            Our commitment to providing loving, nurturing care has earned the trust of families throughout the community, creating a multi-generational legacy.
+
                         </p>
 
                         <h2 className="sr-only">Our Location</h2>
