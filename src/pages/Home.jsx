f154a17c00a34f32eb7ada8e4a903a831eeaa308
Raza Hanif <raza_hanif@ymail.com>
2026-07-27 16:09:53 -0400

    2026 Q3 Improvements @ 4:09:53 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -78,8 +78,7 @@ const Home = () => {
                             Kidz Korner offers licensed childcare programs
                             designed to support children through every stage of
                             their early learning journey. 
-                            From curious toddlers
-taking their first steps toward independence to preschoolers preparing for kindergarten, our programs provide age-appropriate learning experiences that encourage growth, creativity, and confidence.
+                            From curious toddlers taking their first steps toward independence to preschoolers preparing for kindergarten, our programs provide age-appropriate learning experiences that encourage growth, creativity, and confidence.
                         </p>
 
                         <p className="w-full flex justify-center items-center m-4 flex-1">
