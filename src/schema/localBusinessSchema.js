e895400cbd5aa09b5fa53e862acd68f58f336483
Raza Hanif <raza_hanif@ymail.com>
2026-07-27 16:42:36 -0400

    2026 Q3 Improvements @ 4:42:36 PM

---
+++ src/schema/localBusinessSchema.js
@@ -0,0 +1,15 @@
+export const businessSchema = {
+    "@context": "https://schema.org",
+    "@type": "LocalBusiness",
+    name: "Kidz Korner Milton",
+    url: "https://www.kidzkornermilton.com",
+    telephone: "+19058784697",
+    address: {
+        "@type": "PostalAddress",
+        streetAddress: "150 Nipissing Rd Units 10 & 11",
+        addressLocality: "Milton",
+        addressRegion: "ON",
+        postalCode: "L9T 5B3",
+        addressCountry: "CA"
+    }
+};
