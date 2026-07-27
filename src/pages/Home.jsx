20c0c704dd11ba6003983115bf3b915619feae5c
Raza Hanif <raza_hanif@ymail.com>
2026-07-27 16:02:51 -0400

    2026 Q3 Improvements @ 4:02:51 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -61,7 +61,7 @@ const Home = () => {
                         </p>
 
                         <p className="w-full flex justify-center items-center m-4 flex-1">
-                        Our play-based approach helps children develop social, emotional, physical, and cognitive skills while having fun in a nurturing classroom environment. With years of experience serving Milton families, we take pride in creating a community where children receive personalized attention and parents feel like part of the Kidz Korner family.
+                            Our play-based approach helps children develop social, emotional, physical, and cognitive skills while having fun in a nurturing classroom environment. With years of experience serving Milton families, we take pride in creating a community where children receive personalized attention and parents feel like part of the Kidz Korner family.
                         </p>
                     </div>
 
