1d4737e69caaa71c606dddd2285392238f8648e8
Raza Hanif <raza_hanif@ymail.com>
2026-07-27 17:38:19 -0400

    2026 Q3 Improvements @ 5:38:19 PM

--- src/schema/structDataSchema.js
+++ src/schema/structDataSchema.js
@@ -177,20 +177,20 @@ export const websiteSchema = {
 
 export const breadcrumbSchema = ({ breadcrumbs }) => {
     /* 
-const schema = createBreadcrumbSchema([
-    {
-        name: "Home",
-        url: "https://www.kidzkornermilton.com/"
-    },
-    {
-        name: "Daycare",
-        url: "https://www.kidzkornermilton.com/daycare"
-    },
-    {
-        name: "Toddler Daycare",
-        url: "https://www.kidzkornermilton.com/daycare/toddler-daycare-milton"
-    }
-]);
+        const schema = createBreadcrumbSchema([
+            {
+                name: "Home",
+                url: "https://www.kidzkornermilton.com/"
+            },
+            {
+                name: "Daycare",
+                url: "https://www.kidzkornermilton.com/daycare"
+            },
+            {
+                name: "Toddler Daycare",
+                url: "https://www.kidzkornermilton.com/daycare/toddler-daycare-milton"
+            }
+        ]);
     */
 
     return {
