bb05f5220c0a34b0540b044ec1426d9606b4abc5
Raza Hanif <raza_hanif@ymail.com>
2026-07-27 15:53:01 -0400

    2026 Q3 Improvements @ 3:53:01 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -70,7 +70,9 @@ const Home = () => {
                     </div>
 
                     <div className="flex flex-col justify-center items-center w-full p-4">
-                        <h2 className="w-full flex justify-center items-center m-4 font-bold flex-1">Our Programs</h2>
+                        <h2 className="w-full flex justify-center items-center m-4 font-bold flex-1">
+                            Our Programs
+                        </h2>
                         <p className="w-full flex justify-center items-center m-4 flex-1">
                             Briefly introduce
                             Toddler Daycare
