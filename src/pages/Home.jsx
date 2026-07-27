2c29bb7fa63fe92ff5f633f7bd563116c3568f81
Raza Hanif <raza_hanif@ymail.com>
2026-07-27 16:10:36 -0400

    2026 Q3 Improvements @ 4:10:36 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -93,7 +93,8 @@ const Home = () => {
                         <p className="w-full flex justify-center items-center m-4 flex-1">
                             Choosing a licensed daycare gives parents confidence
                             that their child is learning and growing in a safe,
-                            regulated environment. Licensed childcare centres in
+                            regulated environment. 
+                            Licensed childcare centres in
                             Ontario must meet standards established by the
                             Ministry of Education, including health and safety
                             requirements, educator qualifications,
