5f1534290f3724adcc42a39e3b3b3ba343819f87
Raza Hanif <raza_hanif@ymail.com>
2026-07-27 17:08:29 -0400

    2026 Q3 Improvements @ 5:08:29 PM

--- src/schema/structDataSchema.js
+++ src/schema/structDataSchema.js
@@ -30,19 +30,19 @@ export const localBusinessSchema = {
 
     // Business Hours
     "openingHoursSpecification": [
-    {
-        "@type": "OpeningHoursSpecification",
-        "dayOfWeek": [
-            "Monday",
-            "Tuesday",
-            "Wednesday",
-            "Thursday",
-            "Friday"
-        ],
-        "opens": "09:00",
-        "closes": "18:00"
-    }
-],
+        {
+            "@type": "OpeningHoursSpecification",
+            "dayOfWeek": [
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday"
+            ],
+            "opens": "09:00",
+            "closes": "18:00"
+        }
+    ],
 
     // Social / External Profiles
     "sameAs": [
