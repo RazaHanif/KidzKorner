d549178a51316f6f475b7b7c7bf71ec80480f97f
Raza Hanif <raza_hanif@ymail.com>
2026-07-30 18:24:34 -0400

    2026 Q3 Improvements @ 6:24:34 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -85,7 +85,7 @@ const Home = () => {
                             TODO: update NavLinks when those pages are created 
                         */}
                         <div className="flex md:flex-row flex-col gap-4">
-                            <NavLink to="/childcare" end className="hover:scale-110">
+                            <NavLink to="/childcare" end className="hover:scale-105">
                                 <Card className="flex-1 flex">
                                     <CardHeader className="text-start">
                                         <CardTitle>
