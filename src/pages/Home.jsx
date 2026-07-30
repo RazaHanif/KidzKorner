8afbc11ba1cfdd583584dbe98918a60c1264da0c
Raza Hanif <raza_hanif@ymail.com>
2026-07-30 17:35:42 -0400

    2026 Q3 Improvements @ 5:35:42 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -70,7 +70,7 @@ const Home = () => {
                                 Every program combines structured learning, creative play, outdoor activities, and opportunities to build friendships in a safe and supportive environment.
                             </p>
 
-                            <div className="flex flex-1 flex-col gap-4 w-full items-center">
+                            <div className="flex flex-1 flex-col gap-4 w-full">
 
                                 <Card>
                                     <CardHeader>
