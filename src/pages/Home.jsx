dd277e0387008667aa2a5953cee04079fd28b8fa
Raza Hanif <raza_hanif@ymail.com>
2026-07-30 18:22:28 -0400

    2026 Q3 Improvements @ 6:22:28 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -85,6 +85,7 @@ const Home = () => {
                             TODO: update NavLinks when those pages are created 
                         */}
                         <div className="flex md:flex-row flex-col gap-4">
+                            
                             <Card className="flex-1 flex">
                                 <CardHeader className="text-start">
                                     <CardTitle>
