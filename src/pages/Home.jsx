2562199f80e8c86cd7c0cc6942d613140a045cf1
Raza Hanif <raza_hanif@ymail.com>
2026-07-30 17:29:13 -0400

    2026 Q3 Improvements @ 5:29:13 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -82,18 +82,18 @@ const Home = () => {
                             </h2>
 
                             <Card>
-  <CardHeader>
-    <CardTitle>Card Title</CardTitle>
-    <CardDescription>Card Description</CardDescription>
-    <CardAction>Card Action</CardAction>
-  </CardHeader>
-  <CardContent>
-    <p>Card Content</p>
-  </CardContent>
-  <CardFooter>
-    <p>Card Footer</p>
-  </CardFooter>
-</Card>
+                                <CardHeader>
+                                    <CardTitle>Card Title</CardTitle>
+                                    <CardDescription>Card Description</CardDescription>
+                                    <CardAction>Card Action</CardAction>
+                                </CardHeader>
+                                <CardContent>
+                                    <p>Card Content</p>
+                                </CardContent>
+                                <CardFooter>
+                                    <p>Card Footer</p>
+                                </CardFooter>
+                            </Card>
 
                             <p className="w-full flex justify-center items-center m-4 flex-1">
                                 At Kidz Korner, we're committed to providing a safe, welcoming, and engaging environment where children feel supported every day. 
