1e0a6958371b321196ea8155c07d2189e4df942c
Raza Hanif <raza_hanif@ymail.com>
2026-07-30 18:41:39 -0400

    2026 Q3 Improvements @ 6:41:39 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -82,7 +82,7 @@ const Home = () => {
                     
                     */}
 
-                    <div className="flex flex-1 flex-col gap-4 w-full border-2">
+                    <div className="flex flex-1 flex-col gap-4 w-full">
                         {/* 
                             TODO: update NavLinks when those pages are created 
                         */}
