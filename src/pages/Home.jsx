49537812ea1a4d85e3e7619c97e56963fd21ce7c
Raza Hanif <raza_hanif@ymail.com>
2026-07-30 18:53:33 -0400

    2026 Q3 Improvements @ 6:53:33 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -181,7 +181,7 @@ const Home = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-col justify-center items-center w-full p-4">
+                <div className="flex flex-col justify-center items-center w-full py-16">
                     <h2 className="w-full flex justify-center items-center m-4 font-bold text-xl flex-1">
                         Why Families Choose Kidz Korner
                     </h2>
