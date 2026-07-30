2febe3721c7136172b0625fdd6c7aca217987e22
Raza Hanif <raza_hanif@ymail.com>
2026-07-30 17:27:24 -0400

    2026 Q3 Improvements @ 5:27:24 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -143,7 +143,7 @@ const Home = () => {
                                 <Card>
                                     <CardHeader>
                                         <CardTitle>
-                                            Toddler Daycare
+                                            Before & After School Care
                                         </CardTitle>
                                         <CardDescription>
                                             A safe, nurturing environment where toddlers learn through play while developing confidence, social skills, and independence.
