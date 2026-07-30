bc699f53a05cf35a103de8fc1603388baa7c66e9
Raza Hanif <raza_hanif@ymail.com>
2026-07-30 18:46:27 -0400

    2026 Q3 Improvements @ 6:46:27 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -194,6 +194,7 @@ const Home = () => {
                     <Card>
                         <CardContent className="flex flex-col gap-4">
                             <div className="flex flex-col items-start justify-start w-full">
+                                
                                 <h2 className="font-bold">
                                     ⭐ Licensed Ontario Childcare
                                 </h2>
