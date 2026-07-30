17dac64534919686f5bd018f7a844dadb097fe94
Raza Hanif <raza_hanif@ymail.com>
2026-07-30 17:24:20 -0400

    2026 Q3 Improvements @ 5:24:20 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -124,18 +124,18 @@ const Home = () => {
                                     Camps
                                 </Button>
                                 <Card>
-  <CardHeader>
-    <CardTitle>Card Title</CardTitle>
-    <CardDescription>Card Description</CardDescription>
-    <CardAction>Card Action</CardAction>
-  </CardHeader>
-  <CardContent>
-    <p>Card Content</p>
-  </CardContent>
-  <CardFooter>
-    <p>Card Footer</p>
-  </CardFooter>
-</Card>
+                                    <CardHeader>
+                                    <CardTitle>Card Title</CardTitle>
+                                    <CardDescription>Card Description</CardDescription>
+                                    <CardAction>Card Action</CardAction>
+                                    </CardHeader>
+                                    <CardContent>
+                                    <p>Card Content</p>
+                                    </CardContent>
+                                    <CardFooter>
+                                    <p>Card Footer</p>
+                                    </CardFooter>
+                                </Card>
                             </div>
                         </div>
 
