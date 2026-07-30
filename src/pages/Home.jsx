97e1f93daf1750f8b9bafaf327053879c479742a
Raza Hanif <raza_hanif@ymail.com>
2026-07-30 18:41:17 -0400

    2026 Q3 Improvements @ 6:41:17 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -86,7 +86,7 @@ const Home = () => {
                         {/* 
                             TODO: update NavLinks when those pages are created 
                         */}
-                        <div className="flex md:flex-row flex-col gap-4 w-full">
+                        <div className="flex md:flex-row flex-col gap-4">
                             <NavLink 
                                 to="/childcare" 
                                 end 
