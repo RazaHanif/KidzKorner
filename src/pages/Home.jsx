dd915870cb34ee7f28a25732804feaac272053a2
Raza Hanif <raza_hanif@ymail.com>
2026-07-30 17:12:40 -0400

    2026 Q3 Improvements @ 5:12:40 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -101,7 +101,7 @@ const Home = () => {
                                 Every program combines structured learning, creative play, outdoor activities, and opportunities to build friendships in a safe and supportive environment.
                             </p>
 
-                            <div className="flex flex-1">
+                            <div className="flex flex-1 flex-col">
                                 <Button>
                                     Toddler Daycare
                                 </Button>
