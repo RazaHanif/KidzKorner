473fe96adf35a003a4e18209fa34b0045c235c81
Raza Hanif <raza_hanif@ymail.com>
2026-07-30 19:01:54 -0400

    2026 Q3 Improvements @ 7:01:54 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,7 +41,7 @@ const Home = () => {
 
                         </p>
 
-                        <div className="w-full flex lg:flex-col gap-4 justify-around lg:justify-center ">
+                        <div className="w-full flex lg:flex-col gap-4 justify-around lg:justify-center lg:">
                             <NavLink 
                                 to="/about" 
                                 end
