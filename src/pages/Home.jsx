bd18ef4800fbe620b3fdc9c5e966868fa3d8818d
Raza Hanif <raza_hanif@ymail.com>
2026-07-30 18:17:10 -0400

    2026 Q3 Improvements @ 6:17:10 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -87,7 +87,7 @@ const Home = () => {
                         <div className="flex md:flex-row flex-col gap-4">
                             <Card className="flex-1 flex">
                                 <CardHeader className="border-2">
-                                    <CardTitle className="self-start justify-end items-end border-2">
+                                    <CardTitle className="self-start justify-end items-end border-2 text-start">
                                         Toddler Daycare
                                     </CardTitle>
                                     <CardDescription>
