a6b3d9add1a8276e593de02181fec7fa6ccb3ceb
Raza Hanif <raza_hanif@ymail.com>
2026-07-30 18:05:01 -0400

    2026 Q3 Improvements @ 6:05:01 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -75,7 +75,7 @@ const Home = () => {
                         {/* 
                             TODO: update NavLinks when those pages are created 
                         */}
-                        <div className="flex lg:flex-row flex-col gap-4">
+                        <div className="flex md:flex-row flex-col gap-4">
                             <Card className="flex-1 flex">
                                 <CardHeader>
                                     <CardTitle>
@@ -119,7 +119,7 @@ const Home = () => {
                             </Card>
                         </div>
 
-                        <div className="flex lg:flex-row flex-col gap-4">
+                        <div className="flex md:flex-row flex-col gap-4">
                             <Card className="flex-1 flex">
                                 <CardHeader>
                                     <CardTitle>
