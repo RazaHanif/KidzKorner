b5310fd31fbd5da20896d163e98d0cf9ad923db1
Raza Hanif <raza_hanif@ymail.com>
2026-07-30 18:07:27 -0400

    2026 Q3 Improvements @ 6:07:27 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,7 +41,7 @@ const Home = () => {
 
                         </p>
 
-                        <div className="w-full flex lg:flex-col gap-4 border-2 justify-around">
+                        <div className="w-full flex lg:flex-col gap-4 border-2 justify-around lg:justify-center">
                             {/* 
                                 Maybe a button to call? or jump to the injury form on the childcare/aboutus page 
                             */}
