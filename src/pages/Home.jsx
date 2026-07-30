4a010ffcdb939cb63225909e267bc8a926e09183
Raza Hanif <raza_hanif@ymail.com>
2026-07-30 17:15:30 -0400

    2026 Q3 Improvements @ 5:15:30 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -111,31 +111,29 @@ const Home = () => {
                             </p>
 
                             <div className="flex flex-1 flex-col gap-4 w-full items-center">
-                                <Button className="w-1/2">
-                                    <Card>
-                                        <CardHeader>
-                                            <CardTitle>
-                                                Card Title
-                                            </CardTitle>
-                                            <CardDescription>
-                                                Card Description
-                                            </CardDescription>
-                                            <CardAction>
-                                                Card Action
-                                            </CardAction>
-                                        </CardHeader>
-                                        <CardContent>
-                                            <p>
-                                                Card Content
-                                            </p>
-                                        </CardContent>
-                                        <CardFooter>
-                                            <p>
-                                                Card Footer
-                                            </p>
-                                        </CardFooter>
-                                    </Card>
-                                </Button>
+                                        <Card>
+                                            <CardHeader>
+                                                <CardTitle>
+                                                    Card Title
+                                                </CardTitle>
+                                                <CardDescription>
+                                                    Card Description
+                                                </CardDescription>
+                                                <CardAction>
+                                                    Card Action
+                                                </CardAction>
+                                            </CardHeader>
+                                            <CardContent>
+                                                <p>
+                                                    Card Content
+                                                </p>
+                                            </CardContent>
+                                            <CardFooter>
+                                                <p>
+                                                    Card Footer
+                                                </p>
+                                            </CardFooter>
+                                        </Card>
                                 <Button className="w-1/2">
                                     Preschool Program
                                 </Button>
