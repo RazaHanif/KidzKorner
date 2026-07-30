de5371e881201da30384bdf14768c3b587555bb0
Raza Hanif <raza_hanif@ymail.com>
2026-07-30 18:59:08 -0400

    2026 Q3 Improvements @ 6:59:08 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -102,7 +102,6 @@ const Home = () => {
                                 end 
                                 className="hover:scale-102 ease-in-out duration-300 flex flex-1"
                             >
-
                                 <Card className="flex-1 flex">
                                     <CardHeader className="text-start">
                                         <CardTitle>
