1c5925e3fa9fee25c221249805fefa99837a3fe2
Raza Hanif <raza_hanif@ymail.com>
2026-07-30 18:37:44 -0400

    2026 Q3 Improvements @ 6:37:43 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -272,6 +272,11 @@ const Home = () => {
                 </div>
 
                 <div className="flex lg:flex-row flex-col justify-center items-center gap-4">
+                                        <Image
+                        src="/K_K_39.jpg"
+                        alt="Kidz Korner licensed daycare in  Milton"
+                        w="w-md"
+                    />
                     <div className="flex flex-col justify-center items-center w-full p-4">
                         <h2 className="w-full flex justify-center items-center m-4 font-bold text-xl flex-1">
                             Licensed Daycare Conveniently Located in Milton
