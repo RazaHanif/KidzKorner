22ba22ca89b555a6451710da450097e6659fe548
Raza Hanif <raza_hanif@ymail.com>
2026-07-30 17:04:27 -0400

    2026 Q3 Improvements @ 5:04:27 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,7 +31,7 @@ const Home = () => {
 
                         </p>
 
-                        <div className="border-2 w-full justify-center items-end">
+                        <div className="border-2 w-full justify-around">
                             {/* 
                                 Maybe a button to call? or jump to the injury form on the childcare/aboutus page 
                                 */}
