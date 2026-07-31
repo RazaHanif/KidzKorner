3fe4eb0ed1dcbe076d7b0bdced7b0aec587a1507
Raza Hanif <raza_hanif@ymail.com>
2026-07-31 00:35:52 -0400

    2026 Q3 Improvements @ 12:35:52 AM

--- src/pages/Daycare.jsx
+++ src/pages/Daycare.jsx
@@ -151,7 +151,7 @@ const Daycare = () => {
 
             <div className="flex flex-col justify-center items-center w-3/4 m-4">
                 <h2 className="text-xl font-bold text-center lg:mb-4">
-                    Care Schedule
+                    Flexible Care Options for Milton Families
                 </h2>
                 <div className="flex lg:flex-row flex-col lg:justify-evenly justify-center items-center w-full">
                     <div className="flex flex-col justify-center m-4 lg:m-0">
