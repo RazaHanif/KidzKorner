dc0c2c01b39d5b9e3d4f24f8e28a1d6ff3477a95
Raza Hanif <raza_hanif@ymail.com>
2026-07-31 00:29:16 -0400

    2026 Q3 Improvements @ 12:29:16 AM

--- src/pages/Daycare.jsx
+++ src/pages/Daycare.jsx
@@ -151,7 +151,7 @@ const Daycare = () => {
 
             <div className="flex flex-col justify-center items-center w-3/4 m-4">
                 <h2 className="text-xl font-bold text-center lg:mb-4">
-                    Program Options - Full Time & Part Time Care
+                    Care Scheu - Full Time & Part Time Care
                 </h2>
                 <div className="flex lg:flex-row flex-col lg:justify-evenly justify-center items-center w-full">
                     <div className="flex flex-col justify-center m-4 lg:m-0">
