71cd2f325909cf572f760d9d7aa4615c12afa932
Raza Hanif <raza_hanif@ymail.com>
2026-07-30 23:00:47 -0400

    2026 Q3 Improvements @ 11:00:47 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -57,7 +57,7 @@ const Home = () => {
                                 end
                                 className="hover:scale-105 ease-in-out duration-300 flex-1 border-1 lg:w-3/4 flex justify-end"
                             >
-                                <Button className="cursor-pointer ">
+                                <Button className="cursor-pointer">
                                     Explore Programs
                                 </Button>
                             </NavLink>
