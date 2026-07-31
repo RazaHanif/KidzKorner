3619cb538241991039c9e524105512fff9e68a36
Raza Hanif <raza_hanif@ymail.com>
2026-07-31 00:29:08 -0400

    2026 Q3 Improvements @ 12:29:08 AM

--- src/pages/Daycare.jsx
+++ src/pages/Daycare.jsx
@@ -146,7 +146,17 @@ const Daycare = () => {
                             </Card>
                         </NavLink>
                     </div>
+                </div>
+            </div>
 
+            <div className="flex flex-col justify-center items-center w-3/4 m-4">
+                <h2 className="text-xl font-bold text-center lg:mb-4">
+                    Program Options - Full Time & Part Time Care
+                </h2>
+                <div className="flex lg:flex-row flex-col lg:justify-evenly justify-center items-center w-full">
+                    <div className="flex flex-col justify-center m-4 lg:m-0">
+                        <CareSchedule />
+                    </div>
                     <Image src='/20250822_121126.jpg' alt='Child Playing With Blocks at Kidz Korner Milton, Ontario' w="w-sm"/>
                 </div>
             </div>
