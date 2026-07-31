0d28b1015f9fab4fa9585efb790abafc2e46f895
Raza Hanif <raza_hanif@ymail.com>
2026-07-31 00:18:57 -0400

    2026 Q3 Improvements @ 12:18:57 AM

--- src/pages/Daycare.jsx
+++ src/pages/Daycare.jsx
@@ -77,7 +77,17 @@ const Daycare = () => {
                     Program Options - Full Time & Part Time Care
                 </h2>
                 <div className="flex lg:flex-row flex-col lg:justify-evenly justify-center items-center w-full">
-                    <div>btns</div>
+                    <div>
+                        <Card>
+  <CardTitle>Toddler Program</CardTitle>
+  <CardDescription>
+      Nurturing early learning for toddlers through play, exploration, and discovery.
+  </CardDescription>
+  <Button>
+      Learn More
+  </Button>
+</Card>
+                    </div>
                     <div className="flex flex-col justify-center m-4 lg:m-0">
                         <TimeTable />
                     </div>
