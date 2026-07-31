fadc75164fda9a19df43a6ad44ea07587dfffb3f
Raza Hanif <raza_hanif@ymail.com>
2026-07-31 00:00:49 -0400

    2026 Q3 Improvements @ 12:00:49 AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { Routes, Route, Navigate } from 'react-router'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import About from './pages/About.jsx'
-import Childcare from './pages/Childcare.jsx'
-import Home from './pages/Home.jsx'
-import Parties from './pages/Parties.jsx'
-import Playground from './pages/Playground.jsx'
-import Workshops from './pages/Workshops.jsx'
-import Camp from './pages/Camp.jsx'
-import Test from './pages/Test.jsx'
-import NotFound from './pages/NotFound.jsx'
+import { Routes, Route, Navigate } from "react-router";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import About from "./pages/About.jsx";
+import Childcare from "./pages/Childcare.jsx";
+import Home from "./pages/Home.jsx";
+import Parties from "./pages/Parties.jsx";
+import Playground from "./pages/Playground.jsx";
+import Workshops from "./pages/Workshops.jsx";
+import Camp from "./pages/Camp.jsx";
+import Test from "./pages/Test.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 /* 
 
@@ -33,53 +33,60 @@ import NotFound from './pages/NotFound.jsx'
 */
 
 function App() {
-  return (
-    <div className='flex-1 flex flex-col w-full min-h-screen'>
-      <Header/>
-      <div className='flex-1 flex flex-col w-full p-4 relative'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='*' element={<NotFound />} />
+    return (
+        <div className="flex-1 flex flex-col w-full min-h-screen">
+            <Header />
+            <div className="flex-1 flex flex-col w-full p-4 relative">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="*" element={<NotFound />} />
 
-          <Route path='/about-us' element={<About />} />
+                    <Route path="/about-us" element={<About />} />
 
-          <Route path='/blog' element={<Test />} />
-          <Route path='/blog/:slug' element={<Test />} />
+                    {/* Blog */}
+                    <Route path="/blog" element={<Test />} />
+                    <Route path="/blog/:slug" element={<Test />} />
 
-          {/* Camp */}
-          <Route path='/camp' element={<Camp />} />
-          <Route path='/camp/summer-camp' element={<Camp />} />
-          <Route path='/camp/march-break-camp' element={<Camp />} />
-          <Route path='/camp/winter-break-camp' element={<Camp />} />
-          <Route path='/camp/pa-day-camp' element={<Camp />} />
+                    {/* Camp */}
+                    <Route path="/camp" element={<Camp />} />
+                    <Route path="/camp/summer-camp" element={<Camp />} />
+                    <Route path="/camp/march-break-camp" element={<Camp />} />
+                    <Route path="/camp/winter-break-camp" element={<Camp />} />
+                    <Route path="/camp/pa-day-camp" element={<Camp />} />
 
-            {/* Daycare */}
-          <Route path='/daycare' element={<Childcare />} />
-          <Route path='/daycare/toddler-daycare' element={<Childcare />} />
-          <Route path='/daycare/preschool' element={<Childcare />} />
-          <Route path='/daycare/before-after-school-care' element={<Childcare />} />
+                    {/* Daycare */}
+                    <Route path="/daycare" element={<Childcare />} />
+                    <Route
+                        path="/daycare/toddler-daycare"
+                        element={<Childcare />}
+                    />
+                    <Route path="/daycare/preschool" element={<Childcare />} />
+                    <Route
+                        path="/daycare/before-after-school-care"
+                        element={<Childcare />}
+                    />
 
-          <Route path='/faq' element={<Test />} />
+                    <Route path="/faq" element={<Test />} />
 
-          <Route path='/birthday-parties' element={<Parties />} />
+                    <Route path="/birthday-parties" element={<Parties />} />
 
-          <Route path='/indoor-playground' element={<Playground />} />
+                    <Route path="/indoor-playground" element={<Playground />} />
 
-          <Route path='/test' element={<Test />} />
+                    <Route path="/test" element={<Test />} />
 
-          <Route path='/workshops' element={<Workshops />} /> 
+                    <Route path="/workshops" element={<Workshops />} />
 
-          {/* Redirected but keeping here */}
-          <Route path='/about' element={<About />} />
-          <Route path='/childcare' element={<Childcare />} />
-          <Route path='/parties' element={<Parties />} />
-          <Route path='/playground' element={<Playground />} />
-          <Route path='/workshops' element={<Workshops />} /> 
-        </Routes>
-      </div>
-      <Footer/>
-    </div>
-  )
+                    {/* Redirected but keeping here */}
+                    <Route path="/about" element={<About />} />
+                    <Route path="/childcare" element={<Childcare />} />
+                    <Route path="/parties" element={<Parties />} />
+                    <Route path="/playground" element={<Playground />} />
+                    <Route path="/workshops" element={<Workshops />} />
+                </Routes>
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
-export default App
+export default App;
