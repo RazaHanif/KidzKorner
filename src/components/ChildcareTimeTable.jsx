718c93d268da41fe8bbe9ff6a27b82bcb2877168
Raza Hanif <raza_hanif@ymail.com>
2026-07-31 00:40:14 -0400

    2026 Q3 Improvements @ 12:40:14 AM

--- src/components/ChildcareTimeTable.jsx
+++ src/components/ChildcareTimeTable.jsx
@@ -43,7 +43,7 @@ const ChildcareTimeTable = () => {
         },
         {
             time: "12:15pm - 1:30pm",
-            program: "Rest & Mindfulness for Full Day Children - Arts & Crafts / Sensory Play for Half Day Children"
+            program: "Rest & Mindfulness (Full Day Children) / Arts & Crafts & Sensory Play (Half Day Children)"
         },
         {
             time: "1:30pm - 1:45pm",
