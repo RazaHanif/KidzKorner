a08e5d29e84572d0cf3a733b85ca42dda10741ab
Raza Hanif <raza_hanif@ymail.com>
2026-07-31 00:23:19 -0400

    2026 Q3 Improvements @ 12:23:18 AM

--- src/pages/Daycare.jsx
+++ src/pages/Daycare.jsx
@@ -81,7 +81,7 @@ const Daycare = () => {
                 <div className="flex lg:flex-row flex-col lg:justify-evenly justify-center items-center w-full">
                     <div className="flex flex-col flex-1 w-3/4 gap-4">
                         <NavLink 
-                            to="/daycare" 
+                            to="/daycare/toddler-" 
                             end 
                             className="hover:scale-102 ease-in-out duration-300 flex flex-1"
                         >
