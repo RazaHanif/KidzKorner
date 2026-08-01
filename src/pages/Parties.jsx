3f26a605ff16dcad31e2184cc6e9f23cae794435
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 19:56:25 -0400

    2026 Q3 Improvements @ 7:56:25 PM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -29,7 +29,7 @@ const Parties = () => {
                     Make your child's special day unforgettable with a Kidz Korner Party! Enjoy exclusive access to our indoor playground, just for you and your guests.
 
                     Celebrate your child's birthday with a private indoor party at Kidz Korner in Milton, Ontario.
-                    Our 
+                    Our birthday party packages include ex
                 </p>
             </div>
 
