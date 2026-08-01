1344b07490c7878225c75e6174517ce2ff78b741
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 19:51:01 -0400

    2026 Q3 Improvements @ 7:51:01 PM

--- src/pages/ToddlerDaycare.jsx
+++ src/pages/ToddlerDaycare.jsx
@@ -27,7 +27,6 @@ const ToddlerDaycare = () => {
             <title>
                 Toddler Daycare | Toddler | Kidz Korner
             </title>
-            <meta name="keywords" content="childcare Milton, daycare programs Milton, preschool programs Milton, infant care Milton, toddler care Milton" />
             <meta name="description" content="Kidz Korner is a licensed daycare in Milton, Ontario offering toddler, preschool, and before & after school programs with experienced RECE educators, play-based learning, and a safe indoor environment." />
         </div>
     );
