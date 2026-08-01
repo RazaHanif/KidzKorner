204bfe5ec2ac392c704d035dc2f32912b8069ebb
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 19:52:16 -0400

    2026 Q3 Improvements @ 7:52:16 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -1,7 +1,7 @@
 import StructData from "../components/StructData";
 import { breadcrumbSchema, localBusinessSchema } from "../schema/structDataSchema";
 
-const ToddlerDaycare = () => {
+const BeforeAfterDaycare = () => {
     const breadCrumb = [
         {
             name: "Home",
@@ -32,4 +32,4 @@ const ToddlerDaycare = () => {
     );
 }
   
-export default ToddlerDaycare;
+export default BeforeAfterDaycare;
