9e9af7ac3597cd53160bb327083a40c5323d1032
Raza Hanif <raza_hanif@ymail.com>
2026-08-02 10:58:01 -0400

    2026 Q3 Improvements @ 10:58:01 AM

--- src/pages/Playground.jsx
+++ src/pages/Playground.jsx
@@ -20,7 +20,7 @@ const Playground = () => {
         <div className="flex flex-col flex-1 justify-center items-center gap-4 w-full lg:py-16 pb-8">
             <div className="flex flex-col justify-center items-center w-full mb-4">
                 <h1 className="lg:text-5xl text-3xl font-bold font-serif text-center py-4">
-                    Drop-In Fun!
+                    Indoor Playground in Milton, Ontario
                 </h1>
             </div>
 
