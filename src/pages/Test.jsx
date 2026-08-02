679bf95d6a90a32587eb409fd37eebbfa610a1e9
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:13:14 -0400

    2026 Q3 Improvements @ 10:13:14 PM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -3,22 +3,6 @@ const Test = () => {
         <div className="flex flex-col justify-center items-center gap-2 w-full text-justify">
             <div className="flex lg:flex-row flex-col justify-center w-3/4 m-4 gap-4">
             </div>
-
-            <div className="flex justify-center items-center w-3/4 rounded-lg border-2 shadow-lg overflow-hidden mb-4">
-                <iframe
-                    title="Google Maps"
-                    loading="lazy"
-                    referrerPolicy="no-referrer"
-                    style={{ border: 0 }}
-                    allowFullScreen=""
-                    aria-hidden="false"
-                    alt="Kidz Korner Google Maps PlugIn"
-                    tabIndex="0"
-                    src={`https://www.google.com/maps/embed/v1/place?key=${import.meta.env.VITE_GOOGLE_MAPS_API}&q=Kidz+Korner+Milton+Ontario`}
-                    className="border-4 w-full h-[75vw] lg:h-[75vh]"
-                />
-            </div>
-            
         </div>
     );
 }
