65be65160fd170356aff51b5a25f67b784f06cd8
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:15:42 -0400

    2026 Q3 Improvements @ 10:15:42 PM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -3,7 +3,7 @@ import { Shapes, ToyBrick } from "lucide-react";
 const Test = () => {
     return (
         <div className="flex flex-col justify-center items-center gap-2 w-full text-center">
-            <div className="flex lg:flex-row flex-col justify-center items-center w-3/4 m-4 gap-4">
+            <div className="flex lg:flex-row flex-col justify-center items-center w-3/4 gap-4">
                 <h1 className="text-3xl font-bold">
                     The Sandbox
                 </h1>
