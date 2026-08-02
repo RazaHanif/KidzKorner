a569af1b3e0e2f9d671dd0831690309f2b0b73cb
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:18:56 -0400

    2026 Q3 Improvements @ 10:18:56 PM

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Navigation from '@/components/Navigation';
 
 const Header = () => {
   return (
-    <div className='flex flex-row justify-center px-2 my-2 w-full border-2'>
+    <div className='flex flex-row justify-center px-2 mt-2 w-full border-2'>
       <div className="flex flex-row justify-between items-center w-full pr-2 rounded-md bg-primary font-serif">
         <Link 
           to="/"
