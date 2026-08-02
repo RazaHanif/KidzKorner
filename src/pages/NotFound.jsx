347a0f7f2c2aa0cca5a6ddf308ae86f54dca33bc
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:35:42 -0400

    2026 Q3 Improvements @ 10:35:41 PM

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -16,7 +16,7 @@ const NotFound = () => {
             </div>
 
             <div className="w-full flex lg:flex-col gap-4 justify-center lg:items-end lg:pr-4 items-center">
-                <NavLink 
+                <NavLink
                     to="/about" 
                     end
                     className="hover:scale-105 ease-in-out duration-300 flex-1 lg:w-3/4 flex justify-center lg:justify-end"
