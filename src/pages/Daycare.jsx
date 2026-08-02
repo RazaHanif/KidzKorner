8eccd9c36620e051ce3eacd98493938ad973f7f8
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:26:52 -0400

    2026 Q3 Improvements @ 10:26:51 PM

--- src/pages/Daycare.jsx
+++ src/pages/Daycare.jsx
@@ -29,7 +29,7 @@ const Daycare = () => {
     return (
         <div className="flex flex-col flex-1 justify-center items-center gap-4 w-full lg:py-16">
             <div className="flex flex-col justify-center items-center w-full">
-                <h1 className="text-3xl font-bold font-serif text-center">
+                <h1 className="lg:text-5xl text-3xl font-bold font-serif text-center">
                     Licensed Daycare in Milton, Ontario
                 </h1>
                 <h2 className="text-xl font-bold text-center">
