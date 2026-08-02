e33988ff99f82f58a0c8203b4c3c04256a60988b
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 20:56:00 -0400

    2026 Q3 Improvements @ 8:56:00 PM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -212,10 +212,10 @@ const Parties = () => {
                             <PartyPopper className="h-6 w-6 shrink-0 text-primary"/> Family Celebrations
                         </p>
                         <p className="flex items-start gap-3">
-                            <PartyPopper className="h-6 w-6 shrink-0 text-primary"/> Winter Birthdays Parties
+                            <PartyPopper className="h-6 w-6 shrink-0 text-primary"/> Winter Birthday Parties
                         </p>
                         <p className="flex items-start gap-3">
-                            <PartyPopper className="h-6 w-6 shrink-0 text-primary"/> Rainy Day Birthdays Parties
+                            <PartyPopper className="h-6 w-6 shrink-0 text-primary"/> Rainy Day Birthday Parties
                         </p>
                     </div>
                 </div>
