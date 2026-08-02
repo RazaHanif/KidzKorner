e64a171cc6469185fd1e237405d8c0056a23b8b3
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:31:39 -0400

    2026 Q3 Improvements @ 10:31:39 PM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -231,6 +231,9 @@ const Parties = () => {
             </div>
 
             <div className="flex flex-col justify-center items-center w-3/4 gap-4">
+                          <p className="flex w-2/3 text-center">
+                    Ready to join us? Contact us today to learn more about our preschool programs and schedule a tour of our facility! 
+                </p>
                 <PartyForm />
             </div>
 
