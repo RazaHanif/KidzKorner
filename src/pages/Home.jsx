9461850966926fec1d275816dfad00d9ce03afcf
Raza Hanif <raza_hanif@ymail.com>
2026-08-02 12:13:36 -0400

    2026 Q3 Improvements @ 12:13:36 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -174,6 +174,18 @@ const Home = () => {
                     <Card>
                         <CardContent className="flex md:flex-row flex-col gap-2">
                             <div className="flex flex-col flex-1 gap-2">
+                                                                <div className="flex flex-col items-start justify-start w-full">
+                                    <div className="flex gap-2 text-primary">
+                                        <ShieldCheck />
+                                        <h2 className="font-bold">
+                                            Safe Environment
+                                        </h2>
+
+                                    </div>
+                                    <p className="text-start pl-8">
+                                        A secure, welcoming space where children can thrive.
+                                    </p>
+                                </div>
                                 <div className="flex flex-col items-start justify-start w-full">
                                     <h2 className="flex flex-row gap-2 font-bold w-full">
                                         <BadgeCheck />
