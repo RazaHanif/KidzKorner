25b53134628221cc70295984ebb65e0ac4da28a0
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:39:35 -0400

    2026 Q3 Improvements @ 10:39:35 PM

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -15,7 +15,7 @@ const NotFound = () => {
                 <p>Oops! This page ran off to play!</p>
             </div>
 
-            <div className="w-full flex lg:flex-col gap-4 justify-center items-center">
+            <div className="w-full flex justify-center items-center">
                 <NavLink
                     to="/" 
                     end
