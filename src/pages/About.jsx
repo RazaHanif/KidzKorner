57f1398310023edfefdb95aad2f7b881d3067998
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:55:22 -0400

    2026 Q3 Improvements @ 10:55:22 PM

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -61,7 +61,7 @@ const About = () => {
             
             <div className="flex flex-col justify-center items-center w-3/4 gap-4">
                 <p className="flex w-2/3 text-center">
-                    We would love to hear from you! 
+                    We look forward to hearing from you! 
                 </p> 
                 <ContactForm/>
             </div>
