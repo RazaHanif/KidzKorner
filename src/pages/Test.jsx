b4a5662f36d377ef37b053570b573b4b842a86e2
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:44:48 -0400

    2026 Q3 Improvements @ 10:44:48 PM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -7,8 +7,8 @@ const Test = () => {
                 <h1 className="text-3xl font-bold">
                     The Sandbox
                 </h1>
-                <Shapes className="h-10 w-10 text-primary"/>
             </div>
+                <Shapes className="h-10 w-10 text-primary"/>
         </div>
     );
 }
