bb9605fd743d5ef67a0af89a4abecf951a9d3bdc
Raza Hanif <raza_hanif@ymail.com>
2026-08-02 12:19:36 -0400

    2026 Q3 Improvements @ 12:19:35 PM

--- src/App.jsx
+++ src/App.jsx
@@ -39,8 +39,12 @@ function App() {
             <Header />
             <div className="flex-1 flex flex-col w-full p-4 relative">
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="*" element={<NotFound />} />
+                    <Route 
+                        path="/" 
+                        element={<Home />} 
+                    />
+                    <Route 
+                        path="*" element={<NotFound />} />
 
                     <Route path="/about" element={<About />} />
 
