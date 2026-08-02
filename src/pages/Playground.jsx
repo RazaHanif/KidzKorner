bd432a34ed3e58c2b800c09c90b57b8d76dc6982
Raza Hanif <raza_hanif@ymail.com>
2026-08-02 11:31:28 -0400

    2026 Q3 Improvements @ 11:31:28 AM

--- src/pages/Playground.jsx
+++ src/pages/Playground.jsx
@@ -68,7 +68,7 @@ const Playground = () => {
                         <div className="flex flex-col gap-2 w-full flex-1">
                             <p className="flex items-start gap-3">
                                 <CheckCircle className="h-6 w-6 shrink-0 text-primary"/>
-                                
+                                Climbing Structure
                             </p>
                             <p className="flex items-start gap-3">
                                 <CheckCircle className="h-6 w-6 shrink-0 text-primary"/> 2nd Birthdays
