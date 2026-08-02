08e051fa69100464502ac71c4b6b4f3b3a1a503f
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 20:07:28 -0400

    2026 Q3 Improvements @ 8:07:28 PM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -130,7 +130,7 @@ const Parties = () => {
                 <div className="w-full flex lg:flex-row flex-col p-2 gap-2">
                     <div className="flex flex-col gap-2 w-full flex-1">
                         <p className="ml-4 flex">
-                            <Check className="size"/> Private Venue Rental
+                            <Check /> Private Venue Rental
                         </p>
                         <p className="ml-4">
                             Indoor Playground exclusive to your guests
