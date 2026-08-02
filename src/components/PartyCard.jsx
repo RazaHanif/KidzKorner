953d76d3373b98f8f8b8000f295b4f131a88d391
Raza Hanif <raza_hanif@ymail.com>
2026-08-02 11:25:34 -0400

    2026 Q3 Improvements @ 11:25:34 AM

--- src/components/PartyCard.jsx
+++ src/components/PartyCard.jsx
@@ -18,9 +18,9 @@ const PartyCard = ({ title, price, desc, bullet = [], tax=true }) => {
                 <CardDescription className="p-2 bg-accent flex justify-center items-center rounded-md text-bold text-lg">
                     {tax ? (
                         <>
-                        ${price} <span className="text-xs">+tax</span>
+                           ${price} <span className="text-xs">+tax</span>
                         </>
-                        ) : (
+                    ) : (
                         <>${price}</>
                     )}
                     ${ price }<span className="text-xs">+tax</span>
