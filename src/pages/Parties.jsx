6aaf53e634a6b31e80986fc578f8e54fad2e4b05
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 20:23:39 -0400

    2026 Q3 Improvements @ 8:23:39 PM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -125,7 +125,7 @@ const Parties = () => {
                 </div>
             </div>
 
-            <div className="flex flex-col justify-center items-center w-full mb-4 bg-primary">
+            <div className="flex flex-col justify-center items-center w-3/4 mb-4 bg-gray">
                 <h2 className="text-xl font-bold text-center pb-4">
                     Why Families Love Our Birthday Parties
                 </h2>
