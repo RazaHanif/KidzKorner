958282a1629f300b27b56d01b494de97bf9dbe32
Raza Hanif <raza_hanif@ymail.com>
2026-08-02 11:31:23 -0400

    2026 Q3 Improvements @ 11:31:23 AM

--- src/pages/Playground.jsx
+++ src/pages/Playground.jsx
@@ -67,7 +67,8 @@ const Playground = () => {
                     <div className="flex lg:flex-row flex-col p-2 gap-2">
                         <div className="flex flex-col gap-2 w-full flex-1">
                             <p className="flex items-start gap-3">
-                                <CheckCircle className="h-6 w-6 shrink-0 text-primary"/> 1st Birthdays
+                                <CheckCircle className="h-6 w-6 shrink-0 text-primary"/>
+                                
                             </p>
                             <p className="flex items-start gap-3">
                                 <CheckCircle className="h-6 w-6 shrink-0 text-primary"/> 2nd Birthdays
