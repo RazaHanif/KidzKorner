f2e685c3a668121284df1e7975be1bc86313a99a
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:37:01 -0400

    2026 Q3 Improvements @ 10:37:01 PM

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -10,7 +10,7 @@ const NotFound = () => {
                 </h1>
             </div>
 
-            <div className="w-full flex flex-col justify-center items-center gap-4 text-muted-foreground">
+            <div className="w-full flex flex-col justify-center items-center gap-4 text-muted-foreground ">
                 <p className="text-6xl">🎈</p>
                 <p>Oops! This page ran off to play!</p>
             </div>
