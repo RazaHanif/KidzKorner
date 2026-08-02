293b299012c1beaac5149068acb7b42b570c7561
Raza Hanif <raza_hanif@ymail.com>
2026-08-02 12:09:27 -0400

    2026 Q3 Improvements @ 12:09:27 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ import {
 } from "@/components/ui/card"
 import StructData from "../components/StructData";
 import { localBusinessSchema, organizationSchema, websiteSchema } from "../schema/structDataSchema";
-import { BadgeCheck, GraduationCap, HeartHandshake } from "lucide-react";
+import { BadgeCheck, GraduationCap, HeartHandshake, Palette } from "lucide-react";
 
 const Home = () => {
     return (
@@ -206,7 +206,7 @@ const Home = () => {
 
                                 <div className="flex flex-col items-start justify-start w-full">
                                     <h2 className="font-bold">
-                                        
+                                        <Palette />
                                         Play-Based Learning
                                     </h2>
                                     <p className="text-start pl-4">
