2493ec0502c64be334a54effae4f93aa655997f2
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 21:11:43 -0400

    2026 Q3 Improvements @ 9:11:43 PM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -183,7 +183,7 @@ const Parties = () => {
                         alt="Birthday cake celebration at Kidz Korner birthday party in Milton"
                     />
                 </div>
-                <div>
+                <div flex flex>
                     <h2 className="text-xl font-bold text-center">
                         Our birthday parties are perfect for:
                     </h2>
