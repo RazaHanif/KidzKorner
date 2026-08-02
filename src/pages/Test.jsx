f234b993c6abe2836279e6d205e55010c851b6fa
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:13:00 -0400

    2026 Q3 Improvements @ 10:13:00 PM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,13 +1,40 @@
 const Test = () => {
     return (
-        <>
+        <div className="flex flex-col justify-center items-center gap-2 w-full text-justify">
+            <div className="flex lg:flex-row flex-col justify-center w-3/4 m-4 gap-4">
+                <div>
+                    <ContactUsTable />
+                </div>
+
+                <div className="flex justify-center">
+                    <Image 
+                        src='IMG-20260208-WA0002.jpg'
+                        alt='Child Posing at Kidz Korner Milton, Ontario'
+                        w={'w-sm'}
+                    />
+                </div>
+
+                <div>
+                    <HoursTimeTable />
+                </div>
+            </div>
+
+            <div className="flex justify-center items-center w-3/4 rounded-lg border-2 shadow-lg overflow-hidden mb-4">
+                <iframe
+                    title="Google Maps"
+                    loading="lazy"
+                    referrerPolicy="no-referrer"
+                    style={{ border: 0 }}
+                    allowFullScreen=""
+                    aria-hidden="false"
+                    alt="Kidz Korner Google Maps PlugIn"
+                    tabIndex="0"
+                    src={`https://www.google.com/maps/embed/v1/place?key=${import.meta.env.VITE_GOOGLE_MAPS_API}&q=Kidz+Korner+Milton+Ontario`}
+                    className="border-4 w-full h-[75vw] lg:h-[75vh]"
+                />
+            </div>
             
-            How did you get in the sandbox?
-            <title>
-                Test Page | Kidz Korner 
-            </title>
-            <meta name="robots" content="noindex, nofollow" />
-        </>
+        </div>
     );
 }
   
