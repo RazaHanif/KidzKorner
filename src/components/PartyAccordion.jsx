2ae966e2fda55f17efded8826d8c1cd96057148e
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 20:34:37 -0400

    2026 Q3 Improvements @ 8:34:37 PM

--- src/components/PartyAccordion.jsx
+++ src/components/PartyAccordion.jsx
@@ -14,7 +14,7 @@ const PartyAccordion = () => {
                     Can we bring our own Food?
                 </AccordionTrigger>
                 <AccordionContent>
-                    Yes. Fami
+                    Yes. Families are welcome to bring their own food at no additional cost. We also provide access to a private kitchen space. Please note that our facility follows a strict nut-free policy.
                 </AccordionContent>
             </AccordionItem>
         </Accordion>
