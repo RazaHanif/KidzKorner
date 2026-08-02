45a1071a70467060a3d5aea98940b268040fed49
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:40:44 -0400

    2026 Q3 Improvements @ 10:40:43 PM

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -19,7 +19,7 @@ const NotFound = () => {
                 <NavLink
                     to="/" 
                     end
-                    className="hover:scale-105 ease-in-out duration-300 flex-1 lg:w-3/4 flex justify-center lg:justify-end w-full"
+                    className="hover:scale-105 ease-in-out duration-300 flex-1 flex justify-center lg:justify-end"
                 >
                     <Button className="cursor-pointer min-w-1/2 w-3/4 text-lg">
                         Go Home!
