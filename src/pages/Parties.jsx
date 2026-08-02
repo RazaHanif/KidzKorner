8ea7c3aaef991bd4c0ee73058d02162a9a743112
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:57:22 -0400

    2026 Q3 Improvements @ 10:57:22 PM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -231,7 +231,7 @@ const Parties = () => {
             </div>
 
             <div className="flex flex-col justify-center items-center w-3/4 gap-4">
-                <p className="flex w-2/3 text-center">
+                <p className="flex text-center">
                     Ready to Party? Contact us today to secure your date! 
                 </p>
                 <PartyForm />
