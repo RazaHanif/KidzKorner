8385edb15ac209fa1f8e71950cc11273437dcc05
Raza Hanif <raza_hanif@ymail.com>
2026-08-02 12:16:39 -0400

    2026 Q3 Improvements @ 12:16:39 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -228,7 +228,6 @@ const Home = () => {
                                         <h2 className="font-bold">
                                             Safe Environment
                                         </h2>
-
                                     </div>
                                     <p className="text-start pl-8">
                                         A secure, welcoming space where children can thrive.
