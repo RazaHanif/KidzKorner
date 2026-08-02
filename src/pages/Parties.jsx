3f3d38268a746e9ea7a1a13fb395ac2990601018
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 20:02:52 -0400

    2026 Q3 Improvements @ 8:02:52 PM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -126,7 +126,7 @@ const Parties = () => {
 
             <div className="flex flex-col justify-center items-center w-3/4 mb-4">
                 <h2>Why Families Love Our Birthday Parties</h2>
-                <div className="bg-red-500 w-full flex flex-row">
+                <div className="bg-red-500 w-full flex lg:flex-row flex-col">
                     <div>
                         <p>Private Venue Rental</p>
                         <p>Indoor Playground exclusive to your guests</p>
