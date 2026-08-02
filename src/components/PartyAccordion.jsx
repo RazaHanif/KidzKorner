42c5638f25d59d7086f32b0b6668cd45a18fb496
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 20:43:47 -0400

    2026 Q3 Improvements @ 8:43:47 PM

--- src/components/PartyAccordion.jsx
+++ src/components/PartyAccordion.jsx
@@ -26,7 +26,7 @@ const PartyAccordion = () => {
         },
         {
             q: "Do you provide food?",
-            a: "Yes. Families are welcome to bring their own food at no additional cost. We also provide access to a private kitchen space. Please note that our facility follows a strict nut-free policy."
+            a: "Our Platinum and VIP."
         },
         {
             q: "Can we bring our own Food?",
