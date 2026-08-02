02658a94e34c3c6ec9eda3651e2284ace6dda4e0
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 20:13:55 -0400

    2026 Q3 Improvements @ 8:13:55 PM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -201,7 +201,7 @@ const Parties = () => {
                     </div>
                     <div className="flex flex-col gap-2 w-full flex-1">
                         <p className="ml-4 flex gap-2">
-                            <Dot /> 1st Birthdays
+                            <Dot /> School
                         </p>
                         <p className="ml-4 flex gap-2">
                             <Dot /> 2nd Birthdays
