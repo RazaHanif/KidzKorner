1a5885cc830341578754a533edb2e16543094773
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:28:38 -0400

    2026 Q3 Improvements @ 10:28:38 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ import { localBusinessSchema, organizationSchema, websiteSchema } from "../schem
 
 const Home = () => {
     return (
-        <div className="flex flex-col flex-1 justify-center items-center gap-4 w-full lg:py-16">
+        <div className="flex flex-col flex-1 justify-center items-center gap-4 w-full lg:py-16 py-8">
             <div className="flex flex-col justify-center items-center w-full mb-4">
                 <h1 className="lg:text-5xl text-3xl font-bold font-serif text-center py-4">
                     Licensed Daycare & Childcare in Milton
