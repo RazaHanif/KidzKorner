75e65baf6cb0498a5635532d5eed3daa7439570c
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:29:43 -0400

    2026 Q3 Improvements @ 10:29:43 PM

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
     ];
 
     return (
-        <div className="flex flex-col justify-center items-center gap-2 w-full text-justify">
+        <div className="flex flex-col flex-1 justify-center items-center gap-4 w-full lg:py-16 pb-8">
             <div className="flex lg:flex-row flex-col justify-center w-3/4 m-4 gap-4">
                 <div>
                     <ContactUsTable />
