69028dffcff465e3b4e138845bd354c66aa43bd9
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:59:53 -0400

    2026 Q3 Improvements @ 10:59:53 PM

--- src/pages/ToddlerDaycare.jsx
+++ src/pages/ToddlerDaycare.jsx
@@ -18,8 +18,16 @@ const ToddlerDaycare = () => {
     ];
 
     return (
-        <div className="flex flex-col justify-center items-center gap-4 w-full text-justify">
+        <div className="flex flex-col flex-1 justify-center items-center gap-4 w-full lg:py-16 pb-8">
+            <div className="flex flex-col justify-center items-center w-full mb-4">
+                <h1 className="lg:text-5xl text-3xl font-bold font-serif text-center py-4">
+                    Licensed Daycare & Childcare in Milton
+                </h1>
+            </div>
 
+            <div className="flex flex-col justify-center items-center lg:w-3/4 w-full lg:text-justify text-center">
+                Stuff
+            </div>
 
             <StructData schema={localBusinessSchema} />
             <StructData schema={breadcrumbSchema(breadCrumb)} />
