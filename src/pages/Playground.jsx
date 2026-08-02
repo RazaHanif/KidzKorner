365646c2c3eccfc18c89db0f7a173807b30d0ac4
Raza Hanif <raza_hanif@ymail.com>
2026-08-02 11:31:40 -0400

    2026 Q3 Improvements @ 11:31:40 AM

--- src/pages/Playground.jsx
+++ src/pages/Playground.jsx
@@ -72,7 +72,7 @@ const Playground = () => {
                             </p>
                             <p className="flex items-start gap-3">
                                 <CheckCircle className="h-6 w-6 shrink-0 text-primary"/> 
-                                
+                                Active Play Areas
                             </p>
                             <p className="flex items-start gap-3">
                                 <CheckCircle className="h-6 w-6 shrink-0 text-primary"/> Preschool Birthdays
