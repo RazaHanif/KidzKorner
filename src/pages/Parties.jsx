f6bcc3b478f0f7d51aeec25cbce00ef53bc95bad
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 20:12:10 -0400

    2026 Q3 Improvements @ 8:12:10 PM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -183,7 +183,7 @@ const Parties = () => {
             </div>
 
             <div className="flex flex-col justify-center items-center w-3/4 mb-4">
-                <h2>Why Families Love Our Birthday Parties</h2>
+                <h2>Our birthday parties are perfect for:</h2>
                 <div className="w-full flex lg:flex-row flex-col p-2 gap-2">
                     <div className="flex flex-col gap-2 w-full flex-1">
                         <p className="ml-4 flex gap-2">
