bf7eea61f4a570bf47240d240cf0604fdf438fb5
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:25:12 -0400

    2026 Q3 Improvements @ 10:25:11 PM

--- src/pages/Daycare.jsx
+++ src/pages/Daycare.jsx
@@ -158,7 +158,7 @@ const Daycare = () => {
                 </div>
             </div>
 
-            <div className="flex flex-col justify-center w-3/4">
+            <div className="flex flex-col justify-center w-3/4 lg:py-16 py-8">
                 <Card>
                     <CardHeader>
                         <CardTitle className="p-2 bg-primary text-primary-foreground flex justify-center items-center rounded-md text-lg">
