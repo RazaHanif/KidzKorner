d0878c5bd2754fea6fe5982eb644fd018d7aca9d
Raza Hanif <raza_hanif@ymail.com>
2026-08-02 12:10:40 -0400

    2026 Q3 Improvements @ 12:10:40 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -175,7 +175,7 @@ const Home = () => {
                         <CardContent className="flex md:flex-row flex-col gap-2">
                             <div className="flex flex-col flex-1 gap-2">
                                 <div className="flex flex-col items-start justify-start w-full">
-                                    <h2 className="font-bold">
+                                    <h2 className="flex flex-row gap-2 font-bold">
                                         <BadgeCheck />
                                         Licensed Ontario Childcare
                                     </h2>
@@ -184,7 +184,7 @@ const Home = () => {
                                     </p>
                                 </div>
                                 <div className="flex flex-col items-start justify-start w-full">
-                                    <h2 className="font-bold">
+                                    <h2 className="flex flex-row gap-2 font-bold">
                                         <HeartHandshake />
                                         Family-Owned
                                     </h2>
@@ -193,7 +193,7 @@ const Home = () => {
                                     </p>
                                 </div>
                                 <div className="flex flex-col items-start justify-start w-full">
-                                    <h2 className="font-bold">
+                                    <h2 className="flex flex-row gap-2 font-bold">
                                         <GraduationCap />
                                         Experienced Educators
                                     </h2>
@@ -205,7 +205,7 @@ const Home = () => {
                             <div className="flex flex-col flex-1 gap-2">
 
                                 <div className="flex flex-col items-start justify-start w-full">
-                                    <h2 className="font-bold">
+                                    <h2 className="flex flex-row gap-2 font-bold">
                                         <Palette />
                                         Play-Based Learning
                                     </h2>
@@ -214,7 +214,7 @@ const Home = () => {
                                     </p>
                                 </div>
                                 <div className="flex flex-col items-start justify-start w-full">
-                                    <h2 className="font-bold">
+                                    <h2 className="flex flex-row gap-2 font-bold">
                                         <ShieldCheck />
                                         Safe Environment
                                     </h2>
