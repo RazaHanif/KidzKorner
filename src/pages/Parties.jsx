d9690bb953d26b48084a3a1c95b3ab93e2599393
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 20:39:16 -0400

    2026 Q3 Improvements @ 8:39:16 PM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -220,8 +220,12 @@ const Parties = () => {
                 </div>
             </div>
 
-            
 
+
+            <div className="flex flex-col justify-center items-center w-3/4 gap-4">
+                <PartyForm />
+            </div>
+            
             <div className="flex flex-col justify-center items-center w-3/4 gap-4">
                 <PartyForm />
             </div>
