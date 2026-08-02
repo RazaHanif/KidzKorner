3847bca001498b11cce70e57fa2de9493ebfd69f
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 20:10:26 -0400

    2026 Q3 Improvements @ 8:10:26 PM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -129,7 +129,7 @@ const Parties = () => {
                 <h2>Why Families Love Our Birthday Parties</h2>
                 <div className="w-full flex lg:flex-row flex-col p-2 gap-2">
                     <div className="flex flex-col gap-2 w-full flex-1">
-                        <div>
+                        <div className="flex gap-2">
                             <CircleCheck />
                             <p className="ml-4">    
                                 Private Venue Rental
