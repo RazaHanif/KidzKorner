708f59ef1b2d699d8659442c54a2619efe0b4464
Raza Hanif <raza_hanif@ymail.com>
2026-08-02 11:15:32 -0400

    2026 Q3 Improvements @ 11:15:32 AM

--- src/pages/Playground.jsx
+++ src/pages/Playground.jsx
@@ -50,7 +50,19 @@ const Playground = () => {
                 <PlaygroundAccordion />
             </div>
 
-            
+            <Card>
+  <CardHeader>
+    <CardTitle>Card Title</CardTitle>
+    <CardDescription>Card Description</CardDescription>
+    <CardAction>Card Action</CardAction>
+  </CardHeader>
+  <CardContent>
+    <p>Card Content</p>
+  </CardContent>
+  <CardFooter>
+    <p>Card Footer</p>
+  </CardFooter>
+</Card>
 
 {/* Make this better */}
             <div className="border-y py-8">
