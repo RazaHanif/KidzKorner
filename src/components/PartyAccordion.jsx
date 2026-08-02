528c051fc1e52bcd4717874608707b87600a5be0
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 20:30:50 -0400

    2026 Q3 Improvements @ 8:30:50 PM

--- src/components/PartyAccordion.jsx
+++ src/components/PartyAccordion.jsx
@@ -9,12 +9,12 @@ import {
 const PartyAccordion = () => {
     return (
         <Accordion defaultValue={["item-1"]}>
-        <AccordionItem value="item-1">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
-        <AccordionContent>
-        Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
-        </AccordionItem>
+            <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+            </AccordionItem>
         </Accordion>
     )
 
