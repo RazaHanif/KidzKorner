a6ba316e082aa529dd5fc8207544c0fa06f8e137
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:35:33 -0400

    2026 Q3 Improvements @ 10:35:33 PM

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -15,6 +15,28 @@ const NotFound = () => {
                 <p>Oops! This page ran off to play!</p>
             </div>
 
+            <div className="w-full flex lg:flex-col gap-4 justify-center lg:items-end lg:pr-4 items-center">
+                            <NavLink 
+                                to="/about" 
+                                end
+                                className="hover:scale-105 ease-in-out duration-300 flex-1 lg:w-3/4 flex justify-center lg:justify-end"
+                            >
+                                <Button className="cursor-pointer min-w-1/2">
+                                    Schedule Tour
+                                </Button>
+                            </NavLink>
+
+                            <NavLink 
+                                to="/daycare" 
+                                end
+                                className="hover:scale-105 ease-in-out duration-300 flex-1 lg:w-3/4 flex justify-center lg:justify-end"
+                            >
+                                <Button className="cursor-pointer min-w-1/2">
+                                    Explore Programs
+                                </Button>
+                            </NavLink>
+                        </div>
+
             <div className="w-full flex flex-col justify-start items-center">
                 <Button
                     variant="btn"
