943c67829f8cda7b2c4eabdad2e64791b9b9ebee
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:53:57 -0400

    2026 Q3 Improvements @ 10:53:57 PM

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -59,7 +59,7 @@ const About = () => {
                 </div>
             </div>
             
-            <div className="flex flex-col justify-center items-center w-3/4 gap-4 lg:pt-16 pt-8">
+            <div className="flex flex-col justify-center items-center w-3/4 gap-4">
                 <p className="flex w-2/3 text-center">
                     Contact us today to lock in your spot! 
                 </p> 
