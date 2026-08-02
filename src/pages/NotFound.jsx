2f12da24e3fadadba1c2ce95cfd4f5090879f774
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:29:32 -0400

    2026 Q3 Improvements @ 10:29:32 PM

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col flex-1 justify-center items-center w-full gap-8 py-16">
+    <div className="flex flex-col flex-1 justify-center items-center gap-4 w-full lg:py-16 pb-8">
       <h1 className="w-full flex flex-col justify-end items-center text-6xl font-bold font-serif">
         404
       </h1>
