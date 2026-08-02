3de005658aff7d430ee5f71e416a9147190d573e
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 20:26:03 -0400

    2026 Q3 Improvements @ 8:26:03 PM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -184,7 +184,7 @@ const Parties = () => {
                 </div>
             </div>
 
-            <div className="flex flex-col justify-center items-center w-3/4 my-4 border-t border-b">
+            <div className="flex flex-col justify-center items-center w-3/4 border-t border-b">
                 <h2 className="text-xl font-bold text-center pb-4">
                     Our birthday parties are perfect for:
                 </h2>
