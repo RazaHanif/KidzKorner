db3bc6244324c4d4d63265e57a6e4d6c31cf528b
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:24:32 -0400

    2026 Q3 Improvements @ 10:24:32 PM

--- src/pages/Daycare.jsx
+++ src/pages/Daycare.jsx
@@ -71,7 +71,7 @@ const Daycare = () => {
                 </Card>
             </div>
 
-            <div className="flex flex-col justify-center items-center w-3/4">
+            <div className="flex flex-col justify-center items-center w-3/4 my-">
                 <h2 className="text-xl font-bold text-center pb-4">
                     Program Options - Full Time & Part Time Care
                 </h2>
