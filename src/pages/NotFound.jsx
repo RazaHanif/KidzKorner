82bdc906f58a1d4489e4b7f54d32856bc78bd8c0
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:36:09 -0400

    2026 Q3 Improvements @ 10:36:09 PM

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -22,7 +22,7 @@ const NotFound = () => {
                     className="hover:scale-105 ease-in-out duration-300 flex-1 lg:w-3/4 flex justify-center lg:justify-end"
                 >
                     <Button className="cursor-pointer min-w-1/2">
-                        Schedule Tour
+                        Go Home!
                     </Button>
                 </NavLink>
 
