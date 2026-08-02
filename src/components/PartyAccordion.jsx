93789ac30665f7fe782b584973d78bdfbd0d05f6
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 20:38:16 -0400

    2026 Q3 Improvements @ 8:38:16 PM

--- src/components/PartyAccordion.jsx
+++ src/components/PartyAccordion.jsx
@@ -7,64 +7,27 @@ import {
 
 
 const PartyAccordion = () => {
+    const faq = [
+        {
+            q: "Can we bring our own Food?",
+            a: "Yes. Families are welcome to bring their own food at no additional cost. We also provide access to a private kitchen space. Please note that our facility follows a strict nut-free policy."
+        },
+
+    ]
+
     return (
-        <Accordion defaultValue={["item-1"]}>
+        <Accordion defaultValue={[1]}>
 
-            
-
-            <AccordionItem value="item-1">
-                <AccordionTrigger>
-                    Can we bring our own Food?
-                </AccordionTrigger>
-                <AccordionContent>
-                    Yes. Families are welcome to bring their own food at no additional cost. We also provide access to a private kitchen space. Please note that our facility follows a strict nut-free policy.
-                </AccordionContent>
-            </AccordionItem>
-
-            <AccordionItem value="item-1">
-                <AccordionTrigger>
-                    Can we bring our own Food?
-                </AccordionTrigger>
-                <AccordionContent>
-                    Yes. Families are welcome to bring their own food at no additional cost. We also provide access to a private kitchen space. Please note that our facility follows a strict nut-free policy.
-                </AccordionContent>
-            </AccordionItem>
-
-            <AccordionItem value="item-1">
-                <AccordionTrigger>
-                    Can we bring our own Food?
-                </AccordionTrigger>
-                <AccordionContent>
-                    Yes. Families are welcome to bring their own food at no additional cost. We also provide access to a private kitchen space. Please note that our facility follows a strict nut-free policy.
-                </AccordionContent>
-            </AccordionItem>
-
-            <AccordionItem value="item-1">
-                <AccordionTrigger>
-                    Can we bring our own Food?
-                </AccordionTrigger>
-                <AccordionContent>
-                    Yes. Families are welcome to bring their own food at no additional cost. We also provide access to a private kitchen space. Please note that our facility follows a strict nut-free policy.
-                </AccordionContent>
-            </AccordionItem>
-
-            <AccordionItem value="item-1">
-                <AccordionTrigger>
-                    Can we bring our own Food?
-                </AccordionTrigger>
-                <AccordionContent>
-                    Yes. Families are welcome to bring their own food at no additional cost. We also provide access to a private kitchen space. Please note that our facility follows a strict nut-free policy.
-                </AccordionContent>
-            </AccordionItem>
-
-            <AccordionItem value="item-1">
-                <AccordionTrigger>
-                    Can we bring our own Food?
-                </AccordionTrigger>
-                <AccordionContent>
-                    Yes. Families are welcome to bring their own food at no additional cost. We also provide access to a private kitchen space. Please note that our facility follows a strict nut-free policy.
-                </AccordionContent>
-            </AccordionItem>
+            {faq.map((item, index) => (
+                <AccordionItem value={index + 1}>
+                    <AccordionTrigger>
+                        {item.q}
+                    </AccordionTrigger>
+                    <AccordionContent>
+                        {item.a}
+                    </AccordionContent>
+                </AccordionItem>
+            ))}
         </Accordion>
     )
 
