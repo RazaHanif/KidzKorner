9029733f7d1486c32a43400b993238ed203c52c6
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 20:57:38 -0400

    2026 Q3 Improvements @ 8:57:38 PM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -230,7 +230,7 @@ const Parties = () => {
 
             <div className="flex flex-col justify-center items-center w-3/4 gap-4">
                 <h2 className="text-xl font-bold text-center pb-4">
-                    FAQ
+                    A Private Indoor Birthday Party Venue in Milton
                 </h2>
                 
             </div>
