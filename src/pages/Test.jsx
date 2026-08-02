a18446b26198a0de88af127e30c18ee8c6f5a0f8
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:44:34 -0400

    2026 Q3 Improvements @ 10:44:34 PM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -4,9 +4,6 @@ const Test = () => {
     return (
         <div className="flex flex-col flex-1 justify-center items-center gap-4 w-full lg:py-16 pb-8">
             <div className="flex flex-col justify-center items-center w-full">
-                <h1 className="text-6xl font-bold font-serif text-center py-4">
-                    404
-                </h1>
                 <h1 className="text-3xl font-bold">
                     The Sandbox
                 </h1>
