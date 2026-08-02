c8723ac5922c5b7e1b23d291547d1a15cdf0682b
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:38:48 -0400

    2026 Q3 Improvements @ 10:38:48 PM

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,7 +5,7 @@ const NotFound = () => {
     return (
         <div className="flex flex-col flex-1 justify-center items-center gap-4 w-full lg:py-16 pb-8 text-primary">
             <div className="flex flex-col justify-center items-center w-full">
-                <h1 className="text-6xl font-bold font-serif text-center py-4 text-shadow-amber-500 text-shadow-md">
+                <h1 className="text-6xl font-bold font-serif text-center py-4 text-shadow-md">
                     404
                 </h1>
             </div>
