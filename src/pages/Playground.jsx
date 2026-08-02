89bcf6607d388b751be43e07a5bda606338cc45f
Raza Hanif <raza_hanif@ymail.com>
2026-08-02 11:37:30 -0400

    2026 Q3 Improvements @ 11:37:30 AM

--- src/pages/Playground.jsx
+++ src/pages/Playground.jsx
@@ -99,7 +99,7 @@ const Playground = () => {
 
 
             <div className="flex lg:flex-row flex-col justify-center items-center w-3/4 lg:pt-16 pt-8">
-                <Image src='/20250930_142748.jpg' alt="Children playing on the indoor playground at Kidz Korner in Milton Ontario" />
+                <Image src='/20250930_142748.jpg' alt="Children playing on the indoor playground at Kidz Korner in Milton Ontario" w="w-md"/>
                 <Image src='/20250930_142442.jpg' alt="Indoor climbing structure at Kidz Korner playground in Milton Ontario" />
             </div>
 
