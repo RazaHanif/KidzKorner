41ca131a713236cd64bcd2df35c784a86016f064
Raza Hanif <raza_hanif@ymail.com>
2026-08-01 22:29:57 -0400

    2026 Q3 Improvements @ 10:29:57 PM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -24,7 +24,7 @@ const Parties = () => {
     return (
         <div className="flex flex-col flex-1 justify-center items-center gap-4 w-full lg:py-16 pb-8">
             <div className="flex flex-col justify-center items-center w-full">
-                <h1 className="text-3xl font-bold text-center font-serif">
+                <h1 className="lg:text-5xl text-3xl font-bold text-center font-serif">
                     Kids Birthday Parties in Milton
                 </h1>
             </div>
