9cce48628b20f79cae6e424c76905b067e312de6
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 17:29:01 -0400

    2026 Q3 Improvements @ 5:29:01 PM

--- src/pages/Camp.jsx
+++ src/pages/Camp.jsx
@@ -271,7 +271,7 @@ const Camp = () => {
                         </p>
                         <p className="flex items-start gap-3">
                             <ChefHat className="mt-1 h-5 w-5 shrink-0 text-primary"/>
-                            Indoor Playground exclusive to your guests
+                            Hands-on Cooking Classes
                         </p>
                         <p className="flex items-start gap-3">
                             <Heart className="mt-1 h-5 w-5 shrink-0 text-primary"/>
