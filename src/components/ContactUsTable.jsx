60acdf56d00c58ae7b1487cc815e0b7ef29c2efb
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 19:18:56 -0400

    2026 Q3 Improvements @ 7:18:56 PM

--- src/components/ContactUsTable.jsx
+++ src/components/ContactUsTable.jsx
@@ -47,7 +47,7 @@ const ContactUsTable = () => {
                                     href={item.href}
                                     className="hover:underline flex flex-row w-full flex-1"
                                 >
-                                    <div className="text-end w-full border-2">
+                                    <div className="text-end w-full">
                                         {item.text}
                                     </div>
                                 </a>
