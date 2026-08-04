4a0b5a5a26447db4840a78dbda4eeb6665c0d7cd
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 17:22:03 -0400

    2026 Q3 Improvements @ 5:22:03 PM

--- src/pages/Camp.jsx
+++ src/pages/Camp.jsx
@@ -222,7 +222,7 @@ const Camp = () => {
                 </div>
             </div>
 
-            <div className="flex flex-col justify-center w-3/4 lg:py-16 py-8 border-y">
+            <div className="flex flex-col justify-center items-center w-3/4 lg:py-16 py-8 border-y">
                 <Card className="lg:w-1/3">
                     <CardHeader>
                         <CardTitle className="p-2 bg-primary text-primary-foreground flex justify-center items-center rounded-md text-lg">
