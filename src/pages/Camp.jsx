7f9f39f86e3166834fae56e794487316e1633280
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 17:25:53 -0400

    2026 Q3 Improvements @ 5:25:53 PM

--- src/pages/Camp.jsx
+++ src/pages/Camp.jsx
@@ -259,7 +259,7 @@ const Camp = () => {
                 <Image src='/20250722_100154.jpg' alt='Child Painting at Kidz Korner Milton, Ontario' />
             </div>
 
-                        <div className="flex flex-col justify-center items-center w-3/4 border-y py-10 my-8">
+            <div className="flex flex-col justify-center items-center w-3/4 border-y py-10 my-8">
                 <h2 className="text-xl font-bold text-center pb-4">
                     Why Families Love Our Birthday Parties
                 </h2>
