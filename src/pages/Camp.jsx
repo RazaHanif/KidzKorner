9da6bc42c0e84417cc97668c21db3bd076924fc9
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 17:46:23 -0400

    2026 Q3 Improvements @ 5:46:23 PM

--- src/pages/Camp.jsx
+++ src/pages/Camp.jsx
@@ -241,7 +241,7 @@ const Camp = () => {
                             </ul>
                         </CardContent>
                     </Card>
-                    <Image src='/20250711_111518.jpg' alt='Child Playing With Dinosaurs at Kidz Korner Milton, Ontario' className/>
+                    <Image src='/20250711_111518.jpg' alt='Child Playing With Dinosaurs at Kidz Korner Milton, Ontario' className="w-full" />
                 </div>
             </div>
 
