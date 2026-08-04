73c257c81988aa38589f5cb3a6b15bcf9c974f66
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 17:40:17 -0400

    2026 Q3 Improvements @ 5:40:17 PM

--- src/pages/Camp.jsx
+++ src/pages/Camp.jsx
@@ -205,7 +205,7 @@ const Camp = () => {
                                     </span>
                                     <ul className="font-normal pl-6">
                                         <li>
-                                            Children bring a nut-free lunch and snacks every day
+                                            Children bring nut-free lunch and snacks every day
                                         </li>
                                         <li>
                                             9:00AM - 5:00PM
