7ae892654a17c1bcf5c2a71c5a81b880155ce997
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 18:45:24 -0400

    2026 Q3 Improvements @ 6:45:24 PM

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -67,7 +67,11 @@ const About = () => {
                         Visit Our Childcare Centre
                     </h2>
                     <p>
-                        
+                        Kidz Korner is conveniently located at
+150 Nipissing Road in Milton, making it easy
+for families across Milton to access quality
+licensed childcare, preschool, camps,
+birthday parties and our indoor playground.
                     </p>
 
                 </div>
