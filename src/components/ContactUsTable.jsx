b72e812e40b8bf92c38223d979179d71f12fa138
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 18:57:25 -0400

    2026 Q3 Improvements @ 6:57:25 PM

--- src/components/ContactUsTable.jsx
+++ src/components/ContactUsTable.jsx
@@ -43,7 +43,6 @@ const ContactUsTable = () => {
             </TableHeader>
             <TableBody>
                 {timetable.map((item, idx) => {
-
                     const Icon = item.icon
                     
                     return (
