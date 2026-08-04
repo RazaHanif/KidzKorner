99d9cc4e6f4f24e2c8574c0cc91e16c844d53233
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 17:30:55 -0400

    2026 Q3 Improvements @ 5:30:55 PM

--- src/pages/Camp.jsx
+++ src/pages/Camp.jsx
@@ -281,7 +281,7 @@ const Camp = () => {
                     <div className="flex flex-col gap-2 w-full flex-1">
                         <p className="flex items-start gap-3">
                             <Shapes className="mt-1 h-5 w-5 shrink-0 text-primary"/>
-                            Up to 50 guests
+                            Active Indoor Play
                         </p>
                         <p className="flex items-start gap-3">
                             <Heart className="mt-1 h-5 w-5 shrink-0 text-primary"/>
