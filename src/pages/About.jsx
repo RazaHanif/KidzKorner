c91985128efdea037b2e97f5321f2d6a000f5297
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 19:02:16 -0400

    2026 Q3 Improvements @ 7:02:16 PM

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -59,7 +59,8 @@ const About = () => {
                     Who We Are
                 </h2>
                 <p className="flex lg:flex-row flex-col justify-center items-center lg:text-justify text-center">
-                    Estab
+                    Established in 1995, Kidz Korner has proudly served Milton families for over 30 years.
+                    Founded 
                 </p>            
             </div>
 
