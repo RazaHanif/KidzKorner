4bed1e10f1984c314cad80ed7a554d3014ed9309
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 17:25:17 -0400

    2026 Q3 Improvements @ 5:25:17 PM

--- src/pages/Camp.jsx
+++ src/pages/Camp.jsx
@@ -89,7 +89,7 @@ const Camp = () => {
                         </NavLink>
 
                         <NavLink
-                            to="/camp/winter-camp" 
+                            to="/camp/winter-break-camp" 
                             end 
                             className="hover:scale-102 ease-in-out duration-300 flex flex-1"
                         >
