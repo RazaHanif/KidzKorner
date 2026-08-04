1453c05a90f38d86ae599cd33241deed3ac57d06
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 19:10:47 -0400

    2026 Q3 Improvements @ 7:10:47 PM

--- src/components/ContactUsTable.jsx
+++ src/components/ContactUsTable.jsx
@@ -32,6 +32,13 @@ const ContactUsTable = () => {
         <div className="border">
             <h1>Contact Us</h1>
             <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className='text-xl font-semibold'>
+                            Contact Us
+                        </TableHead>
+                    </TableRow>
+                </TableHeader>
                 <TableBody>
                     {timetable.map((item, idx) => (
                         <TableRow key={idx}>
