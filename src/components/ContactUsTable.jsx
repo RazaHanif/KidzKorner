0f6edb0e7fa0fa557ac4c4f3e2fc5833da219b7e
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 19:14:06 -0400

    2026 Q3 Improvements @ 7:14:06 PM

--- src/components/ContactUsTable.jsx
+++ src/components/ContactUsTable.jsx
@@ -40,7 +40,7 @@ const ContactUsTable = () => {
                             <TableCell className='font-semibold'>
                                 {item.title}
                             </TableCell>
-                            <TableCell  className="whitespace-normal wrap-break-word ">
+                            <TableCell  className="whitespace-normal wrap-break-word bg-blue-500">
                                 <a
                                     target="_blank"
                                     rel="noopener noreferrer" 
