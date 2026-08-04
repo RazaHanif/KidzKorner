ddac1eda84736e8ee047bf93c23d25990f289974
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 17:12:07 -0400

    2026 Q3 Improvements @ 5:12:06 PM

--- src/pages/Camp.jsx
+++ src/pages/Camp.jsx
@@ -52,7 +52,7 @@ const Camp = () => {
                                         Summer Camp
                                     </CardTitle>
                                     <CardDescription>
-                                        8-10 weeks of themed adventures packed with art, cooking, science and special activites
+                                        8-10 weeks of themed adventures packed with art, cooking, science and special activities
                                     </CardDescription>
                                 </CardHeader>
                                 <CardContent className="flex-1 flex justify-end items-end">
