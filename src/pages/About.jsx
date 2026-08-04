5ece6f77bb613faab08bddfeac81862a81e855d2
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 18:36:39 -0400

    2026 Q3 Improvements @ 6:36:39 PM

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -108,7 +108,7 @@ const About = () => {
 
                             <div className="flex flex-col items-start justify-start w-full">
                                 <div className="flex gap-2 text-primary">
-                                    <User />
+                                    <Users />
                                     <h2 className="font-bold">
                                         Indoor Playground
                                     </h2>
