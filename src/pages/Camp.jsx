3b3c46092ada0d22a6f72d055cda9bb009f9fb9a
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 17:22:21 -0400

    2026 Q3 Improvements @ 5:22:21 PM

--- src/pages/Camp.jsx
+++ src/pages/Camp.jsx
@@ -223,7 +223,7 @@ const Camp = () => {
             </div>
 
             <div className="flex flex-col justify-center items-center w-3/4 lg:py-16 py-8 border-y">
-                <Card className="lg:w-1/3 w-full flex-1">
+                <Card className="lg:w-2/3 w-full flex-1">
                     <CardHeader>
                         <CardTitle className="p-2 bg-primary text-primary-foreground flex justify-center items-center rounded-md text-lg">
                             Camp Schedule
