9a350e29cae6c5178fc91496a93b6bb38a64e65b
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 19:31:44 -0400

    2026 Q3 Improvements @ 7:31:43 PM

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -155,7 +155,7 @@ const About = () => {
                     Book a Tour or Ask a Question
                 </h2>
                 <p className="flex text-center w-3/4">
-                    Whether your're looking for daycare, preschool, camps, or birthday parties, our team is happy to answer your questions.
+                    Whether you're looking for daycare, preschool, camps, or birthday parties, our team is happy to answer your questions.
                     Complete the form below and we'll get back to you as soon as possible.
                 </p> 
                 <ContactForm/>
