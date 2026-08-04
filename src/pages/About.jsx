a472248093f7f73bcc1938373df7dc065c99ef54
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 18:42:09 -0400

    2026 Q3 Improvements @ 6:42:09 PM

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -61,7 +61,7 @@ const About = () => {
             
             </div>
 
-            <div className="flex flex-col justify-center items-center w-full lg:py-16 py-8">
+            <div className="flex flex-col justify-center items-center w-full">
                 <h2 className="text-xl font-bold text-center">
                     Visit Our Childcare Centre
                 </h2>
