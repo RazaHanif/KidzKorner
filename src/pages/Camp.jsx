c9fe17c8a47baf38f162869c70f3877a38ad7b0e
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 17:29:36 -0400

    2026 Q3 Improvements @ 5:29:36 PM

--- src/pages/Camp.jsx
+++ src/pages/Camp.jsx
@@ -277,14 +277,6 @@ const Camp = () => {
                             <TestTube2 className="mt-1 h-5 w-5 shrink-0 text-primary"/>
                             Weekly STEM Activities
                         </p>
-                        <p className="flex items-start gap-3">
-                            <Heart className="mt-1 h-5 w-5 shrink-0 text-primary"/>
-                            Private Kitchen Available
-                        </p>
-                        <p className="flex items-start gap-3">
-                            <Heart className="mt-1 h-5 w-5 shrink-0 text-primary"/>
-                            Decorations Welcome
-                        </p>
                     </div>
                     <div className="flex flex-col gap-2 w-full flex-1">
                         <p className="flex items-start gap-3">
