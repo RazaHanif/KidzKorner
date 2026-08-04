e847a28bd08ee3941b64f399ba127f939d7bb6b9
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 17:13:30 -0400

    2026 Q3 Improvements @ 5:13:30 PM

--- src/pages/Camp.jsx
+++ src/pages/Camp.jsx
@@ -97,7 +97,7 @@ const Camp = () => {
                                         Before & After School Program
                                     </CardTitle>
                                     <CardDescription>
-                                        Holiday fun filled with crafts, cooking, indoor games and winter activities
+                                        Holiday fun filled with crafts, cooking, indoor games and winter themed activities
                                     </CardDescription>
                                 </CardHeader>
                                 <CardContent className="flex-1 flex justify-end items-end">
