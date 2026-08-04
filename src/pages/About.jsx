db3127414249f03b884f74764d2733954948bbca
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 19:28:42 -0400

    2026 Q3 Improvements @ 7:28:42 PM

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -29,7 +29,7 @@ const About = () => {
                     About Kidz Korner Childcare & Indoor Playground
                 </h1>
                 <p className="flex lg:flex-row flex-col justify-center items-center text-center lg:w-3/4 w-full">
-                    Serving Milton families with licensed childcare, preschool programs, indoor playground fun, birthday parties, camp and workshops.
+                    Serving Milton families with licensed childcare, preschool programs, indoor playground fun, birthday parties, camps and workshops.
                 </p>
             </div>
 
