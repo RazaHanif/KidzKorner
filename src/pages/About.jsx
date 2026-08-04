d4e764edc61556674314918c1980262a261f08b3
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 19:30:15 -0400

    2026 Q3 Improvements @ 7:30:14 PM

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -57,7 +57,7 @@ const About = () => {
                 </h2>
                 <p className="flex lg:flex-row flex-col justify-center items-center text-center lg:w-3/4 w-full">
                     Established in 1995, Kidz Korner has proudly served Milton families for over 30 years.
-                    Our centre was built on creating a safe, caring and engaging environment where children can learn, play, and grow.
+                    Our centre was built on creating a safe, caring, and engaging environment where children can learn, play, and grow.
                 </p>   
                 <p className="flex lg:flex-row flex-col justify-center items-center text-center lg:w-3/4 w-full">
                     With decades of childcare experience, our dedicated team provides licensed childcare, indoor playground experiences, camps and workshops designed to support children's development while giving families peace of mind.
