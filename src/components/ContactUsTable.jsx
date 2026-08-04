910ab147155041ba06c7ca2dce8479ec2114884b
Raza Hanif <raza_hanif@ymail.com>
2026-08-04 19:08:32 -0400

    2026 Q3 Improvements @ 7:08:32 PM

--- src/components/ContactUsTable.jsx
+++ src/components/ContactUsTable.jsx
@@ -32,7 +32,7 @@ const ContactUsTable = () => {
         <Table className="border">
             <TableHeader className="text-center border-red-500 border">
                 <TableRow className="text-center border-blue-500 border">
-                    <TableHead className='text-xl font-semibold'>
+                    <TableHead className='text-xl font-semibold '>
                         Contact Us
                     </TableHead>
                 </TableRow>
