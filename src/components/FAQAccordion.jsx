47564cf6ddb17a7ede19280978d33dd36c5d95ec
Raza Hanif <raza_hanif@ymail.com>
2026-08-05 16:02:39 -0400

    2026 Q3 Improvements @ 4:02:39 PM

--- src/components/FAQAccordion.jsx
+++ src/components/FAQAccordion.jsx
@@ -6,34 +6,7 @@ import {
 } from "@/components/ui/accordion"
 
 
-const FAQAccordion = () => {
-    const faq = [
-        {
-            q: "Can we bring our own food?",
-            a: "Yes. Families are welcome to bring their own food at no additional cost. We also provide access to a private kitchen space. Please note that our facility follows a strict nut-free policy."
-        },
-        {
-            q: "Is the playground private?",
-            a: "Yes. During your booking, the indoor playground is reserved exclusively for your party and invited guests."
-        },
-        {
-            q: "How many guests can attend?",
-            a: "Our party packages accommodate up to 50 guests."
-        },
-        {
-            q: "Can we decorate?",
-            a: "Absolutely! Your're welcome to bring your own decorations to personalize the space."
-        },
-        {
-            q: "Do you provide food?",
-            a: "Our Platinum and VIP packages include pizza, while all packages allow you to bring your own food."
-        },
-        {
-            q: "Can we stay longer?",
-            a: "Additional party time may be available for $50 per hour, subject to availability."
-        },
-
-    ]
+const FAQAccordion = ({ faq }) => {
 
     return (
         <Accordion className="w-full">
