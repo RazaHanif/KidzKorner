7f2f7c4de77a5638ddde3247ed4fd1a2d314b264
Raza Hanif <raza_hanif@ymail.com>
2026-08-05 16:29:43 -0400

    2026 Q3 Improvements @ 4:29:43 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -171,8 +171,6 @@ const Home = () => {
                         <Card>
                         <CardHeader>
                         <CardTitle>Card Title</CardTitle>
-                        <CardDescription>Card Description</CardDescription>
-                        <CardAction>Card Action</CardAction>
                         </CardHeader>
                         <CardContent>
                             <p>Card Content</p>
