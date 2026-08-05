cdcc2cf4af2ded09ab56d1a25e53075e9b3ca88b
Raza Hanif <raza_hanif@ymail.com>
2026-08-05 17:09:23 -0400

    2026 Q3 Improvements @ 5:09:23 PM

--- src/pages/ToddlerDaycare.jsx
+++ src/pages/ToddlerDaycare.jsx
@@ -81,7 +81,7 @@ const ToddlerDaycare = () => {
                         <NavLink 
                             to="#" 
                             end
-                            className="hover:scale-105 ease-in-out duration-300 flex-1 lg:w-3/4 flex justify-center lg:justify-end"
+                            className="hover:scale-105 ease-in-out duration-300 flex-1 lg:w-3/4 flex justify-center"
                         >
                             <Button className="cursor-pointer min-w-1/2">
                                 Schedule Tour
@@ -91,7 +91,7 @@ const ToddlerDaycare = () => {
                         <NavLink 
                             to="#" 
                             end
-                            className="hover:scale-105 ease-in-out duration-300 flex-1 lg:w-3/4 flex justify-center lg:justify-end"
+                            className="hover:scale-105 ease-in-out duration-300 flex-1 lg:w-3/4 flex justify-center"
                         >
                             <Button className="cursor-pointer min-w-1/2">
                                 Contact Us
