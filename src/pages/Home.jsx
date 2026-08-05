fcdd06b2f5dc07c40d6b8d4606354832c958c53b
Raza Hanif <raza_hanif@ymail.com>
2026-08-05 16:29:29 -0400

    2026 Q3 Improvements @ 4:29:29 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -169,18 +169,18 @@ const Home = () => {
                     </h2>
                     <div>
                         <Card>
-  <CardHeader>
-    <CardTitle>Card Title</CardTitle>
-    <CardDescription>Card Description</CardDescription>
-    <CardAction>Card Action</CardAction>
-  </CardHeader>
-  <CardContent>
-    <p>Card Content</p>
-  </CardContent>
-  <CardFooter>
-    <p>Card Footer</p>
-  </CardFooter>
-</Card>
+                        <CardHeader>
+                        <CardTitle>Card Title</CardTitle>
+                        <CardDescription>Card Description</CardDescription>
+                        <CardAction>Card Action</CardAction>
+                        </CardHeader>
+                        <CardContent>
+                        <p>Card Content</p>
+                        </CardContent>
+                        <CardFooter>
+                        <p>Card Footer</p>
+                        </CardFooter>
+                        </Card>
                     </div>
 
  
