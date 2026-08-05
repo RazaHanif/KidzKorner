d9ceff3fff029a148d73e915b27abd06de065c77
Raza Hanif <raza_hanif@ymail.com>
2026-08-05 16:27:06 -0400

    2026 Q3 Improvements @ 4:27:06 PM

--- src/pages/ToddlerDaycare.jsx
+++ src/pages/ToddlerDaycare.jsx
@@ -87,7 +87,6 @@ const ToddlerDaycare = () => {
                     <div className="flex flex-col justify-center m-4 lg:m-0">
                         <CareSchedule />
                     </div>
-                    <Image src='/20250822_121126.jpg' alt='Child Playing With Blocks at Kidz Korner Milton, Ontario' w="w-sm"/>
                 </div>
             </div>
 
