dfaadc916949ae53c06e631b41830f0a9917ee50
Raza Hanif <raza_hanif@ymail.com>
2026-08-05 16:56:33 -0400

    2026 Q3 Improvements @ 4:56:33 PM

--- src/pages/ToddlerDaycare.jsx
+++ src/pages/ToddlerDaycare.jsx
@@ -180,7 +180,7 @@ const ToddlerDaycare = () => {
                         </p>
                         <p className="flex items-start gap-3">
                             <CircleCheck className="mt-1 h-5 w-5 shrink-0 text-primary"/>
-                            Bring your own food
+                            Building Friendships
                         </p>
                     </div>
                     <div className="flex flex-col gap-2 w-full flex-1">
