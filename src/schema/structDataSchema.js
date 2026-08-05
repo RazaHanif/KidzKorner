9c09dc4be0e5455611cbc50de5040c038235bc64
Raza Hanif <raza_hanif@ymail.com>
2026-08-05 15:58:46 -0400

    2026 Q3 Improvements @ 3:58:46 PM

--- src/schema/structDataSchema.js
+++ src/schema/structDataSchema.js
@@ -214,7 +214,7 @@ export const partyFAQSchema = {
         },
         {
             "@type": "Question",
-            "name": "How many guests can attend?",
+            "name": "How attend?",
             "acceptedAnswer": {
             "@type": "Answer",
             "text": "Our party packages accommodate up to 50 guests."
