2a1b8bd0fc303c91af050607cb173b3064e70162
Raza Hanif <raza_hanif@ymail.com>
2026-08-05 17:04:25 -0400

    2026 Q3 Improvements @ 5:04:25 PM

--- src/pages/ToddlerDaycare.jsx
+++ src/pages/ToddlerDaycare.jsx
@@ -67,11 +67,11 @@ const ToddlerDaycare = () => {
                 <h1 className="lg:text-5xl text-3xl font-bold font-serif text-center py-4">
                     Toddler Daycare in Milton
                 </h1>
-                <h2 className="text-center w-3/4">
-                    A nurturing and engaging toddler daycare program in Milton designed to support your child's early development through play, exploration, and age-appropriate learning.
-                </h2>
-                
                 <div>
+                    <h2 className="text-center w-3/4">
+                        A nurturing and engaging toddler daycare program in Milton designed to support your child's early development through play, exploration, and age-appropriate learning.
+                    </h2>
+                
                     <Button>
                         Schedule a Tour
                     </Button>
