5cd26b69d10ac90dd4ef8ecb222e2b313b835574
Raza Hanif <raza_hanif@ymail.com>
2026-08-05 17:09:10 -0400

    2026 Q3 Improvements @ 5:09:10 PM

--- src/pages/ToddlerDaycare.jsx
+++ src/pages/ToddlerDaycare.jsx
@@ -77,7 +77,7 @@ const ToddlerDaycare = () => {
                         Our commitment to nurturing children in a safe, engaging environment has earned the trust of Milton families for generations.
                     </p>
 
-                    <div className="w-3/4 flex lg:flex-col gap-4 justify-center lg:pr-4 items-center border">
+                    <div className="w-full flex lg:flex-col gap-4 justify-center items-center border">
                         <NavLink 
                             to="#" 
                             end
