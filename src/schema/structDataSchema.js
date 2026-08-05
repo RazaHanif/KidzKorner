a4d412e91c5f9429ed4857877d1fbb70a1fe55ac
Raza Hanif <raza_hanif@ymail.com>
2026-08-05 17:24:56 -0400

    2026 Q3 Improvements @ 5:24:55 PM

--- src/schema/structDataSchema.js
+++ src/schema/structDataSchema.js
@@ -184,7 +184,7 @@ export const breadcrumbSchema = (breadcrumbs) => {
     }
 };
 
-export const faqSchema =(faq) => {
+export const faqSchema = (faq) => {
     return {
         "@context": "https://schema.org",
         "@type": "FAQPage",
