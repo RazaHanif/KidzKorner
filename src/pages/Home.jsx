ec9c69e2978a3516f6f7524b2d4dd99a9984250d
Raza Hanif <raza_hanif@ymail.com>
2026-08-05 17:28:24 -0400

    2026 Q3 Improvements @ 5:28:24 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -63,7 +63,7 @@ const Home = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-col justify-center items-center w-3/4 py-16">
+                <div className="flex flex-col justify-center items-center w-3/4 lg:w-full py-16">
                     <h2 className="w-full flex justify-center items-center m-4 font-bold text-xl flex-1">
                         Programs for Every Stage of Early Learning
                     </h2>
