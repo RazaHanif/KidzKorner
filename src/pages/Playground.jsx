255d007e5222a7dc18485ba9748e30777e786cd9
Raza Hanif <raza_hanif@ymail.com>
2026-08-05 17:21:46 -0400

    2026 Q3 Improvements @ 5:21:45 PM

--- src/pages/Playground.jsx
+++ src/pages/Playground.jsx
@@ -96,7 +96,7 @@ const Playground = () => {
                 />
             </div>
 
-            <div className="flex flex-col justify-center items-center w-3/4 border-y lg:py-16 py-8">
+            <div className="flex flex-col justify-center items-center w-3/4 border-y lg:py-16 py-8 lg:my-8 my-4">
                 <div className="flex flex-col justify-center items-center">
                     <h2 className="text-xl font-bold text-center">
                         Explore Our Indoor Playground
