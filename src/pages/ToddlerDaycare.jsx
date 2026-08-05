69af2869f807f7f8d0884f8756eaf2684aee8d70
Raza Hanif <raza_hanif@ymail.com>
2026-08-05 17:06:14 -0400

    2026 Q3 Improvements @ 5:06:13 PM

--- src/pages/ToddlerDaycare.jsx
+++ src/pages/ToddlerDaycare.jsx
@@ -98,7 +98,7 @@ const ToddlerDaycare = () => {
                             </Button>
                         </NavLink>
                     </div>
-                    
+                    <Image src='/20251010_122316.jpg' alt='Child Playing With Magnetic Toy at Kidz Korner Milton, Ontario' />
 
                 </div>
             </div>  
