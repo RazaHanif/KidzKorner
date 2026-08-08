99635748f9aaa3346c3184f2ace83725d4385625
Raza Hanif <raza_hanif@ymail.com>
2026-08-07 23:14:23 -0400

    2026 Q3 Improvements @ 11:14:23 PM

--- src/pages/ToddlerDaycare.jsx
+++ src/pages/ToddlerDaycare.jsx
@@ -87,7 +87,7 @@ const ToddlerDaycare = () => {
                         </a>
 
                         <a 
-                            to="#" 
+                            href="#" 
                             end
                             className="hover:scale-105 ease-in-out duration-300 flex-1 lg:w-3/4 flex justify-center"
                         >
