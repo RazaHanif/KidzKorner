cb08ffafc9cf29eaf642f2c7bdb44d8389f8309d
Raza Hanif <raza_hanif@ymail.com>
2026-08-07 22:43:14 -0400

    2026 Q3 Improvements @ 10:43:14 PM

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -76,7 +76,7 @@ function Navigation() {
         </NavLink>
       </div>
 
-      <div className='lg:hidden flex flex-col justify-center items-center'>
+      <div className='lg:hidden flex flex-col justify-center items-center curso'>
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant='secondary'>
