6643e45325f0484b6efbb1d2f590510a1e73b442
Raza Hanif <raza_hanif@ymail.com>
2026-08-07 23:08:56 -0400

    2026 Q3 Improvements @ 11:08:56 PM

--- src/pages/ToddlerDaycare.jsx
+++ src/pages/ToddlerDaycare.jsx
@@ -236,6 +236,7 @@ const ToddlerDaycare = () => {
                 <p className="flex text-center">
                     Ready to join us?
                 </p>
+                <a href="#toddler-form"></a>
                 
                 {/* TODO:MAKE THIS FORM TODDLER SPECIFIC  */}
                 {/* [DaycareForm type="toddler"] */}
