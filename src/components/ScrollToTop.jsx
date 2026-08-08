f9fa4fdc997559bb14332d6cf045259c0bb293fb
Raza Hanif <raza_hanif@ymail.com>
2026-08-07 23:16:36 -0400

    2026 Q3 Improvements @ 11:16:36 PM

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -2,11 +2,25 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-    const { pathname } = useLocation();
+    const { pathname, hash } = useLocation();
 
     useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [pathname]);
+        if (hash) {
+            const element = document.querySelector(hash);
+
+            if (element) {
+                element.scrollIntoView({
+                    behavior: "smooth",
+                    block: "start",
+                });
+            }
+        } else {
+            window.scrollTo({
+                top: 0,
+                behavior: "instant",
+            });
+        }
+    }, [pathname, hash]);
 
     return null;
 };
