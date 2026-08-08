41a927c2d5d4211aa6ed966aaa96a61c47cd0a74
Raza Hanif <raza_hanif@ymail.com>
2026-08-07 23:25:24 -0400

    2026 Q3 Improvements @ 11:25:24 PM

--- src/pages/ToddlerDaycare.jsx
+++ src/pages/ToddlerDaycare.jsx
@@ -80,7 +80,7 @@ const ToddlerDaycare = () => {
                         <a 
                             href="#toddler-form" 
                             end
-                            className="hover:scale-105 ease-in-out duration-300 flex-1 lg:w-3/4 flex justify-center border"
+                            className="hover:scale-105 ease-in-out duration-300 flex-1 lg:w-3/4 flex justify-center"
                         >
                             <Button className="cursor-pointer min-w-1/2">
                                 Schedule A Tour
