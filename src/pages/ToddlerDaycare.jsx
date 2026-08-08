6ba0c92f764a4eb1075901f9de1ec4cfaa80424a
Raza Hanif <raza_hanif@ymail.com>
2026-08-07 23:02:48 -0400

    2026 Q3 Improvements @ 11:02:48 PM

--- src/pages/ToddlerDaycare.jsx
+++ src/pages/ToddlerDaycare.jsx
@@ -33,7 +33,7 @@ const ToddlerDaycare = () => {
     const faq = [
         {
             q: "What age is the toddler program?",
-            a: "Our toddler daycare program is designed for young children between the 18 and 30 ."
+            a: "Our toddler daycare program is designed for young children between the 18 months and 2.5 years."
         },
         {
             q: "What does a typical day look like?",
