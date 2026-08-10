78f6b82710a73b3acd54476cf450f54d9ecd2098
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 18:01:55 -0400

    2026 Q3 Improvements @ 6:01:55 PM

--- src/pages/SummerCamp.jsx
+++ src/pages/SummerCamp.jsx
@@ -10,7 +10,7 @@ import CampForm from "@/components/CampForm";
 import Image from "@/components/Image";
 import { breadcrumbSchema, faqSchema, localBusinessSchema } from "../schema/structDataSchema";
 import StructData from "../components/StructData";
-import { BadgeDollarSign, Blocks, PartyPopper, Shapes, Snowflake, Sparkles, Sun, TestTube2, Users, UsersRound } from "lucide-react";
+import { BadgeDollarSign, Blocks, PartyPopper, Sparkles, Sun, TestTube2, Users, UsersRound } from "lucide-react";
 import FAQAccordion from "../components/FAQAccordion";
 
 
