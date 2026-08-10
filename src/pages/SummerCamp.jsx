035cd769e59fc52161ac21214a5efa6f11a21d70
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 17:23:26 -0400

    2026 Q3 Improvements @ 5:23:26 PM

--- src/pages/SummerCamp.jsx
+++ src/pages/SummerCamp.jsx
@@ -172,7 +172,7 @@ const SummerCamp = () => {
                     <p className="flex justify-center items-center">
                         Summer camp runs from 9:00 a.m. to 3:00 p.m., from the end of the school year until the start of the next one, excluding the first and last week of summer. Our summer program is limited to 13 children, allowing us to maintain a smaller group environment while children participate in activities and spend time together.
                     </p>
-                    <p className="justify-center items-center">
+                    <p className="flex justify-center items-center">
                         The weekly cost is <b>$175 per child</b>, and all regular summer camp activities are included. Before- and after-camp care may also be available based on space, so families interested in extended hours can contact us to ask about availability.
                     </p>
                 </div>
