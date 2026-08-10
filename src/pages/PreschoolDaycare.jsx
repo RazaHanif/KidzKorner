f2080887955623cfb305e50e1876b2746ebcf44d
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 13:59:27 -0400

    2026 Q3 Improvements @ 1:59:27 PM

--- src/pages/PreschoolDaycare.jsx
+++ src/pages/PreschoolDaycare.jsx
@@ -274,7 +274,7 @@ const PreschoolDaycare = () => {
                                             <div className="flex gap-2 text-primary">
                                                 <ShieldCheck />
                                                 <h2 className="font-bold">
-                                                    Safe Environment
+                                                    Creativity & Self-Expression
                                                 </h2>
                                             </div>
                                             <p className="text-start pl-8">
