54f52e0655a7d87b8fde70172840c83e31b5346c
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 13:17:30 -0400

    2026 Q3 Improvements @ 1:17:30 PM

--- src/pages/PreschoolDaycare.jsx
+++ src/pages/PreschoolDaycare.jsx
@@ -146,7 +146,7 @@ const PreschoolDaycare = () => {
                                 </CardTitle>
                             </CardHeader>
                             <CardContent className="flex-1 flex justify-end items-end lg:justify-center lg:items-start">
-
+                                With encouragement from caring educators, children are supported in trying new things, expressing themselves, and taking pride in what they can accomplish.
                             </CardContent>
                         </Card>
                     </div>
