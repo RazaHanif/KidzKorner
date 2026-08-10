542df1a38ca344ff2c9eafbcddb12b9b6bcdab83
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 14:05:25 -0400

    2026 Q3 Improvements @ 2:05:25 PM

--- src/pages/PreschoolDaycare.jsx
+++ src/pages/PreschoolDaycare.jsx
@@ -191,7 +191,7 @@ const PreschoolDaycare = () => {
                 </div>
             </div>
 
-            <div className="flex flex-col justify-center items-center w-3/4 lg:py-8 py-4 lg:my-8 my-4">
+            <div className="flex flex-col justify-center items-center w-3/4 lg:py-4 py-4 lg:my-4 my-4">
                 <h2 className="text-xl font-bold text-center pb-4">
                     Supporting Your Preschooler's Development
                 </h2>
