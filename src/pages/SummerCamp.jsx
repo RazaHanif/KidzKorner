f254a4723c6a7a32053e0086f3d997af4fd70533
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 17:57:37 -0400

    2026 Q3 Improvements @ 5:57:36 PM

--- src/pages/SummerCamp.jsx
+++ src/pages/SummerCamp.jsx
@@ -156,7 +156,7 @@ const SummerCamp = () => {
                             <CardHeader className="text-start">
                                 <CardTitle className="flex flex-row items-center gap-2">
                                     <BadgeDollarSign className="text-primary" />
-                                    Week
+                                    Weekly Camp Pricing
                                 </CardTitle>
                                 <CardDescription>
                                     Summer camp is $175 per week, with all regular camp activities and programming included in the weekly price.
