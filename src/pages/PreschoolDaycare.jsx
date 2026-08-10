10cd8511db7abc278ea2d2b1a00cd5ea95e41ecb
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 13:19:33 -0400

    2026 Q3 Improvements @ 1:19:33 PM

--- src/pages/PreschoolDaycare.jsx
+++ src/pages/PreschoolDaycare.jsx
@@ -166,7 +166,7 @@ const PreschoolDaycare = () => {
                             <Card className="flex-1 flex">
                                 <CardHeader className="text-start">
                                     <CardTitle>
-                                        Growing Independence
+                                        Lan
                                     </CardTitle>
                                 </CardHeader>
                                 <CardContent className="flex-1 flex justify-end items-end lg:justify-center lg:items-start">
