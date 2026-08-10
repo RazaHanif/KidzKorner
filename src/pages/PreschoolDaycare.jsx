8fa03c6ba7e4be19f5441ba8a91706cc406e8416
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 18:22:18 -0400

    2026 Q3 Improvements @ 6:22:18 PM

--- src/pages/PreschoolDaycare.jsx
+++ src/pages/PreschoolDaycare.jsx
@@ -57,7 +57,7 @@ const PreschoolDaycare = () => {
 
     return (
         <div className="flex flex-col flex-1 justify-center items-center gap-4 w-full lg:py-16 pb-8">
-            <div className="flex flex-col justify-center items-center w-full lg:pb-16 py-8">
+            <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 lg:pb-16 py-8">
                 <h1 className="lg:text-5xl text-3xl font-bold font-serif text-center">
                     Preschool Daycare in Milton
                 </h1>
