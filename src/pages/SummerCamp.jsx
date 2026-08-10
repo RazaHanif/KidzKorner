bf7aa88c2bfd11ee6abf0a2c21e0823b0a2d0f6a
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 17:31:14 -0400

    2026 Q3 Improvements @ 5:31:13 PM

--- src/pages/SummerCamp.jsx
+++ src/pages/SummerCamp.jsx
@@ -62,7 +62,7 @@ const SummerCamp = () => {
                     </p>
                     <a 
                         href="/about#contact-form" 
-                        className="hover:scale-105 ease-in-out duration-300 flex-1 flex justify-center"
+                        className="hover:scale-105 ease-in-out duration-300 flex-1 flex justify-center self-end"
                     >
                         <Button className="cursor-pointer min-w-1/2">
                             Enquire About Summer Camp
