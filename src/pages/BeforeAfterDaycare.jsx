49b8bb44bd15c7de82a0938375c784fb394ff65a
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 15:39:25 -0400

    2026 Q3 Improvements @ 3:39:25 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -127,7 +127,7 @@ const BeforeAfterDaycare = () => {
                                     </h2>
                                 </div>
                                 <p className="text-start pl-8">
-                                    Art, music, imaginative play, and creative activities give children opportunities to express themselves and explore their own ideas.
+                                    A welcoming place to unwind, play
                                 </p>
                             </div>
                             <div className="flex flex-col items-start justify-start w-full">
