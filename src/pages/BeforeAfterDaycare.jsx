57ec82b0ef77fbc42d02694ac913de3d534a2730
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 16:04:42 -0400

    2026 Q3 Improvements @ 4:04:42 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -382,7 +382,6 @@ const BeforeAfterDaycare = () => {
                 </div>
             </div>
 
-
             <div className="flex flex-col justify-center items-center w-3/4 m-4">
                 <h2 className="text-xl font-bold text-center lg:">
                     Flexible Care Options for Milton Families
