f0cb3c2678c67bc23036debb0497ad7c72a60f9c
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 15:34:36 -0400

    2026 Q3 Improvements @ 3:34:36 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -95,8 +95,7 @@ const BeforeAfterDaycare = () => {
                 <div className="flex flex-col gap-4 lg:justify-evenly justify-center items-center w-full">
                     <p className="flex flex-col justify-center items-center w-full text-center">
                         School days can be busy for both children and parents. Our Before & After School Care program provides a welcoming place for children to spend time before school begins and after the school day ends.
-
-Children have opportunities to play, participate in activities, socialize with friends, and relax in a supportive environment while families manage their daily schedules.
+                        Children have opportunities to play, participate in activities, socialize with friends, and relax in a supportive environment while families manage their daily schedules.
                     </p>
                 </div>
             </div>
