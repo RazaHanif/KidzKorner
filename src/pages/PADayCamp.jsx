0f2b24513af6a99d17cff81596e6b7e5b4807d32
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 15:02:48 -0400

    2026 Q3 Improvements @ 3:02:48 PM

--- src/pages/PADayCamp.jsx
+++ src/pages/PADayCamp.jsx
@@ -254,7 +254,7 @@ const PADayCamp = () => {
             />
             <meta
                 property="og:description"
-                content="Looking for a licensed daycare in Milton? Kidz Korner provides trusted childcare, toddler daycare, preschool programs, before and after school care, and licensed camps for local families."
+
             />
             <meta 
                 property="og:type"
