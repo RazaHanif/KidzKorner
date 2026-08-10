4b141b47ada92ab761fdcedd612ac4f5ed32a39f
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 15:02:57 -0400

    2026 Q3 Improvements @ 3:02:57 PM

--- src/pages/PADayCamp.jsx
+++ src/pages/PADayCamp.jsx
@@ -250,7 +250,7 @@ const PADayCamp = () => {
             />
             <meta 
                 property="og:title"
-                content="Licensed Daycare in Milton, Ontario | Kidz Korner"
+                content="PA Day Camp in Milton, Ontario | Kidz Korner"
             />
             <meta
                 property="og:description"
