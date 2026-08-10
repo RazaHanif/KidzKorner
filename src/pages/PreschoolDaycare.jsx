7948f55de26122c7d59c7ecd50717b61e1f36618
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 13:18:32 -0400

    2026 Q3 Improvements @ 1:18:32 PM

--- src/pages/PreschoolDaycare.jsx
+++ src/pages/PreschoolDaycare.jsx
@@ -161,6 +161,8 @@ const PreschoolDaycare = () => {
                     <p className="flex flex-col justify-center items-center w-full text-center">
                         The preschool years are an exciting time of growing independence, curiosity, and self-discovery. Children are developing new ways to communicate, interact with others, solve problems, and understand the world around them. At Kidz Korner, we provide experiences that support these areas of development through play, hands-on activities, creative expression, and everyday interactions. Our educators encourage children to explore, ask questions, make choices, and take on new challenges while providing the guidance and reassurance they need along the way.
                     </p>
+
+                    
                 </div>
             </div>
 
