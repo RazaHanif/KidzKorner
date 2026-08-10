51a29445a21be4512d703a0e41391fce8bb57f72
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 19:44:26 -0400

    2026 Q3 Improvements @ 7:44:26 PM

--- src/pages/Camp.jsx
+++ src/pages/Camp.jsx
@@ -57,7 +57,7 @@ const Camp = () => {
                 </p>
             </div>
 
-            <div className="flex flex-col justify-center items-center w-3/4 border-y lg:py-12 py-8 lg:my-12 my-8">
+            <div className="flex flex-col justify-center items-center w-9/10 lg:w-3/4 border-y lg:py-12 py-8 lg:my-12 my-8">
                 <h2 className="text-xl font-bold text-center pb-4">
                     Choose Your Camp
                 </h2>
