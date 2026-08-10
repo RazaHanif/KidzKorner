b4c68ab608a7d4275bdbecd27c25eb802fed79ee
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 15:44:33 -0400

    2026 Q3 Improvements @ 3:44:33 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -209,28 +209,6 @@ const BeforeAfterDaycare = () => {
                     <div className="flex flex-col gap-2 w-full text-nowrap">
                         <p className="flex items-start gap-3">
                             <CircleCheck className="mt-1 h-5 w-5 shrink-0 text-primary"/>
-                            Experienced Childcare Educators
-                        </p>
-                        <p className="flex items-start gap-3">
-                            <CircleCheck className="mt-1 h-5 w-5 shrink-0 text-primary"/>
-                            Play-based Learning Environment
-                        </p>
-                        <p className="flex items-start gap-3">
-                            <CircleCheck className="mt-1 h-5 w-5 shrink-0 text-primary"/>
-                            Indoor Playground Access
-                        </p>
-                    </div>
-                    <div className="flex flex-col gap-2 w-full text-nowrap">
-                        <p className="flex items-start gap-3">
-                            <CircleCheck className="mt-1 h-5 w-5 shrink-0 text-primary"/>
-                            Small-group Activities
-                        </p>
-                        <p className="flex items-start gap-3">
-                            <CircleCheck className="mt-1 h-5 w-5 shrink-0 text-primary"/>
-                            Focus on Independence & Confidence
-                        </p>
-                        <p className="flex items-start gap-3">
-                            <CircleCheck className="mt-1 h-5 w-5 shrink-0 text-primary"/>
                             Opportunities to Build Friendships
                         </p>
                     </div>
