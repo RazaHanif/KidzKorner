58a9b2993b7877fe4b91e6c3e48e521935bc98bf
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 18:08:39 -0400

    2026 Q3 Improvements @ 6:08:38 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -276,7 +276,7 @@ const Home = () => {
                     />
                 </div>
 
-                <div className="flex flex-col justify-center items-center w-full p-4">
+                <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 p-4">
                     <h2 className="w-full flex justify-center items-center m-4 font-bold text-xl flex-1">
                         The Benefits of Choosing a Licensed Daycare
                     </h2>
