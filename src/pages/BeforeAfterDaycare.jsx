f6789de72a1e90f82ded08b9d5401eb6f6f889fb
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 16:14:32 -0400

    2026 Q3 Improvements @ 4:14:32 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -423,7 +423,7 @@ const BeforeAfterDaycare = () => {
                 <h2 className="text-xl font-bold text-center pb-4">
                     A Place to Play After the School Bell
                 </h2>
-                <div className="flex flex-col p-2 lg:gap-4 gap-2">
+                <div className="flex flex-col justp-2 lg:gap-4 gap-2">
                     <div className="flex flex-col gap-2 w-full">
                         <p className="flex items-start gap-3">
                             One of the things that makes Kidz Korner different is our indoor playground. After spending much of the day sitting and learning at school, children have the opportunity to get moving, play, and enjoy active time with their friends.
