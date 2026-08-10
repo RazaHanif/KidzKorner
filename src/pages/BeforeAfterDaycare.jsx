30ade8a9c5a1287815a100b254ae6ced167cb5f5
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 16:09:57 -0400

    2026 Q3 Improvements @ 4:09:56 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -409,8 +409,7 @@ const BeforeAfterDaycare = () => {
                             </TableBody>
                         </Table>
                     </div>
-                    {/* <Image src='/media/KK/20250822_121126.jpg' alt='Child Playing With Blocks at Kidz Korner Milton, Ontario' w="w-sm"/> */}
-                    <div className="flex flex-col justify-center items-center border lg:w-3/4 w-full">
+                    <div className="flex flex-col justify-center items-center lg:w-3/4 w-full">
                         <p className="">
                             Our Before & After School Care program is designed to complement your child's regular school schedule, providing care during the hours families need it most
                         </p>
