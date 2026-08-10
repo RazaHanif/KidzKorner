5f9bd1f83a0113fbdc03aeb0217fe3ecacc23276
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 18:26:00 -0400

    2026 Q3 Improvements @ 6:26:00 PM

--- src/pages/PreschoolDaycare.jsx
+++ src/pages/PreschoolDaycare.jsx
@@ -282,7 +282,7 @@ const PreschoolDaycare = () => {
                 </div>
             </div>
 
-            <div className="flex flex-col justify-center items-center w-3/4 border-y lg:py-8 py-4 lg:my-8 my-4">
+            <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 border-y lg:py-8 py-4 lg:my-8 my-4">
                 <h2 className="text-xl font-bold text-center pb-4">
                     Frequently Asked Questions About Preschool Daycare
                 </h2>
