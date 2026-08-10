ad94c799606bd353bdbf505b5c5d86c563dbb75a
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 19:40:24 -0400

    2026 Q3 Improvements @ 7:40:24 PM

--- src/pages/Playground.jsx
+++ src/pages/Playground.jsx
@@ -101,8 +101,8 @@ const Playground = () => {
                     <h2 className="text-xl font-bold text-center">
                         Explore Our Indoor Playground
                     </h2>
-                    <div className="flex lg:flex-row flex-col p-2 gap-4">
-                        <div className="flex flex-col gap-4 w-full flex-1">
+                    <div className="flex lg:flex-row flex-col p-2 gap-2">
+                        <div className="flex flex-col gap-2 w-full flex-1">
                             <p className="flex items-start gap-3">
                                 <CheckCircle className="h-6 w-6 shrink-0 text-primary"/>
                                 Climbing Structure
@@ -116,7 +116,7 @@ const Playground = () => {
                                 Toddler Friendly Spaces
                             </p>
                         </div>
-                        <div className="flex flex-col gap-4 w-full flex-1">
+                        <div className="flex flex-col gap-2 w-full flex-1">
                             <p className="flex items-start gap-3">
                                 <CheckCircle className="h-6 w-6 shrink-0 text-primary"/> 
                                 Imaginative Play Opportunities
