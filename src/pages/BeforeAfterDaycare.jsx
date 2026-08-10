a2ef343f25a8a1d65dc0f43830a1f9fdd21be871
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 19:33:17 -0400

    2026 Q3 Improvements @ 7:33:17 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -102,7 +102,7 @@ const BeforeAfterDaycare = () => {
             </div>
 
 
-            <div className="flex flex-col justify-center items-center lg:w-3/4 w-5/6">
+            <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10">
                 <h2 className="text-xl font-bold text-center pb-4">
                     A Comfortable Start and End to Every School Day
                 </h2>
