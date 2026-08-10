585050a769743288300ace232f1f075a1d4c84db
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 13:07:57 -0400

    2026 Q3 Improvements @ 1:07:57 PM

--- src/pages/PreschoolDaycare.jsx
+++ src/pages/PreschoolDaycare.jsx
@@ -196,7 +196,7 @@ const PreschoolDaycare = () => {
                     <div className="flex flex-col gap-2 w-full text-nowrap">
                         <p className="flex items-start gap-3">
                             <CircleCheck className="mt-1 h-5 w-5 shrink-0 text-primary"/>
-                            Caring & Experienced Educators
+                            Experienced Childcare Educators
                         </p>
                         <p className="flex items-start gap-3">
                             <CircleCheck className="mt-1 h-5 w-5 shrink-0 text-primary"/>
