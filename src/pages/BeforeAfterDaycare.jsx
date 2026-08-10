52b6da6dbb28c9bed906dd86fe02dcfe0fe819fb
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 15:48:09 -0400

    2026 Q3 Improvements @ 3:48:09 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -241,7 +241,8 @@ const BeforeAfterDaycare = () => {
                     <div className="flex flex-col gap-2 w-full">
                         <p className="flex items-start gap-3">
                             After a full day at school, children need time to relax and be themselves. Our After School Care gives them an opportunity to transition out of the school day at their own pace.
-                            
+                        </p>
+                        <p className="flex items-start gap-3">
 
                             Whether they want to play with friends, participate in an activity, explore the indoor playground, or enjoy some quieter time, children can spend their afternoon in a comfortable and supportive environment.
                         </p>
