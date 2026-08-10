e9201cdac6c92c59b3fd8a8988936d80445a33d5
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 17:17:38 -0400

    2026 Q3 Improvements @ 5:17:38 PM

--- src/pages/SummerCamp.jsx
+++ src/pages/SummerCamp.jsx
@@ -78,7 +78,7 @@ const SummerCamp = () => {
                 </h2>
                 <div className="flex lg:flex-row flex-col lg:justify-evenly justify-center items-center w-full">
                     <div className="flex flex-col lg:flex-row flex-1 lg:w-3/4 w-full gap-4">
-                        <div>
+                        <div className="flex flex-col lg:flex-row flex-1 lg:w-3/4 w-full gap-4">
                             <Card className="flex-1 flex">
                                 <CardHeader className="text-start">
                                     <CardTitle className="flex flex-row items-center gap-2">
@@ -116,7 +116,7 @@ const SummerCamp = () => {
                             </Card>
                         </div>
 
-                        <div>
+                        <div className="flex flex-col lg:flex-row flex-1 lg:w-3/4 w-full gap-4">
                             <Card className="flex-1 flex">
                                 <CardHeader className="text-start">
                                     <CardTitle className="flex flex-row items-center gap-2">
