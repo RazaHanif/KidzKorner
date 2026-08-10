b8a87b5b818a0bec2f7c5106e795cf3fdbb7021e
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 16:35:18 -0400

    2026 Q3 Improvements @ 4:35:18 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -121,7 +121,7 @@ const BeforeAfterDaycare = () => {
                                         Before School Care
                                     </CardTitle>
                                 </CardHeader>
-                                <CardContent className>
+                                <CardContent className="text-start">
                                         <p className="text-start pl-8">
                                             A calm and engaging start to the morning before children head off to school.
                                         </p>
