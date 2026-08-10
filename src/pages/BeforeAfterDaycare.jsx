7719d48053ccc97d03f1eee63bb0d006e9300ce2
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 15:41:50 -0400

    2026 Q3 Improvements @ 3:41:49 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -155,7 +155,7 @@ const BeforeAfterDaycare = () => {
                         <Card className="flex-1 flex">
                             <CardHeader className="text-start">
                                 <CardTitle>
-                                    Growing Independence
+                                    Creative Activities
                                 </CardTitle>
                             </CardHeader>
                             <CardContent className="flex-1 flex justify-end items-end lg:justify-center lg:items-start">
