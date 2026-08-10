ed5590a8cda1ac7e0a7d8c1db9130c6ae603087a
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 17:55:54 -0400

    2026 Q3 Improvements @ 5:55:54 PM

--- src/pages/SummerCamp.jsx
+++ src/pages/SummerCamp.jsx
@@ -11,7 +11,7 @@ import CampForm from "@/components/CampForm";
 import Image from "@/components/Image";
 import { breadcrumbSchema, faqSchema, localBusinessSchema } from "../schema/structDataSchema";
 import StructData from "../components/StructData";
-import { ChefHat, Flower, Heart, Leaf, Palette, Shapes, Snowflake, Sun, TestTube2, Users } from "lucide-react";
+import { ChefHat, Flower, Heart, Leaf, Palette, PartyPopper, Shapes, Snowflake, Sun, TestTube2, Users } from "lucide-react";
 import FAQAccordion from "../components/FAQAccordion";
 
 
@@ -93,7 +93,7 @@ const SummerCamp = () => {
                         <Card className="flex-1 flex">
                             <CardHeader className="text-start">
                                 <CardTitle className="flex flex-row items-center gap-2">
-                                    <Sun className="text-primary" />
+                                    <PartyPopper className="text-primary" />
                                     Themed Events & Experiences
                                 </CardTitle>
                                 <CardDescription>
