51e0a7b7ae6aceb48b7373d2c4949eb195a4bedb
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 13:05:12 -0400

    2026 Q3 Improvements @ 1:05:12 PM

--- src/pages/PreschoolDaycare.jsx
+++ src/pages/PreschoolDaycare.jsx
@@ -148,11 +148,11 @@ const PreschoolDaycare = () => {
                         <Card className="flex-1 flex">
                             <CardHeader className="text-start">
                                 <CardTitle>
-                                    Building Independence
+                                    Developing Confidence
                                 </CardTitle>
                             </CardHeader>
                             <CardContent className="flex-1 flex justify-end items-end lg:justify-center lg:items-start">
-                                We encourage toddlers to develop confidence through age-appropriate routines and responsibilities.
+ 
                             </CardContent>
                         </Card>
                     </div>
