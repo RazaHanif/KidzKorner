1b152f682184aaef3c37484b748e00e3d22798ac
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 17:04:27 -0400

    2026 Q3 Improvements @ 5:04:27 PM

--- src/pages/SummerCamp.jsx
+++ src/pages/SummerCamp.jsx
@@ -60,10 +60,15 @@ const SummerCamp = () => {
                     Keep your child active, engaged, and having fun throughout the summer at Kidz Korner. Our summer camp in Milton offers themed activities, arts and crafts, games, indoor playground time, and plenty of opportunities for creative and active play in a welcoming environment.
                 </p>
                 <div>
-                    <a>
-                        
-                    </a>
-                    <Button>Enquire About Summer Camp</Button>
+                    <a 
+                            href="/about#contact-form" 
+                            className="hover:scale-105 ease-in-out duration-300 flex-1 lg:w-3/4 flex justify-center"
+                        >
+                            <Button className="cursor-pointer min-w-1/2">
+                                Contact Us
+                            </Button>
+                        </a>
+
                 </div>
             </div>
 
