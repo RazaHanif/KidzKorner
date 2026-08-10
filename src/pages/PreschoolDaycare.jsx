685065427ded9c1289a6f29f4c876933fd5abf6b
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 13:22:37 -0400

    2026 Q3 Improvements @ 1:22:37 PM

--- src/pages/PreschoolDaycare.jsx
+++ src/pages/PreschoolDaycare.jsx
@@ -241,8 +241,7 @@ const PreschoolDaycare = () => {
                         </p>
                         <p className="flex items-start gap-3">
                             <CircleCheck className="mt-1 h-5 w-5 shrink-0 text-primary"/>
-                            Freshly Cooked Meals & Snacks
-                            Freshly Cooked Meals & Snacks
+                            Encouragment 
                         </p>
                     </div>
                 </div>
