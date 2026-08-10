8889ccb7142b3b6114aead916a803eaedb1e0d24
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 18:21:26 -0400

    2026 Q3 Improvements @ 6:21:26 PM

--- src/pages/ToddlerDaycare.jsx
+++ src/pages/ToddlerDaycare.jsx
@@ -226,7 +226,7 @@ const ToddlerDaycare = () => {
                 </div>
             </div>
 
-            <div className="flex flex-col justify-center items-center w-3/4 border-y lg:py-16 py-8 lg:my-8 my-4">
+            <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 border-y lg:py-16 py-8 lg:my-8 my-4">
                 <h2 className="text-xl font-bold text-center pb-4">
                     Frequently Asked Questions About Toddler Daycare
                 </h2>
