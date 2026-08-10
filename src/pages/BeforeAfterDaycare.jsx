7c7ed6c9b3abffe6ed8d0118d8bd8572d286084e
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 16:12:13 -0400

    2026 Q3 Improvements @ 4:12:13 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -437,7 +437,10 @@ const BeforeAfterDaycare = () => {
                                 to="/daycare/preschool" 
                                 end 
                                 className="hover:scale-102 ease-in-out duration-300 flex flex-1"
-                            >
+                        >
+
+
+                            
                             <Button className="cursor-pointer min-w-1/2">
                                 Schedule A Tour
                             </Button>
