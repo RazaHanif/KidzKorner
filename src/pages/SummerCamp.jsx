d65b6a11a221f5a0b608729e880987c3c8346d27
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 18:04:25 -0400

    2026 Q3 Improvements @ 6:04:25 PM

--- src/pages/SummerCamp.jsx
+++ src/pages/SummerCamp.jsx
@@ -188,7 +188,7 @@ const SummerCamp = () => {
                 </div>
             </div>
 
-            <div className="flex flex-col justify-center items-center w-3/4 border-t lg:pt-8 pt-4 lg:mt-8 mt-4">
+            <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 border-t lg:pt-8 pt-4 lg:mt-8 mt-4">
                 <h2 className="text-xl font-bold text-center pb-4">
                     Summer Camp Highlights
                 </h2>
