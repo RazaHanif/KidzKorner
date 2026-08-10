b70c29f7f7f42502d63f58f5ddee142e729e7080
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 15:37:17 -0400

    2026 Q3 Improvements @ 3:37:17 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -110,7 +110,7 @@ const BeforeAfterDaycare = () => {
                     </p>
                     <div className="flex lg:flex-row flex-col p-2 lg:gap-4 gap-2">
                         <div className="flex flex-col gap-2 w-full text-nowrap">
-                                                        <div className="flex flex-col items-start justify-start w-full">
+                            <div className="flex flex-col items-start justify-start w-full">
                                 <div className="flex gap-2 text-primary">
                                     <ShieldCheck />
                                     <h2 className="font-bold">
