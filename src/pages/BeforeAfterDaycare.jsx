70d437c84d855affae2f47b2d8b8899d4f7471c4
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 15:48:30 -0400

    2026 Q3 Improvements @ 3:48:29 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -211,8 +211,6 @@ const BeforeAfterDaycare = () => {
                             Mornings don't always go according to plan. Our Before School Care provides children with a safe and welcoming place to begin their day before heading off to school.
                         </p>
                         <p className="flex items-start gap-3">
-                            
-
                             Children can ease into their morning with quiet activities, games, and time to socialize before it's time to leave for school.
                         </p>
                     </div>
