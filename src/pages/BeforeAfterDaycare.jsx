a85160ec65bd89946d5e1322c28df043c80b2554
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 16:28:35 -0400

    2026 Q3 Improvements @ 4:28:35 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -18,7 +18,7 @@ import Image from "@/components/Image"
 import FAQAccordion from "../components/FAQAccordion";
 import StructData from "../components/StructData";
 import { breadcrumbSchema, faqSchema, localBusinessSchema } from "../schema/structDataSchema";
-import { BadgeCheck, CircleCheck, Clock3, GraduationCap, HeartHandshake, House, Palette, ShieldCheck } from "lucide-react";
+import { BadgeCheck, CircleCheck, Clock3, GraduationCap, HeartHandshake, House, Palette, ShieldCheck, Sparkles } from "lucide-react";
 import { NavLink } from "react-router";
 
 const BeforeAfterDaycare = () => {
@@ -325,7 +325,7 @@ const BeforeAfterDaycare = () => {
                                         </div>
                                         <div className="flex flex-col items-start justify-start w-full">
                                             <div className="flex gap-2 text-primary">
-                                                <GraduationCap />
+                                                <Sparkles />
                                                 <h2 className="font-bold">
                                                     Active & Engaging
                                                 </h2>
