cc62aa0b6d86bb3c6a68df386e9862f1eb92e7b2
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 18:26:04 -0400

    2026 Q3 Improvements @ 6:26:04 PM

--- src/pages/PreschoolDaycare.jsx
+++ src/pages/PreschoolDaycare.jsx
@@ -289,7 +289,7 @@ const PreschoolDaycare = () => {
                 <FAQAccordion faq={faq} />
             </div>
 
-            <div className="flex flex-col justify-center items-center w-3/4 gap-4" id="preschool-form">
+            <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 gap-4" id="preschool-form">
                 <p className="flex text-center">
                     Ready to join us?
                 </p>
