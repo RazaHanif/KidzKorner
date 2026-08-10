ee04fd73d212b7721cd5f8c21a165f07ec99fd2f
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 16:19:47 -0400

    2026 Q3 Improvements @ 4:19:46 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -44,7 +44,7 @@ const BeforeAfterDaycare = () => {
         },
         {
             q: "What are your Before & After School Care Hours?",
-            a: "7:30 AM - ."
+            a: "7:30 AM - 9:00 AM and 3:00."
         },
         {
             q: "How does the preschool program prepare children for kindergarten?",
