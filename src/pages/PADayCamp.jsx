c2ed64ed3bbf3f107be86fd60a089b8afd7dd729
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 19:49:45 -0400

    2026 Q3 Improvements @ 7:49:44 PM

--- src/pages/PADayCamp.jsx
+++ src/pages/PADayCamp.jsx
@@ -26,7 +26,7 @@ const PADayCamp = () => {
         },
         {
             name: "Summer Break Camp",
-            url: "https://www.kidzkornermilton.com/camp/summer-camp"
+            url: "https://www.kidzkornermilton.com/camp/pa-day-camp"
         },
     ];
 
@@ -260,7 +260,7 @@ const PADayCamp = () => {
             />
             <meta
                 property="og:url"
-                content="https://www.kidzkornermilton.com/camp/summer-camp"
+                content="https://www.kidzkornermilton.com/camp/pa-day-camp"
             />
             <meta
                 property="og:image"
