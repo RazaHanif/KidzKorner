966cd7cef5c26f2e2704cdf0986d3ae186411fc5
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 16:18:39 -0400

    2026 Q3 Improvements @ 4:18:39 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -40,7 +40,7 @@ const BeforeAfterDaycare = () => {
         const faq = [
         {
             q: "What age do you accept for Before & After School Care? [CHECK IF THIS IS RIGHT!!!!!]",
-            a: "Our toddler daycare program is designed for young children between 2-4 years."
+            a: "Our Before & After School Care program is designed for young children between 2-4 years."
         },
         {
             q: "What does a typical preschool day look like?",
