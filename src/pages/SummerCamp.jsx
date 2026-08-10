c9a27eae5f6a6833e72dc5b36a3e14ed50ee32c6
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 17:55:22 -0400

    2026 Q3 Improvements @ 5:55:22 PM

--- src/pages/SummerCamp.jsx
+++ src/pages/SummerCamp.jsx
@@ -310,7 +310,7 @@ const SummerCamp = () => {
             />
             <meta
                 property="og:url"
-                content="https://www.kidzkornermilton.com/camp"
+                content="https://www.kidzkornermilton.com/camp/summer-camp"
             />
             <meta
                 property="og:image"
