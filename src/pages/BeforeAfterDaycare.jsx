586f4d774d1be51898531a233f063b07a332209c
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 16:34:12 -0400

    2026 Q3 Improvements @ 4:34:11 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -117,19 +117,18 @@ const BeforeAfterDaycare = () => {
                         <div className="flex flex-col gap-2 w-full">
                             <Card>
                                 <CardContent>
-                                    
+                                    <div className="flex flex-col items-start justify-start w-full">
+                                        <div className="flex gap-2 self-center">
+                                            <h2 className="font-bold">
+                                                Before School Care
+                                            </h2>
+                                        </div>
+                                        <p className="text-start pl-8">
+                                            A calm and engaging start to the morning before children head off to school.
+                                        </p>
+                                    </div>
                                 </CardContent>
                             </Card>
-                            <div className="flex flex-col items-start justify-start w-full">
-                                <div className="flex gap-2 self-center">
-                                    <h2 className="font-bold">
-                                        Before School Care
-                                    </h2>
-                                </div>
-                                <p className="text-start pl-8">
-                                    A calm and engaging start to the morning before children head off to school.
-                                </p>
-                            </div>
                             <div className="flex flex-col items-start justify-start w-full">
                                 <div className="flex gap-2 self-center">
                                     <h2 className="font-bold">
