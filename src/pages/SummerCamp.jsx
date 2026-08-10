deaa03d899c681b202f1db14970fa0517aae5fbd
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 18:04:17 -0400

    2026 Q3 Improvements @ 6:04:17 PM

--- src/pages/SummerCamp.jsx
+++ src/pages/SummerCamp.jsx
@@ -165,7 +165,7 @@ const SummerCamp = () => {
                 </div>
             </div>
 
-            <div className="flex flex-col justify-center items-center gap-4 w-3/4">
+            <div className="flex flex-col justify-center items-center gap-4 lg:w-3/4 w-9/10">
                 <h2 className="text-xl font-bold text-center pb-4">
                     What to Expect at Kidz Korner Summer Camp
                 </h2>
