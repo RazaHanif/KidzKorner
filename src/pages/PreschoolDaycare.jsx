0ac4407982957c412b0848d4ab1448125b4d2214
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 13:38:09 -0400

    2026 Q3 Improvements @ 1:38:09 PM

--- src/pages/PreschoolDaycare.jsx
+++ src/pages/PreschoolDaycare.jsx
@@ -280,7 +280,7 @@ const PreschoolDaycare = () => {
             <StructData schema={faqSchema(faq)} />
 
             <title>
-                Preschool Daycare in Milton, Ontario | Preschool | Kidz Korner
+                Preschool Daycare in Milton, Ontario | Learning Through Play | Kidz Korner
             </title>
             <meta 
                 name="description" 
