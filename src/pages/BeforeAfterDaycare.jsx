fa464ab9263e24a029ed5839d4fc232eb581cbe2
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 15:46:41 -0400

    2026 Q3 Improvements @ 3:46:40 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -213,7 +213,7 @@ const BeforeAfterDaycare = () => {
                             Children can ease into their morning with quiet activities, games, and time to socialize before it's time to leave for school.
                         </p>
                     </div>
-                    <div className="flex flex-col lg:flex-row gap-2 lg:gap-4">
+                    <div className="flex flex-col lg:flex-row gap-2 lg:gap-4 border">
                         <Image 
                             src='/media/KK/20251010_122316.jpg' 
                             alt='Child Playing With Magnetic Toy at Kidz Korner Milton, Ontario' 
