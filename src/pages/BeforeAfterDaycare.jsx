a83a0ced2a1634497f6a7a16096e4e5401108fa8
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 16:21:54 -0400

    2026 Q3 Improvements @ 4:21:54 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -48,7 +48,7 @@ const BeforeAfterDaycare = () => {
         },
         {
             q: "Do you provide transportation to and from school?",
-            a: "."
+            a: "Limited spots are available, for more information on range and ."
         },
         {
             q: "Do preschool children have opportunities for free play?",
