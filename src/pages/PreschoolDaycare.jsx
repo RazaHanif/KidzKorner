2acdc6132b8b64d37af890f15f1f57dcdb8c5f99
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 14:32:42 -0400

    2026 Q3 Improvements @ 2:32:42 PM

--- src/pages/PreschoolDaycare.jsx
+++ src/pages/PreschoolDaycare.jsx
@@ -92,7 +92,7 @@ const PreschoolDaycare = () => {
                 </div>
                 <div className="flex justify-center items-center">
                     <Image 
-                        src='/20251010_122316.jpg' 
+                        src='/media/KK/20251010_122316.jpg' 
                         alt='Child Playing With Magnetic Toy at Kidz Korner Milton, Ontario' 
                         w="w-sm"    
                     />
