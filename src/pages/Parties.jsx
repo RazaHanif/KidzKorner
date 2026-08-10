605a32e18ec17d0958b4c411c597f690984af3fc
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 19:38:20 -0400

    2026 Q3 Improvements @ 7:38:20 PM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -252,7 +252,7 @@ const Parties = () => {
                 </div>
             </div>
 
-            <div className="flex flex-col justify-center items-center w-3/4 border-y py-10 my-8">
+            <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10">
                 <h2 className="text-xl font-bold text-center pb-4">
                     FAQ
                 </h2>
