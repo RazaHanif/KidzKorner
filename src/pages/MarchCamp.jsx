b8f5f1caa8553e40a5a46cfa5689c31a803694d1
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 13:40:04 -0400

    2026 Q3 Improvements @ 1:40:04 PM

--- src/pages/MarchCamp.jsx
+++ src/pages/MarchCamp.jsx
@@ -240,8 +240,10 @@ const SummerCamp = () => {
             <title>
                 Summer Camp in Milton, Ontario | Kidz Korner 
             </title>
-            <meta name="keywords" content="kids camps Milton, summer camp Milton, seasonal camp Milton, childcare camp Milton, children's camp Milton" />
-            <meta name="description" content="Join Kidz Korner's fun and educational kids' camps in Milton, Ontario. Summer and seasonal camps designed to spark curiosity and creativity." />
+            <meta 
+                name="description" 
+                content="Join Kidz Korner's fun and educational kids' camps in Milton, Ontario. Summer and seasonal camps designed to spark curiosity and creativity." 
+            />
         </div>
     );
 }
