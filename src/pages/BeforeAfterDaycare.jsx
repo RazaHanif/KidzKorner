f0cfc30622be4c08df93218a8fed6f88f70c0536
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 16:06:30 -0400

    2026 Q3 Improvements @ 4:06:30 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -387,7 +387,7 @@ const BeforeAfterDaycare = () => {
                     Before & After School Care Hours
                 </h2>
                 <div className="flex lg:flex-row flex-col lg:justify-evenly justify-center items-center w-full">
-                    <div className="flex flex-col justify-center m-4 lg:m-0">
+                    <div className="flex flex-col justify-center m-4 lg:m-0 flex-1">
                         <Table>
                             <TableBody>
                                 <TableRow>
