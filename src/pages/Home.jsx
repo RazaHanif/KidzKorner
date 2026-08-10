3af52cd92df287aca60bef2e72b328e195b8735e
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 18:05:35 -0400

    2026 Q3 Improvements @ 6:05:34 PM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ import { BadgeCheck, GraduationCap, HeartHandshake, MessageCircleHeart, Palette,
 const Home = () => {
     return (
         <div className="flex flex-col flex-1 justify-center items-center gap-4 w-full lg:py-16 pb-8">
-            <div className="flex flex-col justify-center items-center w-full mb-4">
+            <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 mb-4">
                 <h1 className="lg:text-5xl text-3xl font-bold font-serif text-center py-4">
                     Licensed Daycare & Childcare in Milton
                 </h1>
