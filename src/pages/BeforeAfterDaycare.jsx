4eb6806af9c7e887c87a9faeaab78ebf1d5c7a6b
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 15:54:36 -0400

    2026 Q3 Improvements @ 3:54:36 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -316,7 +316,7 @@ const BeforeAfterDaycare = () => {
                                                 </h2>
                                             </div>
                                             <p className="text-start pl-8">
-                                                Stories, conversations, songs, and everyday interactions help children expand their vocabulary, express their ideas, listen to others, and become confident communicators.
+                                                interactions help children expand their vocabulary, express their ideas, listen to others, and become confident communicators.
                                             </p>
                                         </div>
                                         <div className="flex flex-col items-start justify-start w-full">
