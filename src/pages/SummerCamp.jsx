52aee3e173aeb1c1c6511e483939be35e6895ca4
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 19:57:14 -0400

    2026 Q3 Improvements @ 7:57:14 PM

--- src/pages/SummerCamp.jsx
+++ src/pages/SummerCamp.jsx
@@ -62,7 +62,7 @@ const SummerCamp = () => {
     ]
 
     return (
-        <div className="flex flex-col flex-1 justify-center items-center gap-6 w-full lg:py-16 pb:8">
+        <div className="flex flex-col flex-1 justify-center items-center gap-6 w-full lg:py-16 pb-8">
             <div className="flex flex-col justify-center items-center w-full mb-4">
                 <h1 className="lg:text-5xl text-3xl font-bold font-serif text-center py-4">
                     Summer Camp in Milton
