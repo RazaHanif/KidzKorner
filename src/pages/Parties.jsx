5dac9dd446d2cc31e47f74bfb95ecd7ade9062c8
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 19:36:09 -0400

    2026 Q3 Improvements @ 7:36:09 PM

--- src/pages/Parties.jsx
+++ src/pages/Parties.jsx
@@ -71,7 +71,7 @@ const Parties = () => {
                 </p>
             </div>
 
-            <div className="flex flex-1 flex-col lg:flex-row justify-center gap-4 w-3/4">
+            <div className="flex flex-1 flex-col lg:flex-row justify-center gap-4 lg:w-3/4">
                 <PartyCard
                     title={"Tiny Tornado Package"}
                     price={"395"}
