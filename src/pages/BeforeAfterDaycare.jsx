a93ff7cb618188e45d1b0169ce4f05abd4d341b6
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 15:50:25 -0400

    2026 Q3 Improvements @ 3:50:25 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -257,7 +257,7 @@ const BeforeAfterDaycare = () => {
                                     </CardTitle>
                                 </CardHeader>
                                 <CardContent className="flex-1 flex justify-end items-end lg:justify-center lg:items-start">
-                                    Preschoolers are encouraged to make choices, take on age-appropriate responsibilities, and become more confident in doing things for themselves.
+                                    Enjoy time in our indoor play spaces and participate in fun activities.
                                 </CardContent>
                             </Card>
 
