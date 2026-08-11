778a2607b24fe10e985e68ae432ef6347f224c85
Raza Hanif <raza_hanif@ymail.com>
2026-08-11 19:41:01 -0400

    2026 Q3 Improvements @ 7:41:01 PM

--- src/pages/Camp.jsx
+++ src/pages/Camp.jsx
@@ -60,7 +60,7 @@ const Camp = () => {
                 </p>
                 <Image 
                     src='/media/Stock/18.jpg' 
-                    alt='Childrens at Kidz Korner Milton, Ontario' 
+                    alt=' at Kidz Korner Milton, Ontario' 
                     w="w-sm" 
                 />
             </div>
