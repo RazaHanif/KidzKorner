ea347a8b773ed4965486bcc37789710f9455b3a1
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 22:03:17 -0400

    2026 Q3 Improvements @ 10:03:17 PM

--- src/pages/Workshops.jsx
+++ src/pages/Workshops.jsx
@@ -26,8 +26,7 @@ const Workshops = () => {
             <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 lg:text-justify text-center">
                 <p className="mb-4 lg:text-justify text-center">
                     At Kidz Korner, we offer fun and engaging workshops for
-                        toddlers and preschoolers in Milton, Ontario. Our workshops
-                        give young children the opportunity to explore, create,play, and enjoy new activities in a welcoming environment.
+                        toddlers and preschoolers in Milton, Ontario. Our workshops give young children the opportunity to explore, create,play, and enjoy new activities in a welcoming environment.
                 </p>
 
                 <p className="hover:underline">
