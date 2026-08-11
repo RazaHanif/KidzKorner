65de5c212452b931aa80aa8f87fec981858f143d
Raza Hanif <raza_hanif@ymail.com>
2026-08-11 11:58:43 -0400

    2026 Q3 Improvements @ 11:58:43 AM

--- src/pages/BlogSlug.jsx
+++ src/pages/BlogSlug.jsx
@@ -76,11 +76,7 @@ const BlogSlug = () => {
                 </h1>
             </div>
             <article className="flex flex-2 flex-col justify-center items-center lg:w-3/4 w-9/10 gap-6 lg:py-12 py-8 lg:my-12 my-8 border">
-                {blog.content.map((paragraph, index) => (
-                    <p key={index}>
-                        {paragraph}
-                    </p>
-                ))}
+                
             </article>
             <StructData schema={localBusinessSchema} />
             <StructData schema={breadcrumbSchema(breadCrumb)} />
