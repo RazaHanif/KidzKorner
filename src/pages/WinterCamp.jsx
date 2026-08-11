44e256ce080fe403bdde2068d9ba866f7dd40eb9
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 21:33:53 -0400

    2026 Q3 Improvements @ 9:33:53 PM

--- src/pages/WinterCamp.jsx
+++ src/pages/WinterCamp.jsx
@@ -166,7 +166,7 @@ const WinterCamp = () => {
 
             <div className="flex flex-col justify-center items-center gap-6 lg:w-3/4 w-9/10">
                 <h2 className="text-xl font-bold text-center pb-4">
-                    What to Expect at Kidz Korner Summer Camp
+                    What to Expect at Kidz Korner Winter Break Camp
                 </h2>
                 <div className="flex flex-col gap-2 w-full">
                     <p className="">
