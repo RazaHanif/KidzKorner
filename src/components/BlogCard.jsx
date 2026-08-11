582f727275113b537f69df8c42b75a13e6f66b9d
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 22:38:11 -0400

    2026 Q3 Improvements @ 10:38:11 PM

---
+++ src/components/BlogCard.jsx
@@ -0,0 +1,21 @@
+const BlogCard = ({title, price, desc}) => {
+    return (
+        <Card className="w-full">
+            <CardHeader>
+                <CardTitle className="p-2 bg-primary text-primary-foreground flex justify-center items-center rounded-md text-lg">
+                    { title }
+                </CardTitle>
+                <CardDescription className="p-2 bg-accent flex justify-center items-center rounded-md text-bold text-lg">
+                    ${price}
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <p className="p-2 text-center">
+                    { desc }
+                </p>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default BlogCard
