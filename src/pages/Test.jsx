711960661dbf0f28af4b02f5fff6a56029a96c6c
Raza Hanif <raza_hanif@ymail.com>
2026-08-11 19:04:22 -0400

    2026 Q3 Improvements @ 7:04:21 PM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -3,7 +3,7 @@ import { Shapes } from "lucide-react";
 const Test = () => {
     return (
         <div className="flex flex-col flex-1 justify-center items-center gap-4 w-full lg:py-16 pb-8">
-            <div className="flex flex-col justify-center items-center w-full">
+            {/* <div className="flex flex-col justify-center items-center w-full">
                 <h1 className="text-3xl font-bold">
                     The Sandbox
                 </h1>
@@ -11,7 +11,9 @@ const Test = () => {
 
             <div className="flex flex-col justify-center items-center lg:w-3/4 w-full lg:text-justify text-center">
                 <Shapes className="h-10 w-10 text-primary"/>
-            </div>
+            </div> */}
+
+            {/*  */}
 
             <title>
                 Test | Kidz Korner 
