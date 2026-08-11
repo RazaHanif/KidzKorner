aa8b1dd10ad9c52344f44cca8b1e68b9a126bf22
Raza Hanif <raza_hanif@ymail.com>
2026-08-11 18:43:07 -0400

    2026 Q3 Improvements @ 6:43:07 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -38,7 +38,7 @@ const BeforeAfterDaycare = () => {
         const faq = [
         {
             q: "What ages do you accept for Before & After School Care? [CHECK IF THIS IS RIGHT!!!!!]",
-            a: "Our Licensed Before & After School Care programs are designed for all school-aged children from JK through Grade 8."
+            a: "Our Licensed Before & After School Care programs are designed for all school-aged children from 4 to 13 years old."
         },
         {
             q: "What are your Before & After School Care Hours?",
