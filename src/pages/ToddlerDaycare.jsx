c0894163a9e88883a43a115734f652c511471a17
Raza Hanif <raza_hanif@ymail.com>
2026-08-11 19:31:54 -0400

    2026 Q3 Improvements @ 7:31:54 PM

--- src/pages/ToddlerDaycare.jsx
+++ src/pages/ToddlerDaycare.jsx
@@ -213,7 +213,7 @@ const ToddlerDaycare = () => {
 
                 <div className="flex lg:flex-row flex-col justify-center items-center w-full gap-6">
                     <Image
-                        className="lg:flex"
+                        className="lg:flex hidden"
                         src="/media/Stock/19.jpg"
                         alt="Child Playing with Letters at Kidz Korner in  Milton"
                         w="w-sm"
