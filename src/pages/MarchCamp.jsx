14074fe9aee72873613bc949f088616b1a620466
Raza Hanif <raza_hanif@ymail.com>
2026-08-11 18:56:40 -0400

    2026 Q3 Improvements @ 6:56:40 PM

--- src/pages/MarchCamp.jsx
+++ src/pages/MarchCamp.jsx
@@ -64,7 +64,7 @@ const MarchCamp = () => {
         <div className="flex flex-col flex-1 justify-center items-center gap-6 w-full lg:py-16 pb-8">
             <div className="flex flex-col justify-center items-center w-full mb-4">
                 <h1 className="lg:text-5xl text-3xl font-bold font-serif text-center py-4">
-                    Licensed March Break Camp in Milton
+                    March Break Camp in Milton
                 </h1>
                 <div className="flex flex-col justify-center items-center text-center gap-6 pt-4">
                     <p className="lg:w-3/4 w-9/10">
