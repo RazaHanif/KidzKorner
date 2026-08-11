4245b26315c358e51faf57ad6612e1a63a9c9c5f
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 21:51:47 -0400

    2026 Q3 Improvements @ 9:51:47 PM

--- src/pages/Workshops.jsx
+++ src/pages/Workshops.jsx
@@ -19,7 +19,7 @@ const Workshops = () => {
         <div className="flex flex-col flex-1 justify-center items-center gap-6 w-full lg:py-16 pb-8">
             <div className="flex flex-col justify-center items-center w-full mb-4">
                 <h1 className="lg:text-5xl text-3xl font-bold font-serif text-center py-4">
-                    Licensed Daycare & Childcare in Milton
+                    Kids Workshops in Milton, Ontario
                 </h1>
             </div>
 
