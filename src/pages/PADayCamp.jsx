945b419da580c421290fd98855b11d745d3e449c
Raza Hanif <raza_hanif@ymail.com>
2026-08-11 18:56:51 -0400

    2026 Q3 Improvements @ 6:56:51 PM

--- src/pages/PADayCamp.jsx
+++ src/pages/PADayCamp.jsx
@@ -208,7 +208,7 @@ const PADayCamp = () => {
                                 Small groups of up to 13
                             </p>
                             <p className="lg:border-r-2 lg:border-b-0 border-b-2 p-4">
-                                9AM to 3PM
+                                9AM to 5PM
                             </p>
                             <p className="p-4">
                                 $45 per day
