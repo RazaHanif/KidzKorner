210430b9899866edc44a36413b59d1d3dfcf9c02
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 21:24:36 -0400

    2026 Q3 Improvements @ 9:24:36 PM

--- src/pages/MarchCamp.jsx
+++ src/pages/MarchCamp.jsx
@@ -189,7 +189,7 @@ const MarchCamp = () => {
 
             <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 border-y lg:py-12 py-8 lg:my-12 my-8">
                 <h2 className="text-xl font-bold text-center pb-4">
-                    Summer Camp Highlights
+                    March Break Camp Highlights
                 </h2>
                 <Card>
                     <CardContent>
