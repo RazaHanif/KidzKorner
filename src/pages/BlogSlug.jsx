65f3e98e9694eb6fbd60a97434ceb0cf082a7a2b
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 23:27:58 -0400

    2026 Q3 Improvements @ 11:27:58 PM

--- src/pages/BlogSlug.jsx
+++ src/pages/BlogSlug.jsx
@@ -28,9 +28,7 @@ const BlogSlug = () => {
 
     return (
         <div className="flex flex-col flex-1 justify-center items-center gap-4 w-full lg:py-16 pb-8">
-            <div>
-                {slug}
-            </div>
+            
             <div>
                 {blog.title}
                 {blog.body}
