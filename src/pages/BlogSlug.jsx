952303be5d84015e2c0c09fa8ca8c5a40c4b6b01
Raza Hanif <raza_hanif@ymail.com>
2026-08-11 12:09:01 -0400

    2026 Q3 Improvements @ 12:09:01 PM

--- src/pages/BlogSlug.jsx
+++ src/pages/BlogSlug.jsx
@@ -79,7 +79,7 @@ const BlogSlug = () => {
                     return (
                         <p 
                             key={idx}
-                            className="w-full flex justify-center items-center flex-1 p-4" 
+                            className="w-full flex justify-center items-center flex-1" 
                         >
                             {section}
                         </p>
