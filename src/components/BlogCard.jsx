77fb89f3fe00c581b6e80115b06169d5db65cf2c
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 22:50:16 -0400

    2026 Q3 Improvements @ 10:50:16 PM

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -7,27 +7,40 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { NavLink } from 'react-router-dom';
 
-const BlogCard = ({title, price, desc}) => {
+const BlogCard = ({ blogSlugs=[] }) => {
     return (
-        
-
-
-        <Card className="w-full">
-            <CardHeader>
-                <CardTitle className="p-2 bg-primary text-primary-foreground flex justify-center items-center rounded-md text-lg">
-                    { title }
-                </CardTitle>
-                <CardDescription className="p-2 bg-accent flex justify-center items-center rounded-md text-bold text-lg">
-                    ${price}
-                </CardDescription>
-            </CardHeader>
-            <CardContent>
-                <p className="p-2 text-center">
-                    { desc }
-                </p>
-            </CardContent>
-        </Card>
+        <>
+            {blogSlugs.map((slug) => {
+                <Card className="w-full">
+                    <CardHeader>
+                        { slug.img }
+                        <CardTitle className="">
+                            { slug.title }
+                        </CardTitle>
+                        <CardDescription className="p-2 bg-accent flex justify-center items-center rounded-md text-bold text-lg">
+                            { slug.desc }
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <NavLink 
+                            to={slug.url}
+                            end 
+                            className="hover:scale-102 ease-in-out duration-300 flex flex-1"
+                        >
+                            <Button 
+                                className="cursor-pointer" 
+                                variant="secondary"
+                            >
+                                Read Article
+                            </Button>
+                        </NavLink>
+                    </CardContent>
+                </Card>
+            })}
+        </>
     )
 }
 
