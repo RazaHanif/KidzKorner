f2bd76adaa51b0734a88da96f6ef7abdf02b8010
Raza Hanif <raza_hanif@ymail.com>
2026-08-11 19:11:14 -0400

    2026 Q3 Improvements @ 7:11:14 PM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -40,7 +40,7 @@ const Test = () => {
                 {Array.from({ length: 31 }, (_, index) => (
                     <div key={index}>
                         <Image 
-                            src='/media/KK/20250711_111518.jpg' 
+                            src={`/media/KK/20250711_111518.jpg`} 
                             alt='who cares' 
                         />
                     </div>
