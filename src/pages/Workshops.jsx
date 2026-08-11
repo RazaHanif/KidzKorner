ef0c4753c709ae944af0af60ba8ecffb758e1940
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 22:14:27 -0400

    2026 Q3 Improvements @ 10:14:27 PM

--- src/pages/Workshops.jsx
+++ src/pages/Workshops.jsx
@@ -98,7 +98,7 @@ const Workshops = () => {
                                 key={highlight.title}
                                 className="flex flex-col items-center text-center gap-3 p-5"
                             >
-                                <Icon className="text-xl" />
+                                <Icon className="font-bold text-primary" />
 
                                 <h3 className="font-bold text-lg">
                                     {highlight.title}
