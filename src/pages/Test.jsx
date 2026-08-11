2866eeb0ebae9bbdd6604cbee4000039eba558b2
Raza Hanif <raza_hanif@ymail.com>
2026-08-11 19:06:11 -0400

    2026 Q3 Improvements @ 7:06:10 PM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,6 +1,20 @@
 import { Shapes } from "lucide-react";
 
 const Test = () => {
+    const format = `    "toddler-activities-in-milton" : {
+        title: "Toddler Activities in Milton",
+        slug: "toddler-activities-in-milton",
+        description: "This will be a quick listcle",
+        image: "/media/Stock/12.jpg",
+        imageAlt: "Child playing with blocks at Kidz Korner Milton ",
+        publishedAt: "2026-08-08",
+        content: [
+            "Choosing the right daycare...",
+            "What should you look for in a daycare?",
+            "A good childcare program...",
+        ]
+    },`
+
     return (
         <div className="flex flex-col flex-1 justify-center items-center gap-4 w-full lg:py-16 pb-8">
             {/* <div className="flex flex-col justify-center items-center w-full">
@@ -16,7 +30,7 @@ const Test = () => {
             <div>
                 <h2>Format</h2>
                 <p>
-                    
+                    {format}
                 </p>
             </div>
 
