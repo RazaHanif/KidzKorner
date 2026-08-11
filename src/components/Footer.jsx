480784ef5d48a24e89253bd77661d384bf41dba5
Raza Hanif <raza_hanif@ymail.com>
2026-08-11 12:28:08 -0400

    2026 Q3 Improvements @ 12:28:08 PM

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -86,6 +86,7 @@ const Footer = () => {
       <div className="border-r border-chart-4 lg:flex hidden" />
       <div className="border-t border-chart-4 lg:hidden flex m-2" />
 
+
       <div className="flex-1 flex flex-col justify-start items-center">
         <h3 className="text-xl flex justify-center items-center w-full text-center">
           Contact Us
