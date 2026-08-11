e146753e72578e6627452d8d2a1371e8d0b933b7
Raza Hanif <raza_hanif@ymail.com>
2026-08-11 11:37:09 -0400

    2026 Q3 Improvements @ 11:37:09 AM

--- src/pages/BlogSlug.jsx
+++ src/pages/BlogSlug.jsx
@@ -64,10 +64,10 @@ const BlogSlug = () => {
         <div className="flex flex-col flex-1 justify-center items-center gap-6 w-full lg:py-16 pb-8">
             <div className="border w-full flex-1">
                  <Image src={blog.image} alt={blog.imageAlt} />
+                <h1 className="lg:text-5xl text-3xl font-bold font-serif text-start py-4 w-full">
+                    {blog.title}
+                </h1>
             </div>
-            <h1 className="lg:text-5xl text-3xl font-bold font-serif text-start py-4 w-full">
-                {blog.title}
-            </h1>
             <div>
                 {blog.description}
             </div>
