5b88ab8ab2d315064149d1703b787e3e966978c1
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 23:21:27 -0400

    2026 Q3 Improvements @ 11:21:27 PM

--- api/blogSlugs.js
+++ api/blogSlugs.js
@@ -1,14 +1,20 @@
-
-
-const blogSlugs = [
-    {
-    slug: "how-to-choose-a-daycare-in-milton",
-    title: "How to Choose a Daycare in Milton",
-    description: "...",
-    image: "...",
-    publishedAt: "2026-08-10",
-    body: "this is what to do"
+const blogs = {
+    "how-to-choose-a-daycare-in-milton" : {
+        title: "How to Choose a Daycare in Milton",
+        slug: "how-to-choose-a-daycare-in-milton",
+        description: "...",
+        image: "...",
+        publishedAt: "2026-08-10",
+        body: "this is what to do"
     },
-]
+    "toddler-activities-in-milton" : {
+        title: "Toddler Activities in Milton",
+        slug: "toddler-activities-in-milton",
+        description: "...",
+        image: "...",
+        publishedAt: "2026-08-08",
+        body: "this is what to do"
+    },
+}
 
-export default blogSlugs
+export default blogs
