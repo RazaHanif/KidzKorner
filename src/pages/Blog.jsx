63ec0166b6a95464f642fcd1ba6409e04280900c
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 22:26:34 -0400

    2026 Q3 Improvements @ 10:26:34 PM

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -13,9 +13,6 @@ const Blog = () => {
         },
     ];
 
-    const BlogCard = () => {
-
-    }
     return (
         <div className="flex flex-col flex-1 justify-center items-center gap-4 w-full lg:py-16 pb-8">
             <div className="flex flex-col justify-center items-center w-full mb-4">
