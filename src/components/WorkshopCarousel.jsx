fb9c94daafc1f8a5881c2baaa4e87eb4b4c3ae94
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 21:59:01 -0400

    2026 Q3 Improvements @ 9:59:01 PM

--- src/components/WorkshopCarousel.jsx
+++ src/components/WorkshopCarousel.jsx
@@ -90,7 +90,7 @@ const WorkshopCarousel = () => {
                 })}
             </CarouselContent>
 
-            <CarouselPrevious></CarouselPrevious>
+            <CarouselPrevious />
 
         </Carousel>
     )
