a6a1949602c887a47b7fe4346d301c3cef7273d4
Raza Hanif <raza_hanif@ymail.com>
2026-08-11 18:51:10 -0400

    2026 Q3 Improvements @ 6:51:09 PM

--- src/pages/Workshops.jsx
+++ src/pages/Workshops.jsx
@@ -153,7 +153,7 @@ const Workshops = () => {
 
             
             {/* Why is this form title in a different format than the others? */}
-            <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10">
+            <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 gap-6" id="toddler-form">
                 <WorkshopForm/>
             </div>
 
