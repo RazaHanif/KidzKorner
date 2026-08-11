5e167efe7aadd74662b499641fc4e7c20a3cfbbb
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 21:40:50 -0400

    2026 Q3 Improvements @ 9:40:50 PM

--- src/pages/PADayCamp.jsx
+++ src/pages/PADayCamp.jsx
@@ -76,7 +76,7 @@ const PADayCamp = () => {
                         className="hover:scale-105 ease-in-out duration-300 flex-1 flex justify-center"
                     >
                         <Button className="cursor-pointer min-w-1/2">
-                            Enquire About Summer Camp
+                            Enquire About PA Day Camp
                         </Button>
                     </a>
                 </div>
