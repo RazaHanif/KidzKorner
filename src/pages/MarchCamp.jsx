ce440f3dbf2fafbf1dc5cdd655819cba5e9a8fdb
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 21:19:10 -0400

    2026 Q3 Improvements @ 9:19:10 PM

--- src/pages/MarchCamp.jsx
+++ src/pages/MarchCamp.jsx
@@ -75,7 +75,7 @@ const MarchCamp = () => {
                         className="hover:scale-105 ease-in-out duration-300 flex-1 flex justify-center"
                     >
                         <Button className="cursor-pointer min-w-1/2">
-                            Enquire About Summer Camp
+                            Enquire About March Break Camp
                         </Button>
                     </a>
                 </div>
