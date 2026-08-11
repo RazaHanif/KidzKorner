fe622a76f97910b5aa4c312043ed10480b264e11
Raza Hanif <raza_hanif@ymail.com>
2026-08-11 11:36:19 -0400

    2026 Q3 Improvements @ 11:36:19 AM

--- api/blogSlugs.js
+++ api/blogSlugs.js
@@ -4,6 +4,7 @@ const blogs = {
         slug: "how-to-choose-a-daycare-in-milton",
         description: "...",
         image: "/media/Stock/2.jpg",
+        imageAlt: "/media/Stock/2.jpg",
         publishedAt: "2026-08-10",
         content: [
             "Choosing the right daycare...",
