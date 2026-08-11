7d3b871c0222e63ad8461dc0c37eaaf242f672e5
Raza Hanif <raza_hanif@ymail.com>
2026-08-10 21:34:46 -0400

    2026 Q3 Improvements @ 9:34:46 PM

--- src/pages/WinterCamp.jsx
+++ src/pages/WinterCamp.jsx
@@ -182,7 +182,7 @@ const WinterCamp = () => {
                         Winter Break Camp runs Monday to Friday from 9:00 a.m. to 3:00 p.m. Our program is limited to 13 children, allowing us to maintain a smaller group environment while campers participate in activities, play together, and make the most of their school break.                    
                         </p>
                     <p className="">
-                        The weekly cost is <b>$175 per child</b>, and all regular summer camp activities are included. Before- and after-camp care may also be available based on space, so families interested in extended hours can contact us to ask about availability.
+                        The weekly cost is <b>$175 per child</b>, and all regular Winter Break Camp activities are included. Before- and after-camp care may also be available based on space, so families interested in extended hours can contact us to ask about availability.
                     </p>
                 </div>
             </div>
