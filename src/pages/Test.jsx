3bc7f5d8e8de5c3d0cf19c7ba17f45b178264f88
Raza Hanif <raza_hanif@ymail.com>
2026-08-11 19:14:18 -0400

    2026 Q3 Improvements @ 7:14:18 PM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -39,7 +39,7 @@ const Test = () => {
 
             <div className="flex w-full flex-col justify-center items-center gap-8">
                 {Array.from({ length: 31 }, (_, index) => (
-                    <div key={index + 1} className="flex flexborder">
+                    <div key={index + 1} className="flex flex-col justify-center items-center border">
                         <Image 
                             src={`/media/Stock/${index+1}.jpg`} 
                             alt='who cares' 
