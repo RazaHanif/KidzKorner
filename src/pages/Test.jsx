86e69752edbabee50fa9011ce123653286659c05
Raza Hanif <raza_hanif@ymail.com>
2026-08-12 13:15:43 -0400

    2026 Q3 Improvements @ 1:15:43 PM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -62,11 +62,8 @@ const Test = () => {
                 </div>
 
                 {/* March Break Camp */}
-                <div
-                    className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 gap-6 lg:pt-12 pt-8 lg:mt-12 mt-8"
-                    id="march-camp-form"
-                >
-                    <p className="flex text-center">
+                <div className="flex flex-col justify-center items-center w-full">
+                    <p className="flex justify-center items-center w-full">
                         March Break Camp
                     </p>
 
