acaa0cbbbfc3aeafdff2cde62804f694f4185c6e
Raza Hanif <raza_hanif@ymail.com>
2026-08-12 13:06:21 -0400

    2026 Q3 Improvements @ 1:06:21 PM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -103,13 +103,13 @@ const Test = () => {
                     <DaycareForm />
                 </div>
 
-                            {/* Daycare */}
-            <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 gap-6 lg:py-12 py-8 lg:my-12 my-8">
-                <p className="flex text-center">
-                    Ready to join us? Contact us today to learn more about our preschool programs and schedule a tour of our facility! 
-                </p>
-                <DaycareForm/>
-            </div>
+                {/* Daycare */}
+                <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 gap-6 lg:py-12 py-8 lg:my-12 my-8">
+                    <p className="flex text-center">
+                        Ready to join us? Contact us today to learn more about our preschool programs and schedule a tour of our facility! 
+                    </p>
+                    <DaycareForm/>
+                </div>
 
             </div>
 
