c61bd472ef7f0ca1601ea4d8b25e59ec5e889e53
Raza Hanif <raza_hanif@ymail.com>
2026-08-12 13:14:32 -0400

    2026 Q3 Improvements @ 1:14:32 PM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -13,7 +13,7 @@ const Test = () => {
                 
                 {/* Workshop Form */}
                 <div className="flex flex-col justify-center items-center w-full">
-                    <p className="flex just w-full">
+                    <p className="flex justify-center items-center w-full">
                         Workshop Form
                     </p>
                     <WorkshopForm />
@@ -25,7 +25,7 @@ const Test = () => {
 
                 {/* Camp */}
                 <div className="flex flex-col justify-center items-center w-full">
-                    <p className="flex just w-full">
+                    <p className="flex justify-center items-center w-full">
                         Camp
                     </p> 
                     <CampForm/>
