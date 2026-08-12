635a47d2b42fff80c730c90006ead50bd9d44bb7
Raza Hanif <raza_hanif@ymail.com>
2026-08-12 13:11:05 -0400

    2026 Q3 Improvements @ 1:11:05 PM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -12,7 +12,7 @@ const Test = () => {
                 <h2 className="text-xl font-bold">Work Shop Forms</h2>
                 
                 {/* Workshop Form */}
-                <div className="flex justify-center items-center">
+                <div className="flex flex-col justify-center items-center w-full">
                     <p>
                         Workshop Form
                     </p>
