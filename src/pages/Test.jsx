474f861e1ecbaef97f7259ede9729109d11ca15a
Raza Hanif <raza_hanif@ymail.com>
2026-08-12 12:55:04 -0400

    2026 Q3 Improvements @ 12:55:04 PM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -6,7 +6,7 @@ const Test = () => {
         <>
             {/* Workshop Form */}
             <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 gap-6" id="toddler-form">
-                <WorkshopForm/>
+                <WorkshopForm />
             </div>
 
             {/* Winter Camp */}
