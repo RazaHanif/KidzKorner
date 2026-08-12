bdb0853399438ea2c94d7bd51e206eb92260096c
Raza Hanif <raza_hanif@ymail.com>
2026-08-12 14:48:14 -0400

    2026 Q3 Improvements @ 2:48:14 PM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -47,7 +47,7 @@ const Test = () => {
                     <p className="flex justify-center items-center w-full">
                         Summer Break Camp
                     </p>
-                    <CampForm campType/>
+                    <CampForm campType={"summer"}/>
                 </div>
 
                 {/* PA Day Camp */}
