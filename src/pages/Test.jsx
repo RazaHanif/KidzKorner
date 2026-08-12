12a7b60e13c10df7f1b4f8054756815cd1f67658
Raza Hanif <raza_hanif@ymail.com>
2026-08-12 12:47:24 -0400

    2026 Q3 Improvements @ 12:47:23 PM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -4,6 +4,11 @@ import Image from "@/components/Image";
 const Test = () => {
     return (
         <>
+            {/* Workshop Form */}
+            <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 gap-6" id="toddler-form">
+                <WorkshopForm/>
+            </div>
+
             
 
         </>
