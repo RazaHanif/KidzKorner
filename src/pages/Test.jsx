f0bbe90aeec5ebab092839c425be7ceb604c9758
Raza Hanif <raza_hanif@ymail.com>
2026-08-12 13:09:45 -0400

    2026 Q3 Improvements @ 1:09:45 PM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -7,10 +7,11 @@ import PartyForm from "../components/PartyForm";
 const Test = () => {
     return (
         <div className="flex flex-col gap-6 justify-center items-center">
-            
-            {/* Workshop Form */}
+
             <div className="flex flex-col justify-center items-center w-full border">
                 <h2 className="text-xl font-bold">Work Shop Forms</h2>
+                
+                {/* Workshop Form */}
                 <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 gap-6" id="toddler-form">
                     <WorkshopForm />
                 </div>
