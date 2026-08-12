a939fc89b1c3cefecf0abcd36206c5a8a31bee40
Raza Hanif <raza_hanif@ymail.com>
2026-08-12 17:07:31 -0400

    2026 Q3 Improvements @ 5:07:31 PM

--- src/pages/PADayCamp.jsx
+++ src/pages/PADayCamp.jsx
@@ -272,7 +272,7 @@ const PADayCamp = () => {
             </div>
 
             <div
-                className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 gap-6 border-t lg:pt-12 pt-8 lg:mt-12 mt-8"
+                className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 gap-2 border-t lg:pt-12 pt-8 lg:mt-12 mt-8"
                 id="pa-day-form"
             >
                 <p className="flex text-center">Interested in PA Day Camp?</p>
