8ffd113a0874ff5347e0916afae4a245f1a3bb58
Raza Hanif <raza_hanif@ymail.com>
2026-08-12 13:12:36 -0400

    2026 Q3 Improvements @ 1:12:36 PM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -8,7 +8,7 @@ const Test = () => {
     return (
         <div className="flex flex-col gap-6 justify-center items-center w-full">
 
-            <div className="flex flex-col justify-center items-center w-full border">
+            <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 border">
                 <h2 className="text-xl font-bold">Work Shop Forms</h2>
                 
                 {/* Workshop Form */}
@@ -20,7 +20,7 @@ const Test = () => {
                 </div>
             </div>
 
-            <div className="flex flex-col justify-center items-center w-full border">
+            <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 border">
                 <h2 className="text-xl font-bold">Camp Forms</h2>
 
                 {/* Camp */}
@@ -88,7 +88,7 @@ const Test = () => {
                 </div>
             </div>
 
-            <div className="flex flex-col justify-center items-center w-full border">
+            <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 border">
                 <h2 className="text-xl font-bold">Daycare Forms</h2>
 
                 {/* Daycare */}
@@ -135,7 +135,7 @@ const Test = () => {
             </div>
 
 
-            <div className="flex flex-col justify-center items-center w-full border">
+            <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 border">
                 <h2 className="text-xl font-bold">Party Form</h2>
 
                 {/* Parties */}
@@ -148,7 +148,7 @@ const Test = () => {
 
             </div>
 
-            <div className="flex flex-col justify-center items-center w-full border">
+            <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 border">
 
                 <h2 className="text-xl font-bold">Contact Form</h2>
 
