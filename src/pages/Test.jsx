75cdc9a2d0286625f97344ccb9640bc5bc51a738
Raza Hanif <raza_hanif@ymail.com>
2026-08-12 17:13:07 -0400

    2026 Q3 Improvements @ 5:13:07 PM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -8,15 +8,6 @@ const Test = () => {
     return (
         <div className="flex flex-col gap-16 justify-center items-center w-full">
             <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 border py-16">
-                <h2 className="text-xl font-bold">Work Shop Forms</h2>
-
-                {/* Workshop Form */}
-                <div className="flex flex-col justify-center items-center w-full">
-                    <p className="flex justify-center items-center w-full">
-                        Workshop Form
-                    </p>
-                    <WorkshopForm />
-                </div>
             </div>
 
             <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 border py-16">
