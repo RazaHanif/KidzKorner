1a8314436f7bf7e429f19f7b57b8503c93efcf1b
Raza Hanif <raza_hanif@ymail.com>
2026-08-12 12:59:19 -0400

    2026 Q3 Improvements @ 12:59:19 PM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -8,7 +8,7 @@ const Test = () => {
     return (
         <div className="flex flex-col gap-6 justify-center items-center">
             {/* Workshop Form */}
-            <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 gap-6" id="toddler-form">
+            <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 gap-6  " id="toddler-form">
                 <WorkshopForm />
             </div>
 
