15fb2a1b1a080e2885349795c70b50968e24b588
Raza Hanif <raza_hanif@ymail.com>
2026-08-12 14:10:42 -0400

    2026 Q3 Improvements @ 2:10:42 PM

--- src/components/forms/WorkshopForm.jsx
+++ src/components/forms/WorkshopForm.jsx
@@ -100,8 +100,7 @@ const WorkshopForm = () => {
                     </div>
 
                     <div className="grid items-center gap-2 w-4/5 p-2">
-                    <label 
-                        htmlFor="email">
+                    <label htmlFor="email">
                         Email
                     </label>
                     <Input
