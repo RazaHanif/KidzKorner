a99f50352f5cb469843884db3291e2d2f2f72669
Raza Hanif <raza_hanif@ymail.com>
2026-08-12 17:02:02 -0400

    2026 Q3 Improvements @ 5:02:02 PM

--- src/components/forms/WorkshopForm.jsx
+++ src/components/forms/WorkshopForm.jsx
@@ -2,10 +2,6 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import {
-  RadioGroup,
-  RadioGroupItem,
-} from "@/components/ui/radio-group"
-import {
   Select,
   SelectContent,
   SelectGroup,
