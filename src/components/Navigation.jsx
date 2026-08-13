3bda10afd53a723716037945b30d206500bb4e84
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 11:53:07 -0400

    2026 Q3 Improvements @ 11:53:07 AM

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -38,7 +38,7 @@ function Navigation() {
             {/* Desktop Navigation */}
             <div className="hidden lg:flex flex-row w-4xl text-2xl justify-between items-center gap-4 text-nowrap border border-red-500">
                 <NavigationMenu className="hidden lg:flex flex-row justify-between items-center gap-4 text-nowrap w-4xl text-2xl border border-blue-500">
-                    <NavigationMenuList>
+                    <NavigationMenuList className="">
                         <NavigationMenuItem>
                             <NavigationMenuTrigger
                                 className="bg-primary text-primary-foreground text-2xl"
