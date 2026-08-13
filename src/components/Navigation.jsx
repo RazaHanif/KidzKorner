c4ce92dda7e966cf6b58da492bba8a75180437c6
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 11:18:21 -0400

    2026 Q3 Improvements @ 11:18:21 AM

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -51,9 +51,9 @@ function Navigation() {
                     <NavLink 
                         to='/parties' 
                         end
-                                  className={({ isActive }) =>
-            `text-primary-foreground duration-300 ease-in-out ${ isActive ? "underline underline-offset-2" : "hover:scale-110" }`
-          }
+                        className={({ isActive }) =>
+                            `text-primary-foreground duration-300 ease-in-out ${ isActive ? "underline underline-offset-2" : "hover:scale-110" }`
+                        }
                     >
                             Parties
                     </NavLink>
