20812d4d7d769f701d0e86b3cb0efefef90a0c4e
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 11:08:35 -0400

    2026 Q3 Improvements @ 11:08:35 AM

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -37,7 +37,7 @@ function Navigation() {
                             Day Care
                         </NavLink>
                     </NavigationMenuTrigger>
-                    <NavigationMenuContent>
+                    <NavigationMenuContent >
                         <NavigationMenuLink>
                             <NavLink 
                                 to="/daycare/toddler" 
