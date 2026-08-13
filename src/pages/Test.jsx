71284865eb0f975269ae2575705271b91c8d9e5d
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 11:02:49 -0400

    2026 Q3 Improvements @ 11:02:49 AM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -54,6 +54,17 @@ const components = [
   },
 ]
 
+function ListItem({ title, children, href, ...props}) {
+  return (
+    <li {...props}>
+      <NavigationMenuLink render={<Link href={href}><div className="flex flex-col gap-1 text-sm">
+          <div className="leading-none font-medium">{title}</div>
+          <div className="line-clamp-2 text-muted-foreground">{children}</div>
+        </div></Link>} />
+    </li>
+  )
+}
+
 const Test = () => {
     return (
         <div className="flex flex-col gap-16 justify-center items-center w-full">
