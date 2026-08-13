4caaeadbcbef52fc318fd2355c940433da2d5664
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 11:04:53 -0400

    2026 Q3 Improvements @ 11:04:53 AM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -73,7 +73,7 @@ const Test = () => {
                 <NavigationMenu>
                     <NavigationMenuList>
                         <NavigationMenuItem>
-                            <NavigationMenuTrigger>
+                            <NavigationMenuTrigger className="bg">
                                 Getting started
                             </NavigationMenuTrigger>
                             <NavigationMenuContent>
