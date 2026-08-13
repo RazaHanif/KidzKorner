d705ef802ba477fd6a4b2ca097405828e01225d5
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 11:49:30 -0400

    2026 Q3 Improvements @ 11:49:30 AM

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -23,7 +23,7 @@ function Navigation() {
 
     return (
         <>
-            {/*  */}
+            {/* Desktop Navigation */}
             <div className="hidden lg:flex flex-row w-4xl text-2xl justify-between items-center gap-4 text-nowrap">
                 <NavigationMenu>
                     <NavigationMenuList>
