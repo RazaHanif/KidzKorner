f4567408c3b795b538bcae8dd0ae484293b3dbbf
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 11:43:27 -0400

    2026 Q3 Improvements @ 11:43:27 AM

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -70,7 +70,7 @@ function Navigation() {
                         Camp
                     </NavigationMenuTrigger>
                     <NavigationMenuContent>
-                        <NavigationMenuLink onClick={() => navigate('/camp/summer-camp')}>
+                        <NavigationMenuLink onClick={() => navigate('/camp/summer')}>
                             Summer Camp
                         </NavigationMenuLink>
                         <NavigationMenuLink onClick={() => navigate('/camp/march-camp')}>
