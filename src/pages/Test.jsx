0280cd288eeb2bf5c8b61aaca7f6809d3950b556
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 10:52:51 -0400

    2026 Q3 Improvements @ 10:52:50 AM

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -2,7 +2,7 @@ const Test = () => {
     return (
         <div className="flex flex-col gap-16 justify-center items-center w-full">
             <div className="flex flex-col justify-center items-center lg:w-3/4 w-9/10 border py-16">
-                Test Page
+
             </div>
         </div>
     );
