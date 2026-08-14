609a6614b6cd00b5a72833556965de57d3fe5586
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 21:27:01 -0400

    2026 Q3 Improvements @ 9:27:01 PM

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -197,7 +197,7 @@ function Navigation() {
                             </DropdownMenuItem>
 
                             <DropdownMenuSub>
-                                <DropdownMenuSubTrigger asChild>
+                                <DropdownMenuSubTrigger >
                                     <NavLink to="/daycare" className="cursor-pointer" end>
                                         Day Care
                                     </NavLink>
