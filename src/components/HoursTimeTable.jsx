4ba062c702b89b39c1924a0da9f74029ab8288d2
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 23:25:37 -0400

    2026 Q3 Improvements @ 11:25:37 PM

--- src/components/HoursTimeTable.jsx
+++ src/components/HoursTimeTable.jsx
@@ -35,7 +35,7 @@ const HoursTimeTable = () => {
                 <TableBody>
                     {timetable.map((item, idx) => (
                     <TableRow key={idx} className="">
-                            <TableCell className='font-semibold whitespace-normal wrap-break-word'>
+                            <TableCell className="font-semibold whitespace-normal wrap-break-word min-w-0 flex-1">
                                 {item.session}
                             </TableCell>
                             <TableCell className="flex justify-center items-center w-full">
