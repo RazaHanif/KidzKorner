8e295b7f50b6014fafe197f3b61690db5388a2b6
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 22:10:08 -0400

    2026 Q3 Improvements @ 10:10:08 PM

--- src/App.jsx
+++ src/App.jsx
@@ -61,9 +61,7 @@ function App() {
 
                     <Route path="/indoor-playground" element={<Playground />} />
 
-                    {/*                     
-                    <Route path="/test" element={<Test />} />
-*/}
+                    {/* <Route path="/test" element={<Test />} /> */}
 
                     <Route path="/workshops" element={<Workshops />} />
 
