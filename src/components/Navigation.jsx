fa5e0034d1d82e4e0ff86332823d17497bad8feb
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 21:22:02 -0400

    2026 Q3 Improvements @ 9:22:02 PM

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -148,6 +148,69 @@ function Navigation() {
                 </NavigationMenuList>
             </NavigationMenu>
 
+    <DropdownMenu className='bg-red-500'>
+        <DropdownMenuTrigger 
+            render={
+                <Button variant="outline">Open</Button>
+            } 
+        />
+        <DropdownMenuContent>
+            <DropdownMenuGroup>
+                <DropdownMenuItem>
+                    Team
+                </DropdownMenuItem>
+                <DropdownMenuSub>
+                    <DropdownMenuSubTrigger>
+                        Invite users
+                    </DropdownMenuSubTrigger>
+                    <DropdownMenuPortal>
+                    <DropdownMenuSubContent>
+                        <DropdownMenuItem>
+                            Email
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            Message
+                        </DropdownMenuItem>
+                        <DropdownMenuSub>
+                            <DropdownMenuSubTrigger>
+                                More options
+                            </DropdownMenuSubTrigger>
+                            <DropdownMenuPortal>
+                                <DropdownMenuSubContent>
+                                    <DropdownMenuItem>
+                                        Calendly
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem>
+                                        Slack
+                                    </DropdownMenuItem>
+
+                                    <DropdownMenuSeparator />
+                                    
+                                    <DropdownMenuItem>
+                                        Webhook
+                                    </DropdownMenuItem>
+                                </DropdownMenuSubContent>
+                            </DropdownMenuPortal>
+                        </DropdownMenuSub>
+
+                        <DropdownMenuSeparator />
+
+                        <DropdownMenuItem>
+                            Advanced...
+                        </DropdownMenuItem>
+                    </DropdownMenuSubContent>
+                    </DropdownMenuPortal>
+                </DropdownMenuSub>
+                <DropdownMenuItem>
+                    New Team
+                    <DropdownMenuShortcut>
+                        ⌘+T
+                    </DropdownMenuShortcut>
+                </DropdownMenuItem>
+            </DropdownMenuGroup>
+        </DropdownMenuContent>
+    </DropdownMenu>
+
 
             {/* Mobile Navigation */}
             <div className="lg:hidden flex flex-col justify-center items-center">
