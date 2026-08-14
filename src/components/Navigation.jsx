5dbdcb94f9eb2690824af3972a60b07d3538d978
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 21:52:07 -0400

    2026 Q3 Improvements @ 9:52:07 PM

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -242,7 +242,7 @@ function Navigation() {
 
                             <DropdownMenuSub>
                                 <DropdownMenuSubTrigger 
-                                    className="cursor-pointer p-2"
+                                    className="cursor-pointer p-2 text-md"
                                 >
                                     Camp
                                 </DropdownMenuSubTrigger>
