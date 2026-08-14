5d1ca55a9e61fa942dbbb6f8681dbad0237ae5cc
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 23:22:27 -0400

    2026 Q3 Improvements @ 11:22:27 PM

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -55,7 +55,7 @@ const About = () => {
                     />
                 </div>
 
-                <div className="flex justify-center items-center">
+                <div>
                     <HoursTimeTable />
                 </div>
             </div>
