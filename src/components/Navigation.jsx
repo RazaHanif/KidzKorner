22bf03ff39f1f5ffa8b1a4adb25db9227363790e
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 21:15:59 -0400

    2026 Q3 Improvements @ 9:15:59 PM

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -33,33 +33,33 @@ export function DropdownMenuSubmenu() {
         />
         <DropdownMenuContent>
             <DropdownMenuGroup>
-            <DropdownMenuItem>Team</DropdownMenuItem>
-            <DropdownMenuSub>
-                <DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
-                <DropdownMenuPortal>
-                <DropdownMenuSubContent>
-                    <DropdownMenuItem>Email</DropdownMenuItem>
-                    <DropdownMenuItem>Message</DropdownMenuItem>
-                    <DropdownMenuSub>
-                    <DropdownMenuSubTrigger>More options</DropdownMenuSubTrigger>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+                <DropdownMenuSub>
+                    <DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
                     <DropdownMenuPortal>
-                        <DropdownMenuSubContent>
-                        <DropdownMenuItem>Calendly</DropdownMenuItem>
-                        <DropdownMenuItem>Slack</DropdownMenuItem>
+                    <DropdownMenuSubContent>
+                        <DropdownMenuItem>Email</DropdownMenuItem>
+                        <DropdownMenuItem>Message</DropdownMenuItem>
+                        <DropdownMenuSub>
+                        <DropdownMenuSubTrigger>More options</DropdownMenuSubTrigger>
+                        <DropdownMenuPortal>
+                            <DropdownMenuSubContent>
+                            <DropdownMenuItem>Calendly</DropdownMenuItem>
+                            <DropdownMenuItem>Slack</DropdownMenuItem>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>Webhook</DropdownMenuItem>
+                            </DropdownMenuSubContent>
+                        </DropdownMenuPortal>
+                        </DropdownMenuSub>
                         <DropdownMenuSeparator />
-                        <DropdownMenuItem>Webhook</DropdownMenuItem>
-                        </DropdownMenuSubContent>
+                        <DropdownMenuItem>Advanced...</DropdownMenuItem>
+                    </DropdownMenuSubContent>
                     </DropdownMenuPortal>
-                    </DropdownMenuSub>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>Advanced...</DropdownMenuItem>
-                </DropdownMenuSubContent>
-                </DropdownMenuPortal>
-            </DropdownMenuSub>
-            <DropdownMenuItem>
-                New Team
-                <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
-            </DropdownMenuItem>
+                </DropdownMenuSub>
+                <DropdownMenuItem>
+                    New Team
+                    <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+                </DropdownMenuItem>
             </DropdownMenuGroup>
         </DropdownMenuContent>
     </DropdownMenu>
