9e5e5c57b1efe35cb18fd5a3e8c1daef15d94413
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 21:46:39 -0400

    2026 Q3 Improvements @ 9:46:38 PM

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -164,7 +164,7 @@ function Navigation() {
                         <DropdownMenuGroup>
                             <DropdownMenuSub>
                                 <DropdownMenuSubTrigger 
-                                    className="cursor-pointer"
+                                    className="cursor-pointer "
                                 >
                                     Day Care
                                 </DropdownMenuSubTrigger>
@@ -242,7 +242,7 @@ function Navigation() {
 
                             <DropdownMenuSub>
                                 <DropdownMenuSubTrigger 
-                                    className="cursor-pointer"
+                                    className="cursor-pointer "
                                 >
                                     Camp
                                 </DropdownMenuSubTrigger>
