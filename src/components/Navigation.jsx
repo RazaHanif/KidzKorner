b12a2eac0b0ca1c399e45607d24d99825e9b3945
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 22:04:22 -0400

    2026 Q3 Improvements @ 10:04:21 PM

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -150,7 +150,7 @@ function Navigation() {
 
 
 
-            
+            {/* Mobile Navigation */}
             <div className="lg:hidden flex flex-col justify-center items-center">
                 <DropdownMenu>
                     <DropdownMenuTrigger asChild className="cursor-pointer">
