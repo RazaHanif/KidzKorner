c8abe7db010288d9b0399fb83e56474461477605
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 21:45:35 -0400

    2026 Q3 Improvements @ 9:45:34 PM

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -159,7 +159,7 @@ function Navigation() {
                         side="bottom"
                         align="end"
                         sideOffset={8}
-                        className="w-72"
+                        className="w-32"
                     >
                         <DropdownMenuGroup>
                             <DropdownMenuSub>
