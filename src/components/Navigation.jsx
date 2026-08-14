c79676eb3cc0034bf3328fed78073cd2b091445a
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 21:42:57 -0400

    2026 Q3 Improvements @ 9:42:57 PM

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -155,7 +155,7 @@ function Navigation() {
                             <Menu />
                         </Button>
                     </DropdownMenuTrigger>
-                    <DropdownMenuContent className="w-64 text-xl">
+                    <DropdownMenuContent className="w-64 text-2xl font-bold">
                         <DropdownMenuGroup>
                             <DropdownMenuSub>
                                 <DropdownMenuSubTrigger 
