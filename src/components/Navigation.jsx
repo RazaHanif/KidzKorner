c743c13f11cd5d756a3b4f8e24e1725acbd6fceb
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 21:49:57 -0400

    2026 Q3 Improvements @ 9:49:57 PM

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -200,7 +200,7 @@ function Navigation() {
                                         <DropdownMenuItem asChild>
                                             <NavLink 
                                                 to="/daycare/before-after-school"
-                                                className="cursor-pointer p-2 text-xl flex flex-col text-start"
+                                                className="cursor-pointer p-2 text-xl"
                                                 end
                                             >
                                                 <p>
