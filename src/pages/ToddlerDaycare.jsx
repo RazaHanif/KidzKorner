6a48ffefa910acdfd1a80d8c367b6c59fab8cda2
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 23:50:46 -0400

    pic 11 got deleted idk how

--- src/pages/ToddlerDaycare.jsx
+++ src/pages/ToddlerDaycare.jsx
@@ -100,7 +100,7 @@ const ToddlerDaycare = () => {
                 </div>
                 <div className="flex justify-center items-center">
                     <Image
-                        src="/media/Stock/11.jpg"
+                        src="/media/Stock/7.jpg"
                         alt="Child Playing With Blocks at Kidz Korner Milton, Ontario"
                         w="w-sm"
                     />
