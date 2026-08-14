839d16260ab67b0253e25033eef5c9529bbe7ef6
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 22:29:11 -0400

    2026 Q3 Improvements @ 10:29:11 PM

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -184,10 +184,10 @@ const Footer = () => {
     </div>
 
     <div className="flex justify-center items-center font-light">
-        <p className="border border-red-500 flex ">
+        <p className="border border-red-500 flex justify-center items-center text-center">
             ©
         </p>
-        <p className="border border-red-500 flex ">
+        <p className="border border-red-500 flex justify-center items-center text-center">
             {new Date().getFullYear()} Kidz Korner Milton. All rights reserved.
         </p>
     </div>
