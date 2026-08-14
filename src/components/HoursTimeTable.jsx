d49230538053674817e85bab853b2f1f4c05e787
Raza Hanif <raza_hanif@ymail.com>
2026-08-13 23:22:58 -0400

    2026 Q3 Improvements @ 11:22:58 PM

--- src/components/HoursTimeTable.jsx
+++ src/components/HoursTimeTable.jsx
@@ -38,7 +38,7 @@ const HoursTimeTable = () => {
                             <TableCell className='font-semibold whitespace-normal wrap-break-word'>
                                 {item.session}
                             </TableCell>
-                            <TableCell className="flex justify-center items-center flex-2 w-full">
+                            <TableCell className="flex justify-center items-center w-full">
                                 <p className="text-end w-full">
                                     {item.time}
                                 </p>
