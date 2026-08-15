55e945d97e380ff1b683012a2cf5c555caec6653
Raza Hanif <raza_hanif@ymail.com>
2026-08-14 21:11:57 -0400

    2026 Q3 Improvements @ 9:11:57 PM

--- src/components/ContactUsTable.jsx
+++ src/components/ContactUsTable.jsx
@@ -42,7 +42,7 @@ const ContactUsTable = () => {
                                     target="_blank"
                                     rel="noopener noreferrer" 
                                     href={item.href}
-                                    className="hover:underline flex flex-row w-full flex-1 "
+                                    className="hover:underline flex flex-row w-full flex-1 text-end"
                                 >
                                     <div className="text-end w-full">
                                         {item.text}
