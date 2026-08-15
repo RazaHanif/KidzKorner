89fef99623cf60ec36e86037a9bb1a82b2234d45
Raza Hanif <raza_hanif@ymail.com>
2026-08-14 21:03:59 -0400

    2026 Q3 Improvements @ 9:03:59 PM

--- src/pages/BeforeAfterDaycare.jsx
+++ src/pages/BeforeAfterDaycare.jsx
@@ -101,7 +101,7 @@ const BeforeAfterDaycare = () => {
                     <Image
                         src="/media/Stock/31.jpg"
                         alt="Child Doing Arts & Crafts at Kidz Korner Milton, Ontario"
-                        w="w-lg"
+                        w="w-sm"
                     />
                 </div>
             </div>
